e31cb84871928f2710c3483bd64bd814a5ef0914
Sumanth Puram <sumanth@rudderlabs.com>
2019-07-22 22:31:08 +0530

    AppSpec and BuildSpec for CodePipeline

---
+++ scripts/start_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /home/ubuntu
+chown -R ubuntu:ubuntu /home/ubuntu/rudder-server
+
+# Stop all servers and start the server as a daemon
+kill $(lsof -t -i:8080)
+sudo -u ubuntu ./rudder-server
