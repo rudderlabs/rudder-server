be429c64541c42028495d7c8a2ed490b0b0ec12a
Sumanth Puram <sumanth@rudderlabs.com>
2019-07-23 17:17:23 +0530

    Copy the .env file to the directory

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 chown -R ubuntu:ubuntu /home/ubuntu/rudder-server
+
+# Copy the .env from home folder
+cp /home/ubuntu/.env /home/ubuntu/rudder-server/.env
+
 systemctl enable rudder.service
 systemctl restart rudder.service
