a77dbf42708e3881bc17fc33c8f6b2c4810ddebb
Sumanth Puram <sumanth@rudderlabs.com>
2019-07-23 10:56:31 +0530

    Upstart script for rudder

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 chown -R ubuntu:ubuntu /home/ubuntu/rudder-server
-cd /home/ubuntu/rudder-server
-cp /home/ubuntu/.env /home/ubuntu/rudder-server/.env
-
-# Stop all servers and start the server as a daemon
-kill $(lsof -t -i:8080) || true
-nohup ./rudder-server &
+service rudder stop || true
+service rudder start
