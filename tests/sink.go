3be44ca249e0238a3feb82d8ea0d4a0f8940962d
Sumanth Puram <sumanth.iitm@gmail.com>
2019-07-29 10:55:07 +0530

    Correctness Testing: User IDs, Event IDs, Event order

--- tests/sink.go
+++ tests/sink.go
@@ -5,12 +5,26 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
+	"strconv"
+	"strings"
 	"sync/atomic"
 	"time"
+
+	"github.com/rudderlabs/rudder-server/misc"
+
+	"github.com/go-redis/redis"
+)
+
+const (
+	redisServer = "redis:6379"
+	testName    = "Test8"
 )
 
 var count uint64
 var showPayload = false
+
+var enableTestStats = true
+var redisChan chan string
 
 func handleReq(rw http.ResponseWriter, req *http.Request) {
 	if showPayload {
@@ -19,8 +33,13 @@ func handleReq(rw http.ResponseWriter, req *http.Request) {
 	}
 	if req.Body != nil {
 		ioutil.ReadAll(req.Body)
-		defer req.Body.Close() 
+		defer req.Body.Close()
 	}
+
+	if enableTestStats {
+		redisChan <- req.URL.Query().Get("ea")
+	}
+
 	atomic.AddUint64(&count, 1)
 	respMessage := "OK"
 	rw.Write([]byte(respMessage))
@@ -34,9 +53,58 @@ func printCounter() {
 	}
 }
 
+func redisLoop() {
+	var batchTimeout = 1000 * time.Millisecond
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: redisServer,
+	})
+
+	redisUserSet := fmt.Sprintf("%s_user_dst", testName)
+	redisEventSet := fmt.Sprintf("%s_event_dst", testName)
+	redisEventTimeHash := fmt.Sprintf("%s_event_dst_timestamp", testName)
+
+	pipe := redisClient.Pipeline()
+	var eventAdded bool
+
+	for {
+		select {
+		case eventData := <-redisChan:
+			data := strings.Split(eventData, "-")
+			userID := data[0]
+			messageID := data[1]
+			eventTime, err := strconv.Atoi(data[2])
+			misc.AssertError(err)
+
+			pipe.RPush(testName+":"+userID+":dst_list", messageID)
+			pipe.SAdd(redisUserSet, userID)
+			pipe.SAdd(redisEventSet, messageID)
+			pipe.HSet(redisEventTimeHash, messageID, eventTime)
+			eventAdded = true
+			// pipe.ZAdd(redisEventTimeSortedSet, redis.Z{Score: float64(eventTime), Member: messageID})
+
+		case <-time.After(batchTimeout):
+			if eventAdded {
+				_, err := pipe.Exec()
+				misc.AssertError(err)
+				// fmt.Println("*******************************")
+				// fmt.Println("*******************************")
+				// fmt.Println(responses)
+			}
+			eventAdded = false
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Starting server")
+	redisChan = make(chan string)
 	go printCounter()
+
+	if enableTestStats {
+		go redisLoop()
+	}
+
 	http.HandleFunc("/", handleReq)
 	http.ListenAndServe(":8181", nil)
 }
