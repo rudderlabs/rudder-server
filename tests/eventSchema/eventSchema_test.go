594989dee9f532606ccbd25b637decc86e9892a3
Chandra shekar Varkala <chandra@rudderlabs.com>
2019-09-19 12:52:59 +0530

    removed unused vars

--- tests/eventSchema/eventSchema_test.go
+++ tests/eventSchema/eventSchema_test.go
@@ -5,17 +5,12 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/rudderlabs/rudder-server/config"
 	"github.com/rudderlabs/rudder-server/tests/helpers"
 )
 
 var dbHandle *sql.DB
-var gatewayDBPrefix string
-var routerDBPrefix string
 var dbPollFreqInS int = 1
 var gatewayDBCheckBufferInS int = 5
-var jobSuccessStatus string = "succeeded"
-
 
 var _ = BeforeSuite(func() {
 	var err error
@@ -24,8 +19,6 @@ var _ = BeforeSuite(func() {
 	if err != nil {
 		panic(err)
 	}
-	gatewayDBPrefix = config.GetString("Gateway.CustomVal", "GW")
-	routerDBPrefix = config.GetString("Router.CustomVal", "RT")
 })
 
 var _ = Describe("EventSchema", func() {
@@ -36,11 +29,11 @@ var _ = Describe("EventSchema", func() {
 			initEventSchemaCount := helpers.FetchEventSchemaCount(dbHandle)
 
 			helpers.SendEventRequest(helpers.EventOptsT{})
-		
+
 			Eventually(func() int {
 				return helpers.FetchEventSchemaCount(dbHandle)
 			}, gatewayDBCheckBufferInS, dbPollFreqInS).Should(Equal(initEventSchemaCount + 1))
-		
+
 		})
 	})
 })
