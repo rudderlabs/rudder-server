9f9539a46341a8d8a9b2732c18eab27c64aa53a7
Sayan Mitra <sayan@rudderlabs.com>
2019-11-03 20:29:06 +0530

    revert adding time info to logs

--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"time"
 
 	"github.com/rudderlabs/rudder-server/config"
 )
@@ -48,7 +47,7 @@ func IsDebugLevel() bool {
 // Most verbose logging level.
 func Debug(args ...interface{}) (int, error) {
 	if levelDebug >= level {
-		fmt.Print(time.Now().Format(time.RFC850), " DEBUG: ")
+		fmt.Print(" DEBUG: ")
 		return fmt.Println(args...)
 	}
 	return 0, nil
@@ -58,7 +57,7 @@ func Debug(args ...interface{}) (int, error) {
 // Use this to log the state of the application. Dont use Logger.Info in the flow of individual events. Use Logger.Debug instead.
 func Info(args ...interface{}) (int, error) {
 	if levelInfo >= level {
-		fmt.Print(time.Now().Format(time.RFC850), " INFO: ")
+		fmt.Print(" INFO: ")
 		return fmt.Println(args...)
 	}
 	return 0, nil
@@ -68,7 +67,7 @@ func Info(args ...interface{}) (int, error) {
 // Use this to log errors which dont immediately halt the application.
 func Error(args ...interface{}) (int, error) {
 	if levelError >= level {
-		fmt.Print(time.Now().Format(time.RFC850), " ERROR: ")
+		fmt.Print(" ERROR: ")
 		return fmt.Println(args...)
 	}
 	return 0, nil
@@ -78,7 +77,7 @@ func Error(args ...interface{}) (int, error) {
 // Use this to log errors which crash the application.
 func Fatal(args ...interface{}) (int, error) {
 	if levelFatal >= level {
-		fmt.Print(time.Now().Format(time.RFC850), " FATAL: ")
+		fmt.Print(" FATAL: ")
 		return fmt.Println(args...)
 	}
 	return 0, nil
@@ -88,7 +87,7 @@ func Fatal(args ...interface{}) (int, error) {
 // Most verbose logging level
 func Debugf(format string, args ...interface{}) (int, error) {
 	if levelDebug >= level {
-		fmt.Print(time.Now().Format(time.RFC850), " DEBUG: ")
+		fmt.Print(" DEBUG: ")
 		return fmt.Printf(format, args...)
 	}
 	return 0, nil
@@ -98,7 +97,7 @@ func Debugf(format string, args ...interface{}) (int, error) {
 // Use this to log the state of the application. Dont use Logger.Info in the flow of individual events. Use Logger.Debug instead.
 func Infof(format string, args ...interface{}) (int, error) {
 	if levelInfo >= level {
-		fmt.Print(time.Now().Format(time.RFC850), " INFO: ")
+		fmt.Print(" INFO: ")
 		return fmt.Printf(format, args...)
 	}
 	return 0, nil
@@ -108,7 +107,7 @@ func Infof(format string, args ...interface{}) (int, error) {
 // Use this to log errors which dont immediately halt the application.
 func Errorf(format string, args ...interface{}) (int, error) {
 	if levelError >= level {
-		fmt.Print(time.Now().Format(time.RFC850), " ERROR: ")
+		fmt.Print(" ERROR: ")
 		return fmt.Printf(format, args...)
 	}
 	return 0, nil
@@ -118,7 +117,7 @@ func Errorf(format string, args ...interface{}) (int, error) {
 // Use this to log errors which crash the application.
 func Fatalf(format string, args ...interface{}) (int, error) {
 	if levelFatal >= level {
-		fmt.Print(time.Now().Format(time.RFC850), " FATAL: ")
+		fmt.Print(" FATAL: ")
 		return fmt.Printf(format, args...)
 	}
 	return 0, nil
