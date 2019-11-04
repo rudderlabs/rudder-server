47f9b90e58dedd79a27799c9dee0eee2c713d4e4
Srikanth Paladugu <srikanth.iit@gmail.com>
2019-11-04 13:45:31 +0530

    Add build step to appspec

--- scripts/build_rocksdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev
-cd ~
-wget https://github.com/facebook/rocksdb/archive/master.zip
-unzip master.zip && cd rocksdb-master
-make install
