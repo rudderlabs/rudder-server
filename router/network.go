53b845acbdd7ccf1615ea95b8fc2406955782529
prabrisha <prabrisha@rudderlabs.com>
2019-12-09 22:16:37 +0530

    add new processing

--- router/network.go
+++ router/network.go
@@ -107,7 +107,95 @@ func (network *NetHandleT) processOldResponseType(jsonData []byte) (int, string,
 }
 
 func (network *NetHandleT) processNewResponseType(jsonData []byte) (int, string, string) {
-	return 0, "", ""
+	client := network.httpClient
+	//Parse the response to get parameters
+	postInfo := integrations.GetPostInfoNew(jsonData)
+
+	isRest := postInfo.Type == "REST"
+
+	isMultipart := len(postInfo.Files.(map[string]interface{})) > 0
+
+	if isRest && !isMultipart {
+		requestMethod := postInfo.RequestMethod
+		requestBody := postInfo.Body.(map[string]interface{})
+		requestQueryParams := postInfo.QueryParams.(map[string]interface{})
+		var bodyFormat string
+		var bodyValue map[string]interface{}
+		for k, v := range requestBody {
+			bodyFormat = k
+			bodyValue = v.(map[string]interface{})
+		}
+
+		var req *http.Request
+		var err error
+		if useTestSink {
+			req, err = http.NewRequest(requestMethod, testSinkURL, nil)
+			misc.AssertError(err)
+		} else {
+			req, err = http.NewRequest(requestMethod, postInfo.URL, nil)
+			misc.AssertError(err)
+		}
+
+		queryParams := req.URL.Query()
+		for key, val := range requestQueryParams {
+			queryParams.Add(key, fmt.Sprint(val))
+		}
+
+		req.URL.RawQuery = queryParams.Encode()
+
+		switch bodyFormat {
+		case integrations.PostDataJSON:
+			payloadJSON, ok := bodyValue.(map[string]interface{})
+			misc.Assert(ok)
+			jsonValue, err := json.Marshal(payloadJSON)
+			misc.AssertError(err)
+			req.Body = ioutil.NopCloser(bytes.NewReader(jsonValue))
+
+		case integrations.PostDataFORM:
+			payloadFormKV, ok := bodyValue.(map[string]interface{})
+			misc.Assert(ok)
+			formValues := url.Values{}
+			for key, val := range payloadFormKV {
+				fmt.Println(" === key , ==val=== ", key, " ", val)
+				formValues.Set(key, fmt.Sprint(val)) // transformer ensures top level string values, still val.(string) would be restrictive
+			}
+			req.Body = ioutil.NopCloser(strings.NewReader(formValues.Encode()))
+
+		default:
+			misc.Assert(false)
+
+		}
+
+		headerKV, ok := postInfo.Headers.(map[string]interface{})
+		misc.Assert(ok)
+		for key, val := range headerKV {
+			req.Header.Add(key, val.(string))
+		}
+
+		req.Header.Add("User-Agent", "RudderLabs")
+
+		resp, err := client.Do(req)
+
+		var respBody []byte
+
+		if resp != nil && resp.Body != nil {
+			respBody, _ = ioutil.ReadAll(resp.Body)
+			fmt.Println("== resp body=== ", string(respBody))
+			defer resp.Body.Close()
+		}
+
+		if err != nil {
+			logger.Error("Errored when sending request to the server", err)
+			fmt.Println("== resp body=== ", string(respBody))
+			return http.StatusGatewayTimeout, "", string(respBody)
+		}
+
+		return resp.StatusCode, resp.Status, string(respBody)
+
+	}
+
+	return 200, "method not implemented", ""
+
 }
 
 func (network *NetHandleT) sendPost(jsonData []byte) (int, string, string) {
