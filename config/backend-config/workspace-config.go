6bbf97c4b5ec3e3415e446a02968485205ae68dd
lokey <lokeshiitm08@gmail.com>
2020-02-21 12:59:54 +0530

    Adds support for filtering devicemode and native sdks from config

--- config/backend-config/workspace-config.go
+++ config/backend-config/workspace-config.go
@@ -19,12 +19,42 @@ func (workspaceConfig *WorkspaceConfig) GetWorkspaceIDForWriteKey(writeKey strin
 	return ""
 }
 
+func (WorkspaceConfig *WorkspaceConfig) filterOutNativeSDKDestinations(config SourcesT) SourcesT {
+	var modifiedSources SourcesT
+	modifiedSources.Sources = make([]SourceT, 0)
+	for _, source := range config.Sources {
+		destinations := make([]DestinationT, 0)
+		for _, destination := range source.Destinations {
+			isDeviceModeOnly := destination.DestinationDefinition.Config.(map[string]interface{})["deviceModeOnly"]
+			isUsingNativeSDK := destination.Config.(map[string]interface{})["useNativeSDK"]
+			isServerModeDestination := !(isDeviceModeOnly == true || isUsingNativeSDK == true)
+			logger.Debug("Destination Name :", destination.Name, " deviceModeOnly: ", isDeviceModeOnly, " useNativeSDK: ", isUsingNativeSDK, " serverMode: ", isServerModeDestination)
+
+			if isServerModeDestination {
+				destinations = append(destinations, destination)
+			}
+		}
+		source.Destinations = destinations
+		modifiedSources.Sources = append(modifiedSources.Sources, source)
+	}
+	return modifiedSources
+}
+
 //Get returns sources from the workspace
 func (workspaceConfig *WorkspaceConfig) Get() (SourcesT, bool) {
+	var config SourcesT
+	var ok bool
+
 	if configFromFile {
-		return workspaceConfig.getFromFile()
+		config, ok = workspaceConfig.getFromFile()
+	} else {
+		config, ok = workspaceConfig.getFromAPI()
 	}
-	return workspaceConfig.getFromAPI()
+	logger.Debug("Unfiltered complete config ", config)
+	if !ok {
+		return config, false
+	}
+	return workspaceConfig.filterOutNativeSDKDestinations(config), true
 }
 
 // getFromApi gets the workspace config from api
