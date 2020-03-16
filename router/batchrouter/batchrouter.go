014dc0b7a4954d3e33e104797e57e874b367ac3c
Srikanth <srikanth.iit@gmail.com>
2020-03-16 17:13:52 +0530

    Minor changes

--- router/batchrouter/batchrouter.go
+++ router/batchrouter/batchrouter.go
@@ -574,7 +574,6 @@ func loadConfig() {
 	noOfWorkers = config.GetInt("BatchRouter.noOfWorkers", 8)
 	mainLoopSleep = config.GetDuration("BatchRouter.mainLoopSleepInS", 2) * time.Second
 	uploadFreqInS = config.GetInt64("BatchRouter.uploadFreqInS", 30)
-	warehouseStagingFilesTable = config.GetString("Warehouse.stagingFilesTable", "wh_staging_files")
 	// TODO: change this
 	warehouseURL = config.GetEnv("WAREHOUSE_URL", "http://localhost:8082")
 	objectStorageDestinations = []string{"S3", "GCS", "AZURE_BLOB", "MINIO"}
