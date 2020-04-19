3485c6aa07177e8da0344fd96f8cd364838b5f6f
Siva Shanmukh Vetcha <shanmukh@rudderlabs.com>
2020-04-19 12:56:19 +0530

    Migrator - WIP

---
+++ jobsdb/sequence_provider.go
@@ -0,0 +1,29 @@
+package jobsdb
+
+import "sync"
+
+//SequenceProvider maintains a sequenceId increments it based on request atomically
+type SequenceProvider struct {
+	sequenceAssignerLock sync.RWMutex
+	sequenceID           int64
+}
+
+func (sequenceProvider *SequenceProvider) init(startValue int64) {
+	sequenceProvider.sequenceID = startValue
+}
+
+//NewSequenceProvider initializes a sequenceprovider with a start value and returns the provider
+func NewSequenceProvider(startValue int64) SequenceProvider {
+	sequenceProvider := SequenceProvider{}
+	sequenceProvider.init(startValue)
+	return sequenceProvider
+}
+
+//ReserveIds returns a sequence number (seq) and the user can use ids from seq to seq + count -1
+func (sequenceProvider *SequenceProvider) ReserveIds(count int) int64 {
+	sequenceProvider.sequenceAssignerLock.Lock()
+	defer sequenceProvider.sequenceAssignerLock.Unlock()
+	sequenceIDToReturn := sequenceProvider.sequenceID
+	sequenceProvider.sequenceID = sequenceProvider.sequenceID + int64(count)
+	return sequenceIDToReturn
+}
