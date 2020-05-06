30e5113aefcd0db77d20da66bb43001d65c4b5d2
prabrisha-rudder <prabrisha@rudderlabs.com>
2020-05-06 22:06:45 +0530

    added ssl support for kafka

--- services/streammanager/kafka/kafkamanager.go
+++ services/streammanager/kafka/kafkamanager.go
@@ -1,25 +1,72 @@
 package kafka
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 
 	"github.com/Shopify/sarama"
+	"github.com/rudderlabs/rudder-server/config"
+	"github.com/rudderlabs/rudder-server/utils/logger"
 	"github.com/tidwall/gjson"
 )
 
 // Config is the config that is required to send data to Kafka
 type Config struct {
-	Topic    string
-	HostName string
+	Topic         string
+	HostName      string
+	Port          string
+	SslEnabled    bool
+	CACertificate string
 }
 
-func newProducer(hostName string) (sarama.SyncProducer, error) {
+var (
+	clientCertFile, clientKeyFile string
+	certificate                   tls.Certificate
+)
+
+func init() {
+	loadConfig()
+	loadCertificate()
+}
+
+func loadConfig() {
+	clientCertFile = config.GetEnv("SSL_CERTIFICATE_FILE_PATH", "")
+	clientKeyFile = config.GetEnv("SSL_KEY_FILE_PATH", "")
+}
+
+func loadCertificate() {
+	if clientCertFile != "" && clientKeyFile != "" {
+		cert, err := tls.LoadX509KeyPair(clientCertFile, clientKeyFile)
+		if err != nil {
+			logger.Errorf("error loading certificate: ", err)
+			return
+		}
+		certificate = cert
+	}
+}
+
+func newProducer(destConfig Config) (sarama.SyncProducer, error) {
+	hostName := destConfig.HostName + ":" + destConfig.Port
+	isSslEnabled := destConfig.SslEnabled
+
 	hosts := []string{hostName}
+
 	config := sarama.NewConfig()
 	config.Producer.Partitioner = sarama.NewRandomPartitioner
 	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Return.Successes = true
+
+	if isSslEnabled {
+		caCertificate := destConfig.CACertificate
+		tlsConfig := NewTLSConfig(caCertificate)
+		if tlsConfig != nil {
+			config.Net.TLS.Config = tlsConfig
+			config.Net.TLS.Enable = true
+		}
+	}
+
 	producer, err := sarama.NewSyncProducer(hosts, config)
 
 	return producer, err
@@ -32,6 +79,22 @@ func prepareMessage(topic string, message []byte) *sarama.ProducerMessage {
 	}
 
 	return msg
+}
+
+// NewTLSConfig generates a TLS configuration used to authenticate on server withccertificates.
+func NewTLSConfig(caCertFile string) *tls.Config {
+
+	tlsConfig := tls.Config{}
+	if len(certificate.Certificate) > 0 {
+		tlsConfig.Certificates = []tls.Certificate{certificate}
+	}
+	caCert := []byte(caCertFile)
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	tlsConfig.RootCAs = caCertPool
+
+	tlsConfig.BuildNameToCertificate()
+	return &tlsConfig
 }
 
 // Produce creates a producer and send data to Kafka.
@@ -49,15 +112,15 @@ func Produce(jsonData json.RawMessage) (int, string, string) {
 	var config Config
 	json.Unmarshal(configFromJSON, &config)
 
-	fmt.Println("in Kafka produce")
+	logger.Info("in Kafka produce")
 
-	producer, err := newProducer(config.HostName)
+	producer, err := newProducer(config)
 	if err != nil {
-		fmt.Println("Could not create producer: ", err)
+		logger.Errorf("Could not create producer: ", err)
 		return 400, "Could not create producer", err.Error()
 	}
 
-	fmt.Printf("Created Producer %v\n", producer)
+	logger.Infof("Created Producer %v\n", producer)
 
 	topic := config.Topic
 
@@ -73,6 +136,7 @@ func Produce(jsonData json.RawMessage) (int, string, string) {
 		returnMessage = fmt.Sprintf("%s error occured.", err)
 		statusCode = 400
 		errorMessage = err.Error()
+		logger.Error(returnMessage)
 	} else {
 		returnMessage = fmt.Sprintf("Message delivered at Offset: %v , Partition: %v", offset, partition)
 		statusCode = 200
