a23d3476c5f7a3dffdb92e7c200f63e577f7797e
Siva Shanmukh Vetcha <shanmukh@rudderlabs.com>
2020-05-22 12:18:22 +0530

    Starting gateway's webhandler in main

--- main.go
+++ main.go
@@ -217,6 +217,7 @@ func startRudderCore(clearDB *bool, normalMode bool, degradedMode bool, maintena
 		var rateLimiter ratelimiter.HandleT
 		rateLimiter.SetUp()
 		gateway.Setup(backendconfig.DefaultBackendConfig, &gatewayDB, &rateLimiter, stats.DefaultStats, clearDB)
+		gateway.StartWebHandler()
 	}
 	//go readIOforResume(router) //keeping it as input from IO, to be replaced by UI
 }
