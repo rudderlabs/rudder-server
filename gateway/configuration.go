8ffd451ba4922534f036c41d8f4c277af84a7010
Chandra shekar Varkala <chandra@rudderlabs.com>
2020-06-11 15:34:24 +0530

    batch size  values tuned

--- gateway/configuration.go
+++ gateway/configuration.go
@@ -12,13 +12,13 @@ func loadConfig() {
 	//Port where GW is running
 	webPort = config.GetInt("Gateway.webPort", 8080)
 	//Number of incoming requests that are batched before handing off to write workers
-	maxUserWebRequestBatchSize = config.GetInt("Gateway.maxUserRequestBatchSize", 32)
+	maxUserWebRequestBatchSize = config.GetInt("Gateway.maxUserRequestBatchSize", 4)
 	//Number of userWorkerBatchRequest that are batched before initiating write
 	maxDBBatchSize = config.GetInt("Gateway.maxDBBatchSize", 32)
 	//Timeout after which batch is formed anyway with whatever requests
 	//are available
-	userWebRequestBatchTimeout = (config.GetDuration("Gateway.userWebRequestBatchTimeoutInMS", time.Duration(20)) * time.Millisecond)
-	dbBatchWriteTimeout = (config.GetDuration("Gateway.dbBatchWriteTimeoutInMS", time.Duration(20)) * time.Millisecond)
+	userWebRequestBatchTimeout = (config.GetDuration("Gateway.userWebRequestBatchTimeoutInMS", time.Duration(15)) * time.Millisecond)
+	dbBatchWriteTimeout = (config.GetDuration("Gateway.dbBatchWriteTimeoutInMS", time.Duration(5)) * time.Millisecond)
 	//Multiple workers are used to batch user web requests
 	maxUserWebRequestWorkerProcess = config.GetInt("Gateway.maxUserWebRequestWorkerProcess", 64)
 	//Multiple DB writers are used to write data to DB
