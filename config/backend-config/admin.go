bb8e7737b60a44ab071b3fc4d15d0ad3ccd67443
Subbarao Vakati <subbarao2007@gmail.com>
2020-07-07 18:17:19 +0530

    Added admin command in backend-config to fetch routing config

---
+++ config/backend-config/admin.go
@@ -0,0 +1,74 @@
+package backendconfig
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// BackendConfigAdmin is container object to expose admin functions
+type BackendConfigAdmin struct{}
+
+// RoutingConfig reports current backend config and process config after masking secret fields
+func (bca *BackendConfigAdmin) RoutingConfig(filterProcessor bool, reply *string) error {
+	curSourceJSONLock.RLock()
+	defer curSourceJSONLock.RUnlock()
+	outputJSON := curSourceJSON
+	if filterProcessor {
+		outputJSON = filterProcessorEnabledDestinations(outputJSON)
+	}
+
+	outputObj := make([]interface{}, 0)
+
+	for _, source := range outputJSON.Sources {
+		destinations := make([]interface{}, 0)
+		for _, destination := range source.Destinations {
+			destinationConfigCopy := make(map[string]interface{})
+			for k, v := range destination.Config {
+				destinationConfigCopy[k] = v
+			}
+
+			//Mask secret config fields by replacing latter 2/3rd of the field with 'x's
+			destinationSecretKeys, ok := destination.DestinationDefinition.Config["secretKeys"]
+			if !ok {
+				destinationSecretKeys = []string{}
+			}
+			for _, k := range destinationSecretKeys.([]interface{}) {
+				secretKey := k.(string)
+				secret, ok := destinationConfigCopy[secretKey]
+				if ok {
+					switch v := secret.(type) {
+					case string:
+						s := secret.(string)
+						mask := strings.Repeat("x", (len(s)*2)/3)
+						destinationConfigCopy[secretKey] = s[0:len(s)-len(mask)] + mask
+					default:
+						return fmt.Errorf("I don't know how to handle secret config field of type %T", v)
+					}
+				}
+			}
+
+			destinations = append(destinations, map[string]interface{}{
+				"name":              destination.Name,
+				"enabled":           destination.Enabled,
+				"processor-enabled": destination.IsProcessorEnabled,
+				"id":                destination.ID,
+				"config":            destinationConfigCopy,
+				"type":              destination.DestinationDefinition.DisplayName,
+			})
+		}
+		outputObj = append(outputObj, map[string]interface{}{
+			"name":         source.Name,
+			"config":       source.Config,
+			"writekey":     source.WriteKey,
+			"id":           source.ID,
+			"enabled":      source.Enabled,
+			"destinations": destinations,
+			"type":         source.SourceDefinition.Name,
+		})
+	}
+
+	formattedOutput, err := json.MarshalIndent(outputObj, "", "  ")
+	*reply = string(formattedOutput)
+	return err
+}
