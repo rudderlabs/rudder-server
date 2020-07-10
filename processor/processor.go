676a9e6935448ca034972f5487d7be8c39c7aa30
Ruchira <ruchira@rudderlabs.com>
2020-07-10 19:41:33 +0530

    changed logic as transformer is changed for firehose

--- processor/processor.go
+++ processor/processor.go
@@ -224,7 +224,7 @@ func loadConfig() {
 	sessionInactivityThreshold = config.GetDuration("Processor.sessionInactivityThresholdInS", time.Duration(120)) * time.Second
 	configProcessSessions = config.GetBool("Processor.processSessions", false)
 	rawDataDestinations = []string{"S3", "GCS", "MINIO", "RS", "BQ", "AZURE_BLOB", "SNOWFLAKE", "POSTGRES"}
-	customDestinations = []string{"KAFKA", "KINESIS", "AZURE_EVENT_HUB", "FIREHOSE"}
+	customDestinations = []string{"KAFKA", "KINESIS", "AZURE_EVENT_HUB"}
 
 	isReplayServer = config.GetEnvAsBool("IS_REPLAY_SERVER", false)
 }
