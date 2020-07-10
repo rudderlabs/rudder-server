d7e451d4eb2569a89599959df7b30c7a6c11ea28
gane5hvarma <ganesh@ganesh.local>
2020-07-11 00:14:06 +0530

    include batch size

--- warehouse/clickhouse/clickhouse.go
+++ warehouse/clickhouse/clickhouse.go
@@ -88,7 +88,7 @@ var partitionKeyMap = map[string]string{
 }
 
 func connect(cred credentialsT) (*sql.DB, error) {
-	url := fmt.Sprintf("tcp://%s:%s?debug=true&username=%s&password=%s&database=%s&pool_size=1",
+	url := fmt.Sprintf("tcp://%s:%s?debug=true&username=%s&password=%s&database=%s&pool_size=1&block_size=1000",
 		cred.host,
 		cred.port,
 		cred.user,
