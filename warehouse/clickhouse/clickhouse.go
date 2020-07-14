c74c1113cc41e1286c1fc5b61495307478ee8793
gane5hvarma <ganesh@rudderlabs>
2020-07-14 19:06:49 +0530

    performance testing

--- warehouse/clickhouse/clickhouse.go
+++ warehouse/clickhouse/clickhouse.go
@@ -98,14 +98,14 @@ var partitionKeyMap = map[string]string{
 
 // connect connects to warehouse with provided credentials
 func connect(cred credentialsT) (*sql.DB, error) {
-	url := fmt.Sprintf("tcp://%s:%s?&username=%s&password=%s&database=%s&block_size=%s&pool_size=%s&debug=%s",
+	url := fmt.Sprintf("tcp://%s:%s?&username=%s&password=%s&database=%s&block_size=100&pool_size=10&debug=%s",
 		cred.host,
 		cred.port,
 		cred.user,
 		cred.password,
 		cred.dbName,
-		clickhouseLoadBatchSize,
-		clickhouseQueryPoolSize,
+		//clickhouseLoadBatchSize,
+		//clickhouseQueryPoolSize,
 		clickhouseDebugLogs,
 	)
 
