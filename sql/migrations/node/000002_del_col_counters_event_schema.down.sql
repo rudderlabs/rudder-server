d6705ab0900033206e66f4daa0b9c0dc7ac2328f
Sampath <sampathvinayak1453@gmail.com>
2020-08-21 16:00:15 +0530

    api change

--- sql/migrations/node/000002_del_col_counters_event_schema.down.sql
+++ sql/migrations/node/000002_del_col_counters_event_schema.down.sql
@@ -4,3 +4,4 @@
 
 ALTER TABLE schema_versions DROP COLUMN IF EXISTS private_data;
 ALTER TABLE schema_versions DROP CONSTRAINT unique_uuid;
+ALTER TABLE schema_versions DROP COLUMN IF EXISTS total_count;
