2c6a8c45623f83ffe70ab7baeae7e1b86547e43f
srikanth <psrikanth88@users.noreply.github.com>
2020-08-27 13:40:58 +0530

    Error logs for GZWriter (#513)
    * Error logs for GZWriter
    
    * additional error log
    
    * comment customer.io throttle limits

--- router/throttler/config.go
+++ router/throttler/config.go
@@ -6,12 +6,14 @@ var (
 
 func init() {
 	destSettingsMap = map[string]Settings{
+		// TODO: Revisit Customer.io limits as these seem to be soft limits
+		// and not automatically enforced by customer.io
 		// https://customer.io/docs/api/#api-documentationlimits
-		"CUSTOMERIO": {
-			limit:               30,
-			timeWindowInS:       1,
-			userLevelThrottling: false,
-		},
+		// "CUSTOMERIO": {
+		// 	limit:               30,
+		// 	timeWindowInS:       1,
+		// 	userLevelThrottling: false,
+		// },
 		// https://help.amplitude.com/hc/en-us/articles/360032842391-HTTP-API-V2#upload-limit
 		"AM": {
 			limit:                  1000,
