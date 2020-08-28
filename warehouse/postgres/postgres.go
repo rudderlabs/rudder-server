b66757aa888765f5fa5d4d18030111338dafc301
gane5hvarma <ganesh@rudderlabs>
2020-08-28 16:16:09 +0530

    change few logs level to debug from info

--- warehouse/postgres/postgres.go
+++ warehouse/postgres/postgres.go
@@ -292,7 +292,7 @@ func (pg *HandleT) loadTable(tableName string, columnMap map[string]string, skip
 	// create temporary table
 	stagingTableName = fmt.Sprintf(`%s%s_%s`, stagingTablePrefix, tableName, strings.Replace(uuid.NewV4().String(), "-", "", -1))
 	sqlStatement := fmt.Sprintf(`CREATE TEMPORARY TABLE "%[2]s" (LIKE "%[1]s"."%[3]s")`, pg.Namespace, stagingTableName, tableName)
-	logger.Infof("PG: Creating temporary table for table:%s at %s\n", tableName, sqlStatement)
+	logger.Debugf("PG: Creating temporary table for table:%s at %s\n", tableName, sqlStatement)
 	_, err = txn.Exec(sqlStatement)
 	if err != nil {
 		logger.Errorf("PG: Error creating temporary table for table:%s: %v\n", tableName, err)
