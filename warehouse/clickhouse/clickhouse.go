de361247775e63d2d28b81e33c3775cbcd4b697b
gane5hvarma <ganesh@rudderlabs>
2020-09-02 01:39:26 +0530

    fix datetime empty bug

--- warehouse/clickhouse/clickhouse.go
+++ warehouse/clickhouse/clickhouse.go
@@ -355,7 +355,7 @@ func typeCastDataFromType(data string, datatype string) interface{} {
 	case "datetime":
 		t, err := time.Parse(time.RFC3339, data)
 		if err != nil {
-			return ""
+			return nil
 		}
 		return t
 	case "boolean":
