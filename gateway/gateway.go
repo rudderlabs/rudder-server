0c87305278922e1f512ab646d8befa9da3b5778c
Chandra shekar Varkala <chandra@rudderlabs.com>
2020-09-01 12:27:16 +0530

    remove code blocking gateway db writes

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -684,12 +684,12 @@ func (gateway *HandleT) pixelTrackHandler(w http.ResponseWriter, r *http.Request
 func (gateway *HandleT) webHandler(w http.ResponseWriter, r *http.Request, reqType string) {
 	logger.LogRequest(r)
 	atomic.AddUint64(&gateway.recvCount, 1)
-	//done := make(chan string, 1)
-	//gateway.AddToWebRequestQ(r, &w, done, reqType)
+	done := make(chan string, 1)
+	gateway.AddToWebRequestQ(r, &w, done, reqType)
 
 	//Wait for batcher process to be done
-	//errorMessage := <-done
-	errorMessage := ""
+	errorMessage := <-done
+	//errorMessage := ""
 	atomic.AddUint64(&gateway.ackCount, 1)
 	gateway.trackRequestMetrics(errorMessage)
 	if errorMessage != "" {
