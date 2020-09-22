4ec99b9edeb5baad48a641f867edb20a94068ec7
Chandra shekar Varkala <chandra@rudderlabs.com>
2020-09-23 00:10:45 +0530

    pprof endpoint added

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"os/signal"
 	"runtime"
@@ -349,5 +350,18 @@ func main() {
 
 	rruntime.Go(admin.StartServer)
 
-	misc.KeepProcessAlive()
+	//misc.KeepProcessAlive()
+	r := http.NewServeMux()
+	r.HandleFunc("/", hiHandler)
+	// Register pprof handlers
+	r.HandleFunc("/debug/pprof/", pprof.Index)
+	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	http.ListenAndServe(":8083", r)
+}
+
+func hiHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hi"))
 }
