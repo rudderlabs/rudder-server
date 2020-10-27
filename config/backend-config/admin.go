54d20cd17e91e55e8c84f624062ffd24cd7bbfc3
Chandra shekar Varkala <chandra@rudderlabs.com>
2020-10-27 17:51:10 +0530

    type check added

--- config/backend-config/admin.go
+++ config/backend-config/admin.go
@@ -3,6 +3,7 @@ package backendconfig
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -34,6 +35,15 @@ func (bca *BackendConfigAdmin) RoutingConfig(filterProcessor bool, reply *string
 			if !ok {
 				destinationSecretKeys = []interface{}{}
 			}
+
+			rt := reflect.TypeOf(destinationSecretKeys)
+			switch rt.Kind() {
+			case reflect.Array:
+			case reflect.Slice:
+			default:
+				return fmt.Errorf("secretKeys field of destination definition config is not an array. Destination definition name: %s", destination.DestinationDefinition.DisplayName)
+			}
+
 			for _, k := range destinationSecretKeys.([]interface{}) {
 				secretKey := k.(string)
 				secret, ok := destinationConfigCopy[secretKey]
