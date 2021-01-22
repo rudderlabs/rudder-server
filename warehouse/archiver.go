b549862a2295dd1c914331352e04e1290dfe06b9
srikanth <srikanth.iit@gmail.com>
2021-01-23 02:59:13 +0530

    Add different archival times for staging and load files

--- warehouse/archiver.go
+++ warehouse/archiver.go
@@ -10,26 +10,28 @@ import (
 )
 
 var (
-	archiveLoadFiles    bool
-	archiveStagingFiles bool
-	archivalTimeInDays  int
-	archiverTickerTime  time.Duration
+	archiveLoadFiles               bool
+	archiveStagingFiles            bool
+	stagingFilesArchivalTimeInDays int
+	loadFilesArchivalTimeInDays    int
+	archiverTickerTime             time.Duration
 )
 
 func init() {
 	archiveLoadFiles = config.GetBool("Warehouse.archiveLoadFiles", true)
 	archiveStagingFiles = config.GetBool("Warehouse.archiveStagingFiles", true)
-	archivalTimeInDays = config.GetInt("Warehouse.archivalTimeInDays", 45)
+	stagingFilesArchivalTimeInDays = config.GetInt("Warehouse.stagingFilesArchivalTimeInDays", 45)
+	loadFilesArchivalTimeInDays = config.GetInt("Warehouse.loadFilesArchivalTimeInDays", 15)
 	archiverTickerTime = config.GetDuration("Warehouse.archiverTickerTimeInMin", 1440) * time.Minute // default 1 day
 }
 
 func runArchiver(dbHandle *sql.DB) {
 	for {
 		if archiveLoadFiles {
-			archiver.ArchiveOldRecords(warehouseutils.WarehouseLoadFilesTable, "created_at", archivalTimeInDays, dbHandle)
+			archiver.ArchiveOldRecords(warehouseutils.WarehouseLoadFilesTable, "created_at", loadFilesArchivalTimeInDays, dbHandle)
 		}
 		if archiveStagingFiles {
-			archiver.ArchiveOldRecords(warehouseutils.WarehouseStagingFilesTable, "created_at", archivalTimeInDays, dbHandle)
+			archiver.ArchiveOldRecords(warehouseutils.WarehouseStagingFilesTable, "created_at", stagingFilesArchivalTimeInDays, dbHandle)
 		}
 		time.Sleep(archiverTickerTime)
 	}
