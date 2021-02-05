13385522a1b0786ab0f5951b036966cb4ac1a297
Chandra shekar Varkala <chandra@rudderlabs.com>
2021-02-05 15:37:33 +0530

    pending jobs endpoint

--- router/router.go
+++ router/router.go
@@ -603,7 +603,7 @@ func (worker *workerT) postStatusOnResponseQ(respStatusCode int, respBody string
 
 		worker.rt.failedEventsChan <- *status
 
-		if respStatusCode >= 500 || respStatusCode == 429 {
+		if respStatusCode >= 500 {
 			timeElapsed := time.Since(firstAttemptedAtTime)
 			if timeElapsed > worker.rt.retryTimeWindow && status.AttemptNum >= worker.rt.maxFailedCountForJob {
 				status.JobState = jobsdb.Aborted.State
@@ -617,6 +617,10 @@ func (worker *workerT) postStatusOnResponseQ(respStatusCode int, respBody string
 				worker.retryForJobMap[destinationJobMetadata.JobID] = time.Now().Add(durationBeforeNextAttempt(status.AttemptNum))
 				worker.retryForJobMapMutex.Unlock()
 			}
+		} else if respStatusCode == 429 {
+			worker.retryForJobMapMutex.Lock()
+			worker.retryForJobMap[destinationJobMetadata.JobID] = time.Now().Add(durationBeforeNextAttempt(status.AttemptNum))
+			worker.retryForJobMapMutex.Unlock()
 		} else {
 			status.JobState = jobsdb.Aborted.State
 			addToFailedMap = false
