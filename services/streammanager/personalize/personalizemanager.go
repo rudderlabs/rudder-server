9d8634029837ee43682cc8ae74301260a8427bbe
shrouti <shrouti.gangopadhyay@gmail.com>
2021-03-11 11:47:16 +0530

    PutEvents_transformer_code_unmarshalled_directly

--- services/streammanager/personalize/personalizemanager.go
+++ services/streammanager/personalize/personalizemanager.go
@@ -3,7 +3,6 @@ package personalize
 import (
 	"encoding/json"
 	"fmt"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -11,7 +10,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/personalizeevents"
 	"github.com/rudderlabs/rudder-server/utils/logger"
-	"github.com/tidwall/gjson"
 )
 
 type Config struct {
@@ -27,7 +25,6 @@ func init() {
 }
 func NewProducer(destinationConfig interface{}) (personalizeevents.PersonalizeEvents, error) {
 	var config Config
-	fmt.Printf("HERE IN NEW PRODUCER")
 	jsonConfig, err := json.Marshal(destinationConfig) // produces json
 	if err != nil {
 		return personalizeevents.PersonalizeEvents{}, fmt.Errorf("[Personalize] Error while marshalling destination config :: %w", err)
@@ -36,13 +33,8 @@ func NewProducer(destinationConfig interface{}) (personalizeevents.PersonalizeEv
 	if err != nil {
 		return personalizeevents.PersonalizeEvents{}, fmt.Errorf("[Personalize] Error while unmarshalling destination config :: %w", err)
 	}
-	fmt.Printf("*************************")
-	fmt.Printf(config.AccessKeyID)
-	fmt.Printf("#########################")
-	fmt.Printf(config.SecretAccessKey)
 	var s *session.Session
 	if config.AccessKeyID == "" || config.SecretAccessKey == "" {
-		fmt.Printf("HERE IN IF PORTION")
 		s = session.Must(session.NewSession(&aws.Config{
 			Region: aws.String(config.Region),
 		}))
@@ -54,91 +46,18 @@ func NewProducer(destinationConfig interface{}) (personalizeevents.PersonalizeEv
 	var client *personalizeevents.PersonalizeEvents = personalizeevents.New(s)
 	return *client, nil
 }
-func Produce(jsonData json.RawMessage, producer interface{}, destConfig interface{}) (int, string, string) {
-	fmt.Printf("****** HERE IN SERVER **********")
-	// fmt.Printf(string(jsonData))
-	// get producer
+func Produce(jsonData json.RawMessage, producer interface{}, destConfig interface{}) (statusCode int, respStatus string, responseMessag string) {
+
 	client, ok := producer.(personalizeevents.PersonalizeEvents)
 	if (!ok || client == personalizeevents.PersonalizeEvents{}) {
 		// return 400 if producer is invalid
 		return 400, "Could not create producer for Personalize", "Could not create producer for Personalize"
 	}
 	input := personalizeevents.PutEventsInput{}
-	dataJson := gjson.ParseBytes(jsonData)
-	if dataJson.Get("userId").Value() != nil {
-		fmt.Printf("*****printing UserId******")
-		userId := dataJson.Get("userId").Value().(string)
-		fmt.Printf(userId)
-		input.SetUserId(userId)
-	} else {
-		respStatus := "Failure"
-		responseMessage := "[Personalize] error :: userId from payload not found"
-		return 400, respStatus, responseMessage
-	}
-	if dataJson.Get("sessionId").Value() != nil {
-		fmt.Printf("*****Printing sessionId******")
-		timeStamp := dataJson.Get("sessionId").Value().(string)
-		fmt.Printf(timeStamp)
-		input.SetSessionId(timeStamp)
-	} else {
-		respStatus := "Failure"
-		responseMessage := "[Personalize] error :: sessionId from payload not found"
-		return 400, respStatus, responseMessage
-	}
-	if dataJson.Get("trackingId").Value() != nil {
-		fmt.Printf("*****Printing trackingId******")
-		trackingId := dataJson.Get("trackingId").Value().(string)
-		input.SetTrackingId(trackingId)
-	} else {
-		respStatus := "Failure"
-		responseMessage := "[Personalize] error :: trackingId from payload not found"
-		return 400, respStatus, responseMessage
-	}
-	event := personalizeevents.Event{}
-	if dataJson.Get("eventId").Value() != nil {
-		fmt.Printf("*****Printing eventId******")
-		eventId := dataJson.Get("eventId").Value().(string)
-		event.SetEventId(eventId)
-	}
-	if dataJson.Get("eventType").Value() != nil {
-		fmt.Printf("*****Printing eventType******")
-		eventType := dataJson.Get("eventType").Value().(string)
-		event.SetEventType(eventType)
-	} else {
-		respStatus := "Failure"
-		responseMessage := "[Personalize] error :: eventType from payload not found"
-		return 400, respStatus, responseMessage
-	}
-	if dataJson.Get("eventValue").Value() != nil {
-		fmt.Printf("*****Printing eventValue******")
-		eventValue := dataJson.Get("eventValue").Value().(float64)
-		event.SetEventValue(eventValue)
-	}
-	event.SetSentAt(time.Now())
-	if dataJson.Get("properties").Value() != nil {
-		fmt.Printf("*****Printing Property******")
-		property := dataJson.Get("eventType").Value().(string)
-		bytes := []byte(property)
-		properties := aws.JSONValue{}
-		json.Unmarshal(bytes, &properties)
-		event.SetProperties(properties)
-	}
-	if dataJson.Get("itemId").Value() != nil {
-		fmt.Printf("*****Printing itemId******")
-		itemId := dataJson.Get("itemId").Value().(string)
-		event.SetItemId(itemId)
-	}
-	if dataJson.Get("recommendationId").Value() != nil {
-		fmt.Printf("*****Printing recommId******")
-		recommendationId := dataJson.Get("recommendationId").Value().(string)
-		event.SetRecommendationId(recommendationId)
-	}
-	eventList := make([]*personalizeevents.Event, 0)
-	eventList = append(eventList, &event)
-	input.SetEventList(eventList)
-	_, err := client.PutEvents(&input)
+	bytes := []byte(jsonData)
+	json.Unmarshal(bytes, &input)
+	res, err := client.PutEvents(&input)
 	if err != nil {
-		fmt.Printf("*****put events error*****")
 		pkgLogger.Errorf("Personalize Error while sending event :: %w", err)
 		// set default status code as 500
 		statusCode := 500
@@ -148,5 +67,13 @@ func Produce(jsonData json.RawMessage, producer interface{}, destConfig interfac
 		}
 		return statusCode, err.Error(), err.Error()
 	}
-	return 200, "success", ""
+	if res != nil {
+		responseMessage := fmt.Sprintf("Message delivered with Record information %v", res)
+		respStatus = "Success"
+		return 200, respStatus, responseMessage
+	} else {
+		respStatus = "Failure"
+		responseMessage := "[Personalize] error  :: Delivery Stream not found"
+		return 400, respStatus, responseMessage
+	}
 }
