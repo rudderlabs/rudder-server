9832219f5449542d5daf0ed281860ff05c43988c
Rohith BCS <rohith.bcs@gmail.com>
2021-04-17 21:27:55 +0530

    Changing build spec

--- config/config.go
+++ config/config.go
@@ -64,7 +64,7 @@ func init() {
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig() // Find and read the config file
 	UpdateConfig()
-	// Don't panic if config.toml is not found or error with parsing. Use the default config values instead
+	// Don't panic if config.yaml is not found or error with parsing. Use the default config values instead
 	if err != nil {
 		fmt.Println("[Config] :: Failed to parse Config Yaml, using default values:", err)
 	}
