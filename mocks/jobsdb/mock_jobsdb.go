9e1a96783ca7fc2414cae9e595f1a5f5dcc2909f
Chandra shekar Varkala <chandra@rudderlabs.com>
2021-05-03 12:19:19 +0530

    mocks revert

--- mocks/jobsdb/mock_jobsdb.go
+++ mocks/jobsdb/mock_jobsdb.go
@@ -6,32 +6,31 @@ package mocks_jobsdb
 
 import (
 	sql "database/sql"
-	reflect "reflect"
-
 	gomock "github.com/golang/mock/gomock"
 	jobsdb "github.com/rudderlabs/rudder-server/jobsdb"
 	uuid "github.com/satori/go.uuid"
+	reflect "reflect"
 )
 
-// MockJobsDB is a mock of JobsDB interface.
+// MockJobsDB is a mock of JobsDB interface
 type MockJobsDB struct {
 	ctrl     *gomock.Controller
 	recorder *MockJobsDBMockRecorder
 }
 
-// MockJobsDBMockRecorder is the mock recorder for MockJobsDB.
+// MockJobsDBMockRecorder is the mock recorder for MockJobsDB
 type MockJobsDBMockRecorder struct {
 	mock *MockJobsDB
 }
 
-// NewMockJobsDB creates a new mock instance.
+// NewMockJobsDB creates a new mock instance
 func NewMockJobsDB(ctrl *gomock.Controller) *MockJobsDB {
 	mock := &MockJobsDB{ctrl: ctrl}
 	mock.recorder = &MockJobsDBMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use.
+// EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockJobsDB) EXPECT() *MockJobsDBMockRecorder {
 	return m.recorder
 }
@@ -82,7 +81,7 @@ func (m *MockJobsDB) CheckPGHealth() bool {
 	return ret0
 }
 
-// CheckPGHealth indicates an expected call of CheckPGHealth.
+// CheckPGHealth indicates an expected call of CheckPGHealth
 func (mr *MockJobsDBMockRecorder) CheckPGHealth() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CheckPGHealth", reflect.TypeOf((*MockJobsDB)(nil).CheckPGHealth))
@@ -108,13 +107,13 @@ func (m *MockJobsDB) GetExecuting(arg0 jobsdb.GetQueryParamsT) []*jobsdb.JobT {
 	return ret0
 }
 
-// GetExecuting indicates an expected call of GetExecuting.
+// GetExecuting indicates an expected call of GetExecuting
 func (mr *MockJobsDBMockRecorder) GetExecuting(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetExecuting", reflect.TypeOf((*MockJobsDB)(nil).GetExecuting), arg0)
 }
 
-// GetToRetry mocks base method.
+// GetToRetry mocks base method
 func (m *MockJobsDB) GetToRetry(arg0 jobsdb.GetQueryParamsT) []*jobsdb.JobT {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetToRetry", arg0)
@@ -122,13 +121,13 @@ func (m *MockJobsDB) GetToRetry(arg0 jobsdb.GetQueryParamsT) []*jobsdb.JobT {
 	return ret0
 }
 
-// GetToRetry indicates an expected call of GetToRetry.
+// GetToRetry indicates an expected call of GetToRetry
 func (mr *MockJobsDBMockRecorder) GetToRetry(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetToRetry", reflect.TypeOf((*MockJobsDB)(nil).GetToRetry), arg0)
 }
 
-// GetUnprocessed mocks base method.
+// GetUnprocessed mocks base method
 func (m *MockJobsDB) GetUnprocessed(arg0 jobsdb.GetQueryParamsT) []*jobsdb.JobT {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetUnprocessed", arg0)
@@ -136,7 +135,7 @@ func (m *MockJobsDB) GetUnprocessed(arg0 jobsdb.GetQueryParamsT) []*jobsdb.JobT 
 	return ret0
 }
 
-// GetUnprocessed indicates an expected call of GetUnprocessed.
+// GetUnprocessed indicates an expected call of GetUnprocessed
 func (mr *MockJobsDBMockRecorder) GetUnprocessed(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetUnprocessed", reflect.TypeOf((*MockJobsDB)(nil).GetUnprocessed), arg0)
@@ -174,7 +173,7 @@ func (m *MockJobsDB) Status() interface{} {
 	return ret0
 }
 
-// Status indicates an expected call of Status.
+// Status indicates an expected call of Status
 func (mr *MockJobsDBMockRecorder) Status() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Status", reflect.TypeOf((*MockJobsDB)(nil).Status))
@@ -188,7 +187,7 @@ func (m *MockJobsDB) Store(arg0 []*jobsdb.JobT) error {
 	return ret0
 }
 
-// Store indicates an expected call of Store.
+// Store indicates an expected call of Store
 func (mr *MockJobsDBMockRecorder) Store(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Store", reflect.TypeOf((*MockJobsDB)(nil).Store), arg0)
@@ -216,13 +215,13 @@ func (m *MockJobsDB) StoreWithRetryEach(arg0 []*jobsdb.JobT) map[uuid.UUID]strin
 	return ret0
 }
 
-// StoreWithRetryEach indicates an expected call of StoreWithRetryEach.
+// StoreWithRetryEach indicates an expected call of StoreWithRetryEach
 func (mr *MockJobsDBMockRecorder) StoreWithRetryEach(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "StoreWithRetryEach", reflect.TypeOf((*MockJobsDB)(nil).StoreWithRetryEach), arg0)
 }
 
-// UpdateJobStatus mocks base method.
+// UpdateJobStatus mocks base method
 func (m *MockJobsDB) UpdateJobStatus(arg0 []*jobsdb.JobStatusT, arg1 []string, arg2 []jobsdb.ParameterFilterT) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "UpdateJobStatus", arg0, arg1, arg2)
@@ -230,7 +229,7 @@ func (m *MockJobsDB) UpdateJobStatus(arg0 []*jobsdb.JobStatusT, arg1 []string, a
 	return ret0
 }
 
-// UpdateJobStatus indicates an expected call of UpdateJobStatus.
+// UpdateJobStatus indicates an expected call of UpdateJobStatus
 func (mr *MockJobsDBMockRecorder) UpdateJobStatus(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateJobStatus", reflect.TypeOf((*MockJobsDB)(nil).UpdateJobStatus), arg0, arg1, arg2)
