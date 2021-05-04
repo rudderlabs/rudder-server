c4b574d73d5392091b285bf4a4de11767d19d2e2
Utsab Chowdhury <utsab.c97@gmail.com>
2021-05-04 19:15:54 +0530

    google sheets v2 server changes

--- services/streammanager/googlesheets/googlesheetsmanager.go
+++ services/streammanager/googlesheets/googlesheetsmanager.go
@@ -18,8 +18,10 @@ import (
 )
 
 type Config struct {
-	Credentials     string              `json:"credentials"`
-	EventToSheetMap []map[string]string `json:"eventToSpreadSheetIdMap"`
+	Credentials   string              `json:"credentials"`
+	SheetId       string              `json:"sheetId"`
+	EventSheetMap []map[string]string `json:"eventSheetMapping"`
+	EventKeyMap   []map[string]string `json:"eventKeyMap"`
 }
 
 type Credentials struct {
@@ -76,6 +78,30 @@ func NewProducer(destinationConfig interface{}) (*GoogleAPIService, error) {
 	googleAPIService.Jwt = jwtconfig
 	service, err := generateServiceWithRefreshToken(*jwtconfig)
 	googleAPIService.Service = service
+
+	// Storing All the Spread-Sheets (Pages withing a single Google-sheets) where the events are
+	// required to be mapped
+	// Example: | Sheet1 | Sheet2 | Sheet3 | ..
+	var sheets []string
+	for _, eventtosheetmap := range config.EventSheetMap {
+		sheets = append(sheets, eventtosheetmap["to"])
+	}
+
+	// Creating the array of string which are then coverted in to an array of interface which are to
+	// be added as header to each of the above spreadsheets.
+	// Example: | First Name | Last Name | Birth Day | Item Purchased | ..
+	var headerRowStr []string
+	for _, eventmap := range config.EventKeyMap {
+		headerRowStr = append(headerRowStr, eventmap["to"])
+	}
+	headerRow := getSheetsData(headerRowStr)
+
+	// *** Adding the header ***
+	// Iterating each sheets we adding the header for them if any error occurs here we throw it causing an error in creation of destination, else if all operation occurs successfuly we complete the NewProducer processes successfully
+	for _, sheet := range sheets {
+		// Inserting header to the sheet
+		err = insertDataToSheet(config.SheetId, sheet, headerRow, true)
+	}
 	return &googleAPIService, err
 }
 
@@ -83,14 +109,14 @@ func Produce(jsonData json.RawMessage, producer interface{}, destConfig interfac
 
 	// Here we recieve the transformed json with all the required mappings
 	parsedJSON := gjson.ParseBytes(jsonData)
-	// Storing sheet_id and sheet_tab for the specific message
+	// Storing sheet_id and sheet_name for the specific message
 	spreadSheetId := parsedJSON.Get("spreadSheetId").String()
-	spreadSheetTab := parsedJSON.Get("spreadSheetTab").String()
+	spreadSheet := parsedJSON.Get("spreadSheet").String()
 
-	// Here we extract the keys, values of the transformed json into two string arrays
+	// Here we extract the values of the transformed json into a string array
 	// because google sheets-api works with array of data if any error occurs here we
 	// log it as parse error for that event
-	keys, values, parseErr := parseTransformedData(parsedJSON)
+	values, parseErr := parseTransformedData(parsedJSON)
 
 	if parseErr != nil {
 		respStatus = "Failure"
@@ -99,26 +125,13 @@ func Produce(jsonData json.RawMessage, producer interface{}, destConfig interfac
 
 	}
 
-	// *** Adding the header ***
-	// Converting the string array of keys to an array of interface compatibale with sheets-api (Keys relates to sheet header)
-	headerRow := getSheetsData(keys)
-
-	// Inserting header to the sheet
-	err := insertDataToSheet(spreadSheetId, spreadSheetTab, headerRow, true)
-	if err != nil {
-		statCode, serviceMessage := handleServiceError(err)
-		respStatus = "Failure"
-		responseMessage = "[GoogleSheets] error :: Failed to insert Header :: " + serviceMessage
-		return statCode, respStatus, responseMessage
-	}
+	message := getSheetsData(values)
 
 	// *** Appending actual Rudder event as Row ***
 	// In this section we are appending the actual messages into the sheet
-	// After parsing the parsedJSON as a row specific data which we are storing in message
-	message := getSheetsData(values)
-
-	// Inserting event (message) to the sheet
-	err = insertDataToSheet(spreadSheetId, spreadSheetTab, message, false)
+	// After parsing the parsedJSON as a row specific data which we are storing in message,
+	// and appending event (message) to the sheet
+	err := insertDataToSheet(spreadSheetId, spreadSheet, message, false)
 	if err != nil {
 		statCode, serviceMessage := handleServiceError(err)
 		respStatus = "Failure"
@@ -129,60 +142,6 @@ func Produce(jsonData json.RawMessage, producer interface{}, destConfig interfac
 	respStatus = "Success"
 	responseMessage = "[GoogleSheets] :: Message Payload inserted with messageId :: " + parsedJSON.Get("id").String()
 	return 200, respStatus, responseMessage
-}
-
-// This method is created for fail safety, if in any case when err type is not googleapi.Error
-// we should not crash with a type error.
-func handleServiceError(err error) (statusCode int, responseMessage string) {
-	statusCode = 400
-	responseMessage = err.Error()
-	if reflect.TypeOf(err).String() == "*googleapi.Error" {
-		serviceErr := err.(*googleapi.Error)
-		statusCode = serviceErr.Code
-		responseMessage = serviceErr.Message
-	}
-	return statusCode, responseMessage
-}
-
-// Method to return array of keys, values from a json.
-// source is the json object from transformer and we are iterating the json as a map
-// and we are storing the data into designated position in array based on transformer
-// mappings.
-// Example payload we have from transformer:
-// { message:
-// 	 { 	'0': { key: 'anonymous_id', value: '' },
-// 	 	'1': { key: 'user_id', value: 'userTest004' },
-// 	 	'2': { key: 'event', value: 'Page Call' },
-// 	 	..}
-// }
-func parseTransformedData(source gjson.Result) ([]string, []string, error) {
-	//var sourcemap map[string]gjson.Result
-	messagefields := source.Get("message")
-	keys := make([]string, len(messagefields.Map()))
-	values := make([]string, len(messagefields.Map()))
-	var pos int
-	var err error
-	if messagefields.IsObject() {
-		for k, v := range messagefields.Map() {
-			pos, err = strconv.Atoi(k)
-			if err != nil {
-				return keys, values, err
-			}
-			keys[pos] = v.Get("key").String()
-			values[pos] = v.Get("value").String()
-		}
-	}
-	return keys, values, err
-}
-
-// Func used to parse a string array to an interface array for compatibility
-// with sheets-api
-func getSheetsData(typedata []string) []interface{} {
-	data := make([]interface{}, len(typedata))
-	for key, value := range typedata {
-		data[key] = value
-	}
-	return data
 }
 
 // This method produces a google-sheets client from a jwt.Config client by
@@ -241,4 +200,57 @@ func insertDataToSheet(spreadSheetId string, spreadSheetTab string, data []inter
 		return err
 	}
 	return nil
+}
+
+// Method to return array of values from a json.
+// source is the json object from transformer and we are iterating the json as a map
+// and we are storing the data into designated position in array based on transformer
+// mappings.
+// Example payload we have from transformer:
+// {
+//		message:{
+//			1: { attributeKey: "Product Purchased", attributeValue: "Realme C3" }
+//			2: { attributeKey: "Product Value, attributeValue: "5900"}
+//			..
+// 		}
+// }
+func parseTransformedData(source gjson.Result) ([]string, error) {
+	//var sourcemap map[string]gjson.Result
+	messagefields := source.Get("message")
+	values := make([]string, len(messagefields.Map()))
+	var pos int
+	var err error
+	if messagefields.IsObject() {
+		for k, v := range messagefields.Map() {
+			pos, err = strconv.Atoi(k)
+			if err != nil {
+				return values, err
+			}
+			values[pos] = v.Get("attributeValue").String()
+		}
+	}
+	return values, err
+}
+
+// Func used to parse a string array to an interface array for compatibility
+// with sheets-api
+func getSheetsData(typedata []string) []interface{} {
+	data := make([]interface{}, len(typedata))
+	for key, value := range typedata {
+		data[key] = value
+	}
+	return data
+}
+
+// This method is created for fail safety, if in any case when err type is not googleapi.Error
+// we should not crash with a type error.
+func handleServiceError(err error) (statusCode int, responseMessage string) {
+	statusCode = 500
+	responseMessage = err.Error()
+	if reflect.TypeOf(err).String() == "*googleapi.Error" {
+		serviceErr := err.(*googleapi.Error)
+		statusCode = serviceErr.Code
+		responseMessage = serviceErr.Message
+	}
+	return statusCode, responseMessage
 }
