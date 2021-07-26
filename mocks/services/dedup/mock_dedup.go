ad4b480a6340d18faadd9176755f2889dee8e855
Sidddddarth <82795818+Sidddddarth@users.noreply.github.com>
2021-07-26 19:02:03 +0530

    clearStaleKeys (#1136)
    * clearStaleKeys

--- mocks/services/dedup/mock_dedup.go
+++ mocks/services/dedup/mock_dedup.go
@@ -5,35 +5,34 @@
 package mock_dedup
 
 import (
-	reflect "reflect"
-
 	gomock "github.com/golang/mock/gomock"
+	reflect "reflect"
 )
 
-// MockDedupI is a mock of DedupI interface.
+// MockDedupI is a mock of DedupI interface
 type MockDedupI struct {
 	ctrl     *gomock.Controller
 	recorder *MockDedupIMockRecorder
 }
 
-// MockDedupIMockRecorder is the mock recorder for MockDedupI.
+// MockDedupIMockRecorder is the mock recorder for MockDedupI
 type MockDedupIMockRecorder struct {
 	mock *MockDedupI
 }
 
-// NewMockDedupI creates a new mock instance.
+// NewMockDedupI creates a new mock instance
 func NewMockDedupI(ctrl *gomock.Controller) *MockDedupI {
 	mock := &MockDedupI{ctrl: ctrl}
 	mock.recorder = &MockDedupIMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use.
+// EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockDedupI) EXPECT() *MockDedupIMockRecorder {
 	return m.recorder
 }
 
-// FindDuplicates mocks base method.
+// FindDuplicates mocks base method
 func (m *MockDedupI) FindDuplicates(arg0 []string, arg1 map[string]struct{}) []int {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "FindDuplicates", arg0, arg1)
@@ -41,31 +40,31 @@ func (m *MockDedupI) FindDuplicates(arg0 []string, arg1 map[string]struct{}) []i
 	return ret0
 }
 
-// FindDuplicates indicates an expected call of FindDuplicates.
+// FindDuplicates indicates an expected call of FindDuplicates
 func (mr *MockDedupIMockRecorder) FindDuplicates(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindDuplicates", reflect.TypeOf((*MockDedupI)(nil).FindDuplicates), arg0, arg1)
 }
 
-// MarkProcessed mocks base method.
+// MarkProcessed mocks base method
 func (m *MockDedupI) MarkProcessed(arg0 []string) {
 	m.ctrl.T.Helper()
 	m.ctrl.Call(m, "MarkProcessed", arg0)
 }
 
-// MarkProcessed indicates an expected call of MarkProcessed.
+// MarkProcessed indicates an expected call of MarkProcessed
 func (mr *MockDedupIMockRecorder) MarkProcessed(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "MarkProcessed", reflect.TypeOf((*MockDedupI)(nil).MarkProcessed), arg0)
 }
 
-// PrintHistogram mocks base method.
+// PrintHistogram mocks base method
 func (m *MockDedupI) PrintHistogram() {
 	m.ctrl.T.Helper()
 	m.ctrl.Call(m, "PrintHistogram")
 }
 
-// PrintHistogram indicates an expected call of PrintHistogram.
+// PrintHistogram indicates an expected call of PrintHistogram
 func (mr *MockDedupIMockRecorder) PrintHistogram() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PrintHistogram", reflect.TypeOf((*MockDedupI)(nil).PrintHistogram))
