e35f6bb0bab2dbe50c1da0fb2e571c5ce94d8e49
siddarth <siddarth@rudderstack.com>
2021-08-06 18:18:54 +0530

    procerror_name_typeFix

--- processor/processor.go
+++ processor/processor.go
@@ -631,8 +631,8 @@ func recordEventDeliveryStatus(jobsByDestID map[string][]*jobsdb.JobT) {
 			procErr, _ := params["error"].(string)
 			procErr = strconv.Quote(procErr)
 			statusCode, _ := params["status_code"].(string)
-			eventName := gjson.GetBytes(job.EventPayload, "event").String()
-			eventType := gjson.GetBytes(job.EventPayload, "type").String()
+			eventName := misc.GetStringifiedData(gjson.GetBytes(job.EventPayload, "0.event").String())
+			eventType := misc.GetStringifiedData(gjson.GetBytes(job.EventPayload, "0.type").String())
 			sentAt := time.Now().Format(misc.RFC3339Milli)
 
 			deliveryStatus := destinationdebugger.DeliveryStatusT{
