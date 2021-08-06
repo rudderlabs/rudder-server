6101d0dca719d800644eea5ca2eda936fedca406
siddarth <siddarth@rudderstack.com>
2021-08-06 18:49:07 +0530

    dropEventsAfter24Hrs

--- router/utils/utils.go
+++ router/utils/utils.go
@@ -2,10 +2,12 @@ package utils
 
 import (
 	"strings"
+	"time"
 
 	backendconfig "github.com/rudderlabs/rudder-server/config/backend-config"
 	"github.com/rudderlabs/rudder-server/jobsdb"
 	"github.com/rudderlabs/rudder-server/utils/misc"
+	"github.com/tidwall/gjson"
 )
 
 type BatchDestinationT struct {
@@ -14,6 +16,11 @@ type BatchDestinationT struct {
 }
 
 func ToBeDrained(job *jobsdb.JobT, destID, toAbortDestinationIDs string, destinationsMap map[string]*BatchDestinationT) bool {
+	//drain if job is older than a day
+	jobReceivedAt := gjson.GetBytes(job.Parameters, "received_at")
+	if jobReceivedAt.Exists() && time.Since(jobReceivedAt.Time()).Hours() > 24 {
+		return true
+	}
 	if d, ok := destinationsMap[destID]; ok && !d.Destination.Enabled {
 		return true
 	}
