402402cd7b713a213d85d26695a728879c45429e
Priyam Dixit <priyamdixit@Priyams-MacBook-Pro.local>
2021-08-17 12:30:33 +0530

    integration test poc

--- go.mod
+++ go.mod
@@ -8,12 +8,15 @@ require (
 	cloud.google.com/go/storage v1.10.0
 	github.com/Azure/azure-pipeline-go v0.2.2 // indirect
 	github.com/Azure/azure-storage-blob-go v0.8.0
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Azure/go-autorest/autorest/adal v0.8.1 // indirect
 	github.com/BurntSushi/toml v0.3.1
 	github.com/ClickHouse/clickhouse-go v1.4.1
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/EagleChen/mapmutex v0.0.0-20180418073615-e1a5ae258d8d // indirect
 	github.com/EagleChen/restrictor v0.0.0-20180420073700-9b81bbf8df1d
+	github.com/Microsoft/go-winio v0.5.0 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/Shopify/sarama v1.26.1
 	github.com/araddon/dateparse v0.0.0-20190622164848-0fb0a474d195
 	github.com/aws/aws-sdk-go v1.37.23
@@ -22,17 +25,20 @@ require (
 	github.com/bugsnag/panicwrap v1.2.0 // indirect
 	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/cenkalti/backoff/v4 v4.0.2
+	github.com/confluentinc/confluent-kafka-go v1.7.0 // indirect
+	github.com/containerd/continuity v0.1.0 // indirect
 	github.com/denisenkom/go-mssqldb v0.9.0
 	github.com/dgraph-io/badger/v2 v2.2007.2
 	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
 	github.com/fsnotify/fsnotify v1.4.7
 	github.com/garyburd/redigo v1.6.0 // indirect
 	github.com/go-redis/redis v6.15.7+incompatible
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gofrs/uuid v3.2.0+incompatible
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-migrate/migrate/v4 v4.11.0
 	github.com/golang/mock v1.5.0
-	github.com/golang/protobuf v1.4.3
+	github.com/golang/protobuf v1.5.0
 	github.com/gorilla/mux v1.7.4
 	github.com/hashicorp/go-retryablehttp v0.6.6
 	github.com/hashicorp/yamux v0.0.0-20200609203250-aecfd211c9ce
@@ -40,6 +46,7 @@ require (
 	github.com/jeremywohl/flatten v1.0.1
 	github.com/jinzhu/copier v0.3.2
 	github.com/joho/godotenv v1.3.0
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lib/pq v1.3.0
 	github.com/mattn/go-ieproxy v0.0.0-20200203040449-2dbc853185d9 // indirect
@@ -48,6 +55,8 @@ require (
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/onsi/ginkgo v1.11.0
 	github.com/onsi/gomega v1.7.0
+	github.com/opencontainers/runc v1.0.1 // indirect
+	github.com/ory/dockertest v3.3.5+incompatible // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4 // indirect
@@ -72,15 +81,16 @@ require (
 	github.com/xtgo/uuid v0.0.0-20140804021211-a0b114877d4c // indirect
 	go.uber.org/multierr v1.5.0 // indirect
 	go.uber.org/zap v1.14.0
-	golang.org/x/net v0.0.0-20210224082022-3d97a244fca7 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
 	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
-	golang.org/x/sys v0.0.0-20210225134936-a50acf3fe073 // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
 	google.golang.org/api v0.39.0
 	google.golang.org/grpc v1.34.0
-	google.golang.org/protobuf v1.25.0
+	google.golang.org/protobuf v1.26.0
 	gopkg.in/alexcesaro/statsd.v2 v2.0.0
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.7.0 // indirect
 	gopkg.in/ini.v1 v1.52.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ integrationTest/docker_test.go
@@ -0,0 +1,184 @@
+package dockertest_test
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/go-redis/redis"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	"github.com/ory/dockertest"
+)
+
+var db *sql.DB
+var redisClient *redis.Client
+var DB_DSN = "root@tcp(127.0.0.1:3306)/service"
+var resource *dockertest.Resource
+
+
+type Author struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func CreateTablePostgres ()  {
+	_, err := db.Exec("CREATE TABLE example ( id integer, username varchar(255) )")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func VerifyHealth(){
+	url := fmt.Sprintf("http://localhost:%s/health","8080")
+	method := "GET"
+	
+	client := &http.Client {
+	}
+	req, err := http.NewRequest(method, url, nil)
+	
+	if err != nil {
+		fmt.Println(err)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(body))
+}
+func SendEvent(){
+
+	url := "http://localhost:8080/v1/identify"
+	method := "POST"
+  
+	payload := strings.NewReader(`{
+	"userId": "identified user id",
+	"anonymousId":"anon-id-new",
+	"context": {
+	  "traits": {
+		 "trait1": "new-val"  
+	  },
+	  "ip": "14.5.67.21",
+	  "library": {
+		  "name": "http"
+	  }
+	},
+	"timestamp": "2020-02-02T00:23:09.544Z"
+  }`)
+  
+	client := &http.Client {
+	}
+	req, err := http.NewRequest(method, url, payload)
+  
+	if err != nil {
+	  fmt.Println(err)
+	  return
+	}
+	req.Header.Add("Content-Type", "application/json")
+    req.Header.Add("Authorization", "Basic MXZMYzJ5TFRsamxNY2J1Smwza0NETWs2MkZBOg==")
+  
+	res, err := client.Do(req)
+	if err != nil {
+	  fmt.Println(err)
+	  return
+	}
+	defer res.Body.Close()
+  
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+	  fmt.Println(err)
+	  return
+	}
+	fmt.Println(string(body))
+	fmt.Println("Event Sent Successfully")
+  }
+
+func TestMain(m *testing.M) {
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// pulls an redis image, creates a container based on it and runs it
+	resourceRedis, err := pool.Run("redis", "alpine3.14", []string{"requirepass=secret"})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	if err := pool.Retry(func() error {
+		address := fmt.Sprintf("localhost:%s", resourceRedis.GetPort("6379/tcp"))
+		redisClient = redis.NewClient(&redis.Options{
+			Addr:     address,
+			Password: "",
+			DB:       0,
+		})
+
+		pong, err := redisClient.Ping().Result()
+		fmt.Println(pong, err)
+		return err
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+	// ----------
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	postgrespool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	database := "integrationtest"
+	// pulls an image, creates a container based on it and runs it
+	resource, err = postgrespool.Run("postgres", "9.6", []string{"POSTGRES_PASSWORD=secret", "POSTGRES_DB=" + database})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+	DB_DSN = fmt.Sprintf("postgres://postgres:secret@localhost:%s/%s?sslmode=disable", resource.GetPort("5432/tcp"), database)
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	if err := postgrespool.Retry(func() error {
+		var err error
+		db, err = sql.Open("postgres", DB_DSN)
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+	// ----------
+
+	code := m.Run()
+
+	// You can't defer this because os.Exit doesn't care for defer
+	// if err := pool.Purge(resource); err != nil {
+	// 	log.Fatalf("Could not purge resource: %s", err)
+	// }
+
+	os.Exit(code)
+}
+
+func TestSomething(t *testing.T) {
+	//Testing postgres Client
+	CreateTablePostgres()
+
+	//  Test Rudder docker health point
+	VerifyHealth()
+
+	//SEND EVENT
+	SendEvent()
+ 	// TODO: Verify in POSTGRES
+	// TODO: Verify in Live Evets API
+}
