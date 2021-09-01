140d787977209e5623d745cb4207bf4370051c75
Chandra shekar Varkala <chandra@rudderlabs.com>
2021-09-01 14:59:24 +0530

    fix tests

--- services/debugger/source/eventUploader_test.go
+++ services/debugger/source/eventUploader_test.go
@@ -54,15 +54,6 @@ var _ = Describe("eventUploader", func() {
 		c = &eventUploaderContext{}
 		c.Setup()
 		recordingEvent = `{"t":"a"}`
-		c.mockBackendConfig.EXPECT().Subscribe(gomock.Any(), backendconfig.TopicProcessConfig).
-			Do(func(channel chan utils.DataEvent, topic backendconfig.Topic) {
-				// on Subscribe, emulate a backend configuration event
-				go func() {
-					channel <- utils.DataEvent{Data: sampleBackendConfig, Topic: string(topic)}
-					c.configInitialised = true
-				}()
-			}).
-			Do(c.asyncHelper.ExpectAndNotifyCallback()).Return().Times(1)
 	})
 
 	AfterEach(func() {
@@ -71,6 +62,17 @@ var _ = Describe("eventUploader", func() {
 
 	Context("RecordEvent", func() {
 		It("returns false if disableEventUploads is false", func() {
+
+			c.mockBackendConfig.EXPECT().Subscribe(gomock.Any(), backendconfig.TopicProcessConfig).
+				Do(func(channel chan utils.DataEvent, topic backendconfig.Topic) {
+					// on Subscribe, emulate a backend configuration event
+					go func() {
+						channel <- utils.DataEvent{Data: sampleBackendConfig, Topic: string(topic)}
+						c.configInitialised = true
+					}()
+				}).
+				Do(c.asyncHelper.ExpectAndNotifyCallback()).Return().Times(1)
+
 			time.Sleep(3 * time.Second)
 			disableEventUploads = true
 			Expect(RecordEvent(sample_writeKey, recordingEvent)).To(BeFalse())
@@ -78,11 +80,33 @@ var _ = Describe("eventUploader", func() {
 		})
 
 		It("returns false if writeKey is not part of uploadEnabledWriteKeys", func() {
+
+			c.mockBackendConfig.EXPECT().Subscribe(gomock.Any(), backendconfig.TopicProcessConfig).
+				Do(func(channel chan utils.DataEvent, topic backendconfig.Topic) {
+					// on Subscribe, emulate a backend configuration event
+					go func() {
+						channel <- utils.DataEvent{Data: sampleBackendConfig, Topic: string(topic)}
+						c.configInitialised = true
+					}()
+				}).
+				Do(c.asyncHelper.ExpectAndNotifyCallback()).Return().Times(1)
+
 			time.Sleep(3 * time.Second)
 			Expect(RecordEvent(sample_writeKey, recordingEvent)).To(BeFalse())
 		})
 
 		It("transforms payload properly", func() {
+
+			c.mockBackendConfig.EXPECT().Subscribe(gomock.Any(), backendconfig.TopicProcessConfig).
+				Do(func(channel chan utils.DataEvent, topic backendconfig.Topic) {
+					// on Subscribe, emulate a backend configuration event
+					go func() {
+						channel <- utils.DataEvent{Data: sampleBackendConfig, Topic: string(topic)}
+						c.configInitialised = true
+					}()
+				}).
+				Do(c.asyncHelper.ExpectAndNotifyCallback()).Return().Times(1)
+
 			time.Sleep(3 * time.Second)
 			recordingEvent0 := `{"receivedAt":"2021-08-03T17:26:","writeKey":"1vWezJfHKkbUHexNepDsGcSVWae","requestIP":"[::1]",  "batch": [{"anonymousId":"anon_id","channel":"android-sdk","context":{"app":{"build":"1","name":"RudderAndroidClient","namespace":"com.rudderlabs.android.sdk","version":"1.0"},"device":{"id":"49e4bdd1c280bc00","manufacturer":"Google","model":"Android SDK built for x86","name":"generic_x86"},"library":{"name":"com.rudderstack.android.sdk.core"},"locale":"en-US","network":{"carrier":"Android"},"screen":{"density":420,"height":1794,"width":1080},"traits":{"anonymousId":"49e4bdd1c280bc00"},"user_agent":"Dalvik/2.1.0 (Linux; U; Android 9; Android SDK built for x86 Build/PSR1.180720.075)"},"event":{"name": "Demo Track"},"integrations":{"All":true},"messageId":"7a355fdd-0325-4778-9905-b43f586acdd4","originalTimestamp":"2019-08-12T05:08:30.909Z","properties":{"category":"Demo Category","floatVal":4.501,"label":"Demo Label","testArray":[{"id":"elem1","value":"e1"},{"id":"elem2","value":"e2"}],"testMap":{"t1":"a","t2":4},"value":5},"rudderId":"90ca6da0-292e-4e79-9880-f8009e0ae4a3","sentAt":"2019-08-12T05:08:30.909Z","type":"track"}]}`
 			recordingEvent = `{"receivedAt":"2021-08-03T17:26:00.279+05:30","writeKey":"1vWezJfHKkbUHexNepDsGcSVWae","requestIP":"[::1]",  "batch": [{"anonymousId":"anon_id","channel":"android-sdk","context":{"app":{"build":"1","name":"RudderAndroidClient","namespace":"com.rudderlabs.android.sdk","version":"1.0"},"device":{"id":"49e4bdd1c280bc00","manufacturer":"Google","model":"Android SDK built for x86","name":"generic_x86"},"library":{"name":"com.rudderstack.android.sdk.core"},"locale":"en-US","network":{"carrier":"Android"},"screen":{"density":420,"height":1794,"width":1080},"traits":{"anonymousId":"49e4bdd1c280bc00"},"user_agent":"Dalvik/2.1.0 (Linux; U; Android 9; Android SDK built for x86 Build/PSR1.180720.075)"},"event":"Demo Track","integrations":{"All":true},"messageId":"7a355fdd-0325-4778-9905-b43f586acdd4","originalTimestamp":"2019-08-12T05:08:30.909Z","properties":{"category":"Demo Category","floatVal":4.501,"label":"Demo Label","testArray":[{"id":"elem1","value":"e1"},{"id":"elem2","value":"e2"}],"testMap":{"t1":"a","t2":4},"value":5},"rudderId":"90ca6da0-292e-4e79-9880-f8009e0ae4a3","sentAt":"2019-08-12T05:08:30.909Z","type":"track"}]}`
@@ -95,6 +119,17 @@ var _ = Describe("eventUploader", func() {
 		})
 
 		It("ignores improperly built payload", func() {
+
+			c.mockBackendConfig.EXPECT().Subscribe(gomock.Any(), backendconfig.TopicProcessConfig).
+				Do(func(channel chan utils.DataEvent, topic backendconfig.Topic) {
+					// on Subscribe, emulate a backend configuration event
+					go func() {
+						channel <- utils.DataEvent{Data: sampleBackendConfig, Topic: string(topic)}
+						c.configInitialised = true
+					}()
+				}).
+				Do(c.asyncHelper.ExpectAndNotifyCallback()).Return().Times(1)
+
 			time.Sleep(3 * time.Second)
 			recordingEvent0 := `{"receivedAt":"2021-08-03T17:26:","writeKey":"1vWezJfHKkbUHexNepDsGcSVWae","requestIP":"[::1]",  "batch": [{"anonymousId":"anon_id","channel":"android-sdk","context":{"app":{"build":"1","name":"RudderAndroidClient","namespace":"com.rudderlabs.android.sdk","version":"1.0"},"device":{"id":"49e4bdd1c280bc00","manufacturer":"Google","model":"Android SDK built for x86","name":"generic_x86"},"library":{"name":"com.rudderstack.android.sdk.core"},"locale":"en-US","network":{"carrier":"Android"},"screen":{"density":420,"height":1794,"width":1080},"traits":{"anonymousId":"49e4bdd1c280bc00"},"user_agent":"Dalvik/2.1.0 (Linux; U; Android 9; Android SDK built for x86 Build/PSR1.180720.075)"},"event":{"name": "Demo Track"},"integrations":{"All":true},"messageId":"7a355fdd-0325-4778-9905-b43f586acdd4","originalTimestamp":"2019-08-12T05:08:30.909Z","properties":{"category":"Demo Category","floatVal":4.501,"label":"Demo Label","testArray":[{"id":"elem1","value":"e1"},{"id":"elem2","value":"e2"}],"testMap":{"t1":"a","t2":4},"value":5},"rudderId":"90ca6da0-292e-4e79-9880-f8009e0ae4a3","sentAt":"2019-08-12T05:08:30.909Z","type":"track"}`
 			eventUploader := EventUploader{}
@@ -104,6 +139,17 @@ var _ = Describe("eventUploader", func() {
 		})
 
 		It("records events", func() {
+
+			c.mockBackendConfig.EXPECT().Subscribe(gomock.Any(), backendconfig.TopicProcessConfig).
+				Do(func(channel chan utils.DataEvent, topic backendconfig.Topic) {
+					// on Subscribe, emulate a backend configuration event
+					go func() {
+						channel <- utils.DataEvent{Data: sampleBackendConfig, Topic: string(topic)}
+						c.configInitialised = true
+					}()
+				}).
+				Do(c.asyncHelper.ExpectAndNotifyCallback()).Return().Times(1)
+
 			time.Sleep(3 * time.Second)
 			recordingEvent = `{"receivedAt":"2021-08-03T17:26:00.279+05:30","writeKey":"1vWezJfHKkbUHexNepDsGcSVWae","requestIP":"[::1]",  "batch": [{"anonymousId":"anon_id","channel":"android-sdk","context":{"app":{"build":"1","name":"RudderAndroidClient","namespace":"com.rudderlabs.android.sdk","version":"1.0"},"device":{"id":"49e4bdd1c280bc00","manufacturer":"Google","model":"Android SDK built for x86","name":"generic_x86"},"library":{"name":"com.rudderstack.android.sdk.core"},"locale":"en-US","network":{"carrier":"Android"},"screen":{"density":420,"height":1794,"width":1080},"traits":{"anonymousId":"49e4bdd1c280bc00"},"user_agent":"Dalvik/2.1.0 (Linux; U; Android 9; Android SDK built for x86 Build/PSR1.180720.075)"},"event":"Demo Track","integrations":{"All":true},"messageId":"7a355fdd-0325-4778-9905-b43f586acdd4","originalTimestamp":"2019-08-12T05:08:30.909Z","properties":{"category":"Demo Category","floatVal":4.501,"label":"Demo Label","testArray":[{"id":"elem1","value":"e1"},{"id":"elem2","value":"e2"}],"testMap":{"t1":"a","t2":4},"value":5},"rudderId":"90ca6da0-292e-4e79-9880-f8009e0ae4a3","sentAt":"2019-08-12T05:08:30.909Z","type":"track"}]}`
 			Expect(RecordEvent(WriteKeyEnabled, recordingEvent)).To(BeTrue())
