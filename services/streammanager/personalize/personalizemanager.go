630f761a779b31a43b686cdd0c95de4a3a7b51af
shrouti <shrouti.gangopadhyay@gmail.com>
2021-09-01 13:26:41 +0530

    initial commit

--- services/streammanager/personalize/personalizemanager.go
+++ services/streammanager/personalize/personalizemanager.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/personalizeevents"
 	"github.com/rudderlabs/rudder-server/utils/logger"
+	"github.com/tidwall/gjson"
 )
 
 type Config struct {
@@ -48,30 +49,90 @@ func NewProducer(destinationConfig interface{}) (personalizeevents.PersonalizeEv
 }
 func Produce(jsonData json.RawMessage, producer interface{}, destConfig interface{}) (statusCode int, respStatus string, responseMessag string) {
 
+	//fmt.Printf("jsonData is%s\n", string(jsonData))
+	parsedJSON := gjson.ParseBytes(jsonData)
+	eventChoice := parsedJSON.Get("choice").String()
+	eventPayload := parsedJSON.Get("payload").String()
+	// fmt.Printf("eventChoice is%s\n", string(eventChoice))
 	client, ok := producer.(personalizeevents.PersonalizeEvents)
 	if (!ok || client == personalizeevents.PersonalizeEvents{}) {
 		// return 400 if producer is invalid
 		return 400, "Could not create producer for Personalize", "Could not create producer for Personalize"
 	}
-	input := personalizeevents.PutEventsInput{}
-	bytes := []byte(jsonData)
-	err := json.Unmarshal(bytes, &input)
-	if err != nil {
-		return 400, err.Error(), "Could not unmarshal jsonData according to putEvents input structure"
-	}
-	res, err := client.PutEvents(&input)
-	if err != nil {
-		pkgLogger.Errorf("Personalize Error while sending event :: %w", err)
-		// set default status code as 500
-		statusCode := 500
-		// fetching status code from response
-		if reqErr, ok := err.(awserr.RequestFailure); ok {
-			statusCode = reqErr.StatusCode()
+
+	if eventChoice == "PutEvents" {
+
+		input := personalizeevents.PutEventsInput{}
+		bytes := []byte(eventPayload)
+		err := json.Unmarshal(bytes, &input)
+		if err != nil {
+			return 400, err.Error(), "Could not unmarshal jsonData according to putEvents input structure"
 		}
-		return statusCode, err.Error(), err.Error()
-	} else {
-		responseMessage := fmt.Sprintf("Message delivered with Record information %v", res)
-		respStatus = "Success"
-		return 200, respStatus, responseMessage
+		res, err := client.PutEvents(&input)
+		if err != nil {
+			pkgLogger.Errorf("Personalize Error while sending event :: %w", err)
+			// set default status code as 500
+			statusCode := 500
+			// fetching status code from response
+			if reqErr, ok := err.(awserr.RequestFailure); ok {
+				statusCode = reqErr.StatusCode()
+			}
+			return statusCode, err.Error(), err.Error()
+		} else {
+			responseMessage := fmt.Sprintf("Message delivered with Record information %v", res)
+			respStatus = "Success"
+			return 200, respStatus, responseMessage
+		}
 	}
+	// putUsers not working still now
+	if eventChoice == "PutUsers" {
+		input := personalizeevents.PutUsersInput{}
+		bytes := []byte(eventPayload)
+		err := json.Unmarshal(bytes, &input)
+		if err != nil {
+			fmt.Printf(err.Error())
+			fmt.Printf("Could not unmarshal jsonData according to putEvents input structure")
+		}
+		res, err := client.PutUsers(&input)
+		if err != nil {
+			pkgLogger.Errorf("Personalize Error while sending event :: %w", err)
+			// set default status code as 500
+			statusCode := 500
+			// fetching status code from response
+			if reqErr, ok := err.(awserr.RequestFailure); ok {
+				statusCode = reqErr.StatusCode()
+			}
+			return statusCode, err.Error(), err.Error()
+		} else {
+			responseMessage := fmt.Sprintf("Message delivered with Record information %v", res)
+			respStatus = "Success"
+			return 200, respStatus, responseMessage
+		}
+	}
+
+	if eventChoice == "PutItems" {
+		input := personalizeevents.PutItemsInput{}
+		bytes := []byte(eventPayload)
+		err := json.Unmarshal(bytes, &input)
+		if err != nil {
+			fmt.Printf(err.Error())
+			fmt.Printf("Could not unmarshal jsonData according to putEvents input structure")
+		}
+		res, err := client.PutItems(&input)
+		if err != nil {
+			pkgLogger.Errorf("Personalize Error while sending event :: %w", err)
+			// set default status code as 500
+			statusCode := 500
+			// fetching status code from response
+			if reqErr, ok := err.(awserr.RequestFailure); ok {
+				statusCode = reqErr.StatusCode()
+			}
+			return statusCode, err.Error(), err.Error()
+		} else {
+			responseMessage := fmt.Sprintf("Message delivered with Record information %v", res)
+			respStatus = "Success"
+			return 200, respStatus, responseMessage
+		}
+	}
+	return 400, respStatus, "This is not a supported event type"
 }
