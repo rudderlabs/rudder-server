23a1824153d8e28b4907788074b0d44b4db81d75
Akash Chetty <achetty.iitr@gmail.com>
2021-09-23 16:37:47 +0530

    Added error to previous Job faild uploads.

--- warehouse/upload.go
+++ warehouse/upload.go
@@ -636,6 +636,17 @@ type TableUploadStatusT struct {
 	namespace     string
 	tableName     string
 	status        string
+	error         string
+}
+
+type TableUploadStatusErrorT struct {
+	status string
+	error  string
+}
+
+type TableUploadIDErrorT struct {
+	uploadID int64
+	error    string
 }
 
 func (job *UploadJobT) fetchPendingUploadTableStatus() []*TableUploadStatusT {
@@ -648,7 +659,8 @@ func (job *UploadJobT) fetchPendingUploadTableStatus() []*TableUploadStatusT {
 			%[1]s.destination_id,
 			%[1]s.namespace,
 			%[2]s.table_name,
-			%[2]s.status
+			%[2]s.status,
+			%[2]s.error
 		FROM
 			%[1]s INNER JOIN %[2]s
 		ON
@@ -685,6 +697,7 @@ func (job *UploadJobT) fetchPendingUploadTableStatus() []*TableUploadStatusT {
 			&tableUploadStatus.namespace,
 			&tableUploadStatus.tableName,
 			&tableUploadStatus.status,
+			&tableUploadStatus.error,
 		)
 		if err != nil {
 			panic(err)
@@ -695,28 +708,35 @@ func (job *UploadJobT) fetchPendingUploadTableStatus() []*TableUploadStatusT {
 	return tableUploadStatuses
 }
 
-func getTableUploadStatusMap(tableUploadStatuses []*TableUploadStatusT) map[int64]map[string]string {
-	tableUploadStatus := make(map[int64]map[string]string)
+func getTableUploadStatusMap(tableUploadStatuses []*TableUploadStatusT) map[int64]map[string]*TableUploadStatusErrorT {
+	tableUploadStatus := make(map[int64]map[string]*TableUploadStatusErrorT)
 	for _, tUploadStatus := range tableUploadStatuses {
 		if _, ok := tableUploadStatus[tUploadStatus.uploadID]; !ok {
-			tableUploadStatus[tUploadStatus.uploadID] = make(map[string]string)
+			tableUploadStatus[tUploadStatus.uploadID] = make(map[string]*TableUploadStatusErrorT)
 		}
-		tableUploadStatus[tUploadStatus.uploadID][tUploadStatus.tableName] = tUploadStatus.status
+		tableUploadStatus[tUploadStatus.uploadID][tUploadStatus.tableName] = &TableUploadStatusErrorT{
+			status: tUploadStatus.status,
+			error:  tUploadStatus.error,
+		}
 	}
 	return tableUploadStatus
 }
 
-func (job *UploadJobT) getTablesToSkip() (map[string]int64, map[string]bool) {
+func (job *UploadJobT) getTablesToSkip() (map[string]*TableUploadIDErrorT, map[string]bool) {
 	tableUploadStatuses := job.fetchPendingUploadTableStatus()
 	tableUploadStatus := getTableUploadStatusMap(tableUploadStatuses)
-	previouslyFailedTableMap := make(map[string]int64)
+	previouslyFailedTableMap := make(map[string]*TableUploadIDErrorT)
 	currentlySucceededTableMap := make(map[string]bool)
 	for uploadID, tableStatusMap := range tableUploadStatus {
-		for tableName, status := range tableStatusMap {
+		for tableName, tableStatus := range tableStatusMap {
+			status := tableStatus.status
 			if uploadID < job.upload.ID && (status == TableUploadExportingFailed ||
 				status == UserTableUploadExportingFailed ||
 				status == IdentityTableUploadExportingFailed) { //Previous upload and table upload failed
-				previouslyFailedTableMap[tableName] = uploadID
+				previouslyFailedTableMap[tableName] = &TableUploadIDErrorT{
+					uploadID: uploadID,
+					error:    tableStatus.error,
+				}
 			}
 			if uploadID == job.upload.ID && status == TableUploadExported { //Current upload and table upload succeeded
 				currentlySucceededTableMap[tableName] = true
@@ -779,12 +799,13 @@ func (job *UploadJobT) updateTableSchema(tName string, tableSchemaDiff warehouse
 
 //TableSkipError is a custom error type to capture if a table load is skipped because of a previously failed table load
 type TableSkipError struct {
-	tableName     string
-	previousJobID int64
+	tableName        string
+	previousJobID    int64
+	previousJobError string
 }
 
 func (tse *TableSkipError) Error() string {
-	return fmt.Sprintf("Skipping %s table because it previously failed to load in an earlier job: %d", tse.tableName, tse.previousJobID)
+	return fmt.Sprintf("Skipping %s table because it previously failed to load in an earlier job: %d with error: %s", tse.tableName, tse.previousJobID, tse.previousJobError)
 }
 
 func (job *UploadJobT) loadAllTablesExcept(skipLoadForTables []string) []error {
@@ -813,8 +834,8 @@ func (job *UploadJobT) loadAllTablesExcept(skipLoadForTables []string) []error {
 			wg.Done()
 			continue
 		}
-		if prevJobID, ok := previouslyFailedTables[tableName]; ok {
-			loadErrors = append(loadErrors, &TableSkipError{tableName: tableName, previousJobID: prevJobID})
+		if prevJobStatus, ok := previouslyFailedTables[tableName]; ok {
+			loadErrors = append(loadErrors, &TableSkipError{tableName: tableName, previousJobID: prevJobStatus.uploadID, previousJobError: prevJobStatus.error})
 			wg.Done()
 			continue
 		}
@@ -883,7 +904,7 @@ func (job *UploadJobT) getTotalCount(tName string) (int64, error) {
 	expBackoff.InitialInterval = 5 * time.Second
 	expBackoff.RandomizationFactor = 0
 	expBackoff.Reset()
-	backoffWithMaxRetry := backoff.WithMaxRetries(expBackoff, 5)
+	backoffWithMaxRetry := backoff.WithMaxRetries(expBackoff, 1)
 	err := backoff.RetryNotify(operation, backoffWithMaxRetry, func(err error, t time.Duration) {
 		pkgLogger.Errorf(`Error getting total count in table:%s error: %v`, tName, err)
 	})
@@ -944,8 +965,8 @@ func (job *UploadJobT) loadUserTables() ([]error, error) {
 	var err error
 	previouslyFailedTables, currentJobSucceededTables := job.getTablesToSkip()
 	for _, tName := range userTables {
-		if prevJobID, ok := previouslyFailedTables[tName]; ok {
-			err = &TableSkipError{tableName: tName, previousJobID: prevJobID}
+		if prevJobStatus, ok := previouslyFailedTables[tName]; ok {
+			err = &TableSkipError{tableName: tName, previousJobID: prevJobStatus.uploadID, previousJobError: prevJobStatus.error}
 			return []error{err}, nil
 		}
 	}
@@ -1004,8 +1025,8 @@ func (job *UploadJobT) loadIdentityTables(populateHistoricIdentities bool) (load
 	identityTables := []string{job.identityMergeRulesTableName(), job.identityMappingsTableName()}
 	previouslyFailedTables, currentJobSucceededTables := job.getTablesToSkip()
 	for _, tableName := range identityTables {
-		if prevJobID, ok := previouslyFailedTables[tableName]; ok {
-			return []error{&TableSkipError{tableName: tableName, previousJobID: prevJobID}}, nil
+		if prevJobStatus, ok := previouslyFailedTables[tableName]; ok {
+			return []error{&TableSkipError{tableName: tableName, previousJobID: prevJobStatus.uploadID, previousJobError: prevJobStatus.error}}, nil
 		}
 	}
 
