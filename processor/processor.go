92a123b965c76f4b046eb4bc6c004ad13e82525e
siddarth <siddarth@rudderstack.com>
2021-09-23 14:33:14 +0530

    removedGatewayEventNameType

--- processor/processor.go
+++ processor/processor.go
@@ -1126,7 +1126,7 @@ func (proc *HandleT) processJobsForDest(jobList []*jobsdb.JobT, parsedEventList 
 					}
 					sd, ok := statusDetailsMap[key]
 					if !ok {
-						sd = types.CreateStatusDetail(jobsdb.Succeeded.State, 0, 200, "", []byte(`{}`), commonMetadataFromSingularEvent.EventName, commonMetadataFromSingularEvent.EventType)
+						sd = types.CreateStatusDetail(jobsdb.Succeeded.State, 0, 200, "", []byte(`{}`), "", "")
 						statusDetailsMap[key] = sd
 					}
 					sd.Count++
