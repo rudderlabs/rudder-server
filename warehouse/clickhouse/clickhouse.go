e27ace8e69b7a2ffb848660c071e47f163c5ae89
Akash Chetty <achetty.iitr@gmail.com>
2021-09-27 17:07:01 +0530

    More logs.

--- warehouse/clickhouse/clickhouse.go
+++ warehouse/clickhouse/clickhouse.go
@@ -359,44 +359,57 @@ func (ch *HandleT) DownloadLoadFiles(tableName string) ([]string, error) {
 						pkgLogger.Infof("context is cancelled, stopped processing download load file for table:%s, namespace:%s, workerIdx:%d, goId:%d", tableName, ch.Namespace, workerIdx, goId)
 						return
 					default:
+						pkgLogger.Infof("DownloadLoadFiles GetObjectName started table:%s, namespace:%s", tableName, ch.Namespace)
 						objectName, err := warehouseutils.GetObjectName(object.Location, ch.Warehouse.Destination.Config, ch.ObjectStorage)
+						pkgLogger.Infof("DownloadLoadFiles GetObjectName completed table:%s, namespace:%s", tableName, ch.Namespace)
 						if err != nil {
 							err = fmt.Errorf("CH: Error in converting object location to object key for table:%s, location:%s, error:%v, namespace:%s, workerIdx:%d goId:%d", tableName, object.Location, err, ch.Namespace, workerIdx, goId)
 							onError(err)
 							return
 						}
 						dirName := "/rudder-warehouse-load-uploads-tmp/"
+						pkgLogger.Infof("DownloadLoadFiles CreateTMPDIR started table:%s, namespace:%s", tableName, ch.Namespace)
 						tmpDirPath, err := misc.CreateTMPDIR()
+						pkgLogger.Infof("DownloadLoadFiles CreateTMPDIR completed table:%s, namespace:%s", tableName, ch.Namespace)
+
 						if err != nil {
 							err = fmt.Errorf("CH: Error in getting tmp directory for downloading load file for table:%s, location:%s, error:%v, namespace:%s, workerIdx:%d goId:%d", tableName, object.Location, err, ch.Namespace, workerIdx, goId)
 							onError(err)
 							return
 						}
 						ObjectPath := tmpDirPath + dirName + fmt.Sprintf(`%s_%s_%d/`, ch.Warehouse.Destination.DestinationDefinition.Name, ch.Warehouse.Destination.ID, time.Now().Unix()) + objectName
+						pkgLogger.Infof("DownloadLoadFiles MkdirAll started table:%s, namespace:%s", tableName, ch.Namespace)
 						err = os.MkdirAll(filepath.Dir(ObjectPath), os.ModePerm)
+						pkgLogger.Infof("DownloadLoadFiles MkdirAll completed table:%s, namespace:%s", tableName, ch.Namespace)
 						if err != nil {
 							err = fmt.Errorf("CH: Error in making tmp directory for downloading load file for table:%s, location:%s, error:%v, namespace:%s, workerIdx:%d goId:%d", tableName, object.Location, err, ch.Namespace, workerIdx, goId)
 							onError(err)
 							return
 						}
+						pkgLogger.Infof("DownloadLoadFiles Create started table:%s, namespace:%s", tableName, ch.Namespace)
 						objectFile, err := os.Create(ObjectPath)
+						pkgLogger.Infof("DownloadLoadFiles Create completed table:%s, namespace:%s", tableName, ch.Namespace)
 						if err != nil {
 							err = fmt.Errorf("CH: Error in converting object location to object key for table:%s, location:%s, error:%v, namespace:%s, workerIdx:%d goId:%d", tableName, object.Location, err, ch.Namespace, workerIdx, goId)
 							onError(err)
 							return
 						}
+						pkgLogger.Infof("DownloadLoadFiles Download started table:%s, namespace:%s", tableName, ch.Namespace)
 						err = downloader.Download(objectFile, objectName)
+						pkgLogger.Infof("DownloadLoadFiles Download completed table:%s, namespace:%s", tableName, ch.Namespace)
 						if err != nil {
 							err = fmt.Errorf("CH: Error in downloading file in tmp directory for table:%s, location:%s, error:%v, namespace:%s, workerIdx:%d goId:%d", tableName, object.Location, err, ch.Namespace, workerIdx, goId)
 							onError(err)
 							return
 						}
 						fileName := objectFile.Name()
+						pkgLogger.Infof("DownloadLoadFiles Close started table:%s, namespace:%s", tableName, ch.Namespace)
 						if err = objectFile.Close(); err != nil {
 							err = fmt.Errorf("CH: Error in closing downloaded file in tmp directory for table:%s, location:%s, error:%v, namespace:%s, workerIdx:%d goId:%d", tableName, object.Location, err, ch.Namespace, workerIdx, goId)
 							onError(err)
 							return
 						}
+						pkgLogger.Infof("DownloadLoadFiles Close completed table:%s, namespace:%s", tableName, ch.Namespace)
 						fileNames = append(fileNames, fileName)
 
 						wg.Done()
