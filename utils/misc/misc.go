71ff498c6921b0643d7eb36d98a90b622e4e0f01
Akash Chetty <achetty.iitr@gmail.com>
2021-09-28 13:42:17 +0530

    Retries and Timeout changes.

--- utils/misc/misc.go
+++ utils/misc/misc.go
@@ -1130,15 +1130,3 @@ func MergeMaps(maps ...map[string]interface{}) map[string]interface{} {
 	}
 	return result
 }
-
-// GetGoID Generates Goroutine ID
-func GetGoID() (int, error) {
-	var buf [64]byte
-	n := runtime.Stack(buf[:], false)
-	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
-	id, err := strconv.Atoi(idField)
-	if err != nil {
-		return 0, fmt.Errorf("cannot get goroutine id: %v", err)
-	}
-	return id, nil
-}
