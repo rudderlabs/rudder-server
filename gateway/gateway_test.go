38d6b71e7c12a9650e84147a28926214c5e18668
Leonidas Vrachnis <leo.al.vra@gmail.com>
2021-09-30 02:57:23 +0530

    expect correct error.

--- gateway/gateway_test.go
+++ gateway/gateway_test.go
@@ -494,7 +494,7 @@ var _ = Describe("Gateway", func() {
 			})
 
 			It("should reject requests without request body", func() {
-				expectHandlerResponse(handler, authorizedRequest(WriteKeyInvalid, nil), 400, response.RequestBodyNil+"\n")
+				expectHandlerResponse(handler, authorizedRequest(WriteKeyInvalid, nil), 400, fmt.Sprintf("payload from request: %s\n", response.RequestBodyNil))
 			})
 
 			It("should reject requests without valid json in request body", func() {
