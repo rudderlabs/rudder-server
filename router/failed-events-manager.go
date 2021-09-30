189d2afa3098b7414b450524f9e64dc453f1d03d
Yeshwanth Vuppu <yeshwanth@rudderstack.com>
2021-09-30 12:12:38 +0530

    add valid json check in failed event manager

--- router/failed-events-manager.go
+++ router/failed-events-manager.go
@@ -72,9 +72,9 @@ func (fem *FailedEventsManagerT) SaveFailedRecordIDs(taskRunIDFailedEventsMap ma
 		}
 		createdAt := time.Now()
 		for _, failedEvent := range failedEvents {
-			pkgLogger.Infof("recordId: %s, destinationID: %s , createdAt: %s", failedEvent.RecordID, failedEvent.DestinationID, createdAt)
-			if len(failedEvent.RecordID) == 0 {
-				pkgLogger.Infof("skipped adding to failed keys for empty event payload: %v", failedEvent)
+			pkgLogger.Infof("recordId: %s, destinationID: %s, createdAt: %s, validJSON: %s", failedEvent.RecordID, failedEvent.DestinationID, createdAt, json.Valid(failedEvent.RecordID))
+			if json.Valid(failedEvent.RecordID) || len(failedEvent.RecordID) == 0 {
+				pkgLogger.Infof("skipped adding to failed keys for invalid recordID: %v", failedEvent)
 				continue
 			}
 			_, err = stmt.Exec(failedEvent.DestinationID, failedEvent.RecordID, createdAt)
