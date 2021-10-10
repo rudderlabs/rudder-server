67cf4d6c781b20e59d283ab9e732914d2e61dc04
Leonidas Vrachnis <leo.al.vra@gmail.com>
2021-10-10 20:13:11 +0530

    Capture time correctly

--- router/transformer/transformer.go
+++ router/transformer/transformer.go
@@ -112,7 +112,8 @@ func (trans *HandleT) Transform(transformType string, transformMessage *types.Tr
 	}
 
 	for {
-		trans.transformRequestTimerStat.Start()
+		s := time.Now()
+
 		resp, err = trans.client.Post(url, "application/json; charset=utf-8",
 			bytes.NewBuffer(rawJSON))
 
@@ -123,7 +124,7 @@ func (trans *HandleT) Transform(transformType string, transformMessage *types.Tr
 		}
 
 		if err != nil {
-			trans.transformRequestTimerStat.End()
+			trans.transformRequestTimerStat.SendTiming(time.Since(s))
 			reqFailed = true
 			trans.logger.Errorf("JS HTTP connection error: URL: %v Error: %+v", url, err)
 			if retryCount > maxRetry {
@@ -138,7 +139,7 @@ func (trans *HandleT) Transform(transformType string, transformMessage *types.Tr
 			trans.logger.Errorf("Failed request succeeded after %v retries, URL: %v", retryCount, url)
 		}
 
-		trans.transformRequestTimerStat.End()
+		trans.transformRequestTimerStat.SendTiming(time.Since(s))
 		break
 	}
 
