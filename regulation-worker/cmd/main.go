cdfa322b56c28e7c78b00c9c81874b89825e88e6
Saurav Malani <sauravmalani1@gmail.com>
2021-10-20 14:56:50 +0530

    Regulation worker (#1343)
    *regulation worker scaffold with mock destination of API and Batch type job.

---
+++ regulation-worker/cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/rudderlabs/rudder-server/regulation-worker/internal/client"
+	"github.com/rudderlabs/rudder-server/regulation-worker/internal/service"
+)
+
+//TODO: in case of os.Interrupt, syscall.SIGTERM make sure that all are notified about the termination of the service (like transformer, if a job is in progress, etc.)
+//creates `loop` object and call getJobLoop and updateStatusLoop methods in different go routines.
+func main() {
+	svc := service.JobSvc{
+		API: client.JobAPI{
+			WorkspaceID: getWorkspaceID(),
+		},
+	}
+
+	l := &service.Looper{Svc: svc}
+	err := l.Loop()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//TODO: get workspaceID from env
+func getWorkspaceID() string {
+	workspaceID := "mockWorkspaceID"
+	return workspaceID
+}
---
+++ regulation-worker/internal/model/model.go
@@ -0,0 +1,43 @@
+package model
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrDestTypeNotFound = errors.New("destination type not found for the destination ID")
+	ErrDestDetail       = errors.New("error while getting destination details")
+	ErrNoRunnableJob    = errors.New("no runnable job found")
+)
+
+type JobStatus string
+
+const (
+	JobStatusUndefined JobStatus = ""
+	JobStatusPending   JobStatus = "pending"
+	JobStatusRunning   JobStatus = "running"
+	JobStatusComplete  JobStatus = "complete"
+	JobStatusFailed    JobStatus = "failed"
+)
+
+type Job struct {
+	ID             int
+	WorkspaceID    string
+	DestinationID  string
+	Status         JobStatus
+	UserAttributes []UserAttribute
+	UpdatedAt      time.Time
+}
+
+type UserAttribute struct {
+	UserID string
+	Phone  *string
+	Email  *string
+}
+
+type Destination struct {
+	DestinationID string
+	Type          string
+	Credentials   string
+}
