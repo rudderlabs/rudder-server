32e439d090e0dc33f27a94f8f9fef5205a6c8874
Saurav Malani <sauravmalani1@gmail.com>
2021-10-29 16:31:24 +0530

    Destination integration (#1379)
    Added support to get realtime destination details (like credentials, config) corresponding to a destinationID of the job.

--- regulation-worker/cmd/main_test.go
+++ regulation-worker/cmd/main_test.go
@@ -3,6 +3,7 @@ package main_test
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -12,6 +13,8 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/rudderlabs/rudder-server/config"
+	backendconfig "github.com/rudderlabs/rudder-server/config/backend-config"
 	main "github.com/rudderlabs/rudder-server/regulation-worker/cmd"
 	"github.com/rudderlabs/rudder-server/regulation-worker/internal/model"
 	"github.com/stretchr/testify/require"
@@ -39,7 +42,7 @@ func handler() http.Handler {
 func run(m *testing.M) int {
 	svr := httptest.NewServer(handler())
 	defer svr.Close()
-	workspaceID := "1001"
+	workspaceID := "1zzAn8ZshcdkLN5TvP86VqLMT90"
 	svcCtx, svcCancel := context.WithCancel(context.Background())
 
 	go func() {
@@ -49,7 +52,11 @@ func run(m *testing.M) int {
 		<-c
 		svcCancel()
 	}()
-
+	os.Setenv("CONFIG_BACKEND_URL", "https://api.dev.rudderlabs.com")
+	os.Setenv("WORKSPACE_TOKEN", "1zzAnCvbknUuGogjQIBhkST0O4K")
+	os.Setenv("CONFIG_PATH", "./test_config.yaml")
+	config.Load()
+	backendconfig.Init()
 	code := m.Run()
 
 	return code
@@ -66,19 +73,19 @@ func TestFlow(t *testing.T) {
 	t.Run("TestFlow", func(t *testing.T) {
 		testData = []test{
 			{
-				respBody:          `{"jobId":"1","destinationId":"23","userAttributes":[{"userId":"1","phone":"555-555-5555"},{"userId":"2","email":"john@example.com"}]}`,
+				respBody:          `{"jobId":"1","destinationId":"1zzK2ZRgKofS6nxfcJ2nthi0Cme","userAttributes":[{"userId":"1","phone":"555-555-5555"},{"userId":"2","email":"john@example.com"}]}`,
 				getJobRespCode:    200,
 				updateJobRespCode: 201,
 				status:            "pending",
 			},
 			{
-				respBody:          `{"jobId":"2","destinationId":"123","userAttributes":[{"userId":"3","phone":"555-555-666"},{"userId":"4","email":"johnny@example.com"}]}`,
+				respBody:          `{"jobId":"2","destinationId":"1zzK2ZRgKofS6nxfcJ2nthi0Cme","userAttributes":[{"userId":"3","phone":"555-555-666"},{"userId":"4","email":"johnny@example.com"}]}`,
 				getJobRespCode:    200,
 				updateJobRespCode: 201,
 				status:            "pending",
 			},
 			{
-				respBody:          `{"jobId":"3","destinationId":"123","userAttributes":[{"userId":"3","phone":"555-555-666"},{"userId":"4","email":"johnny@example.com"}]}`,
+				respBody:          `{"jobId":"3","destinationId":"1zzK2ZRgKofS6nxfcJ2nthi0Cme","userAttributes":[{"userId":"3","phone":"555-555-666"},{"userId":"4","email":"johnny@example.com"}]}`,
 				getJobRespCode:    200,
 				updateJobRespCode: 201,
 				status:            "pending",
@@ -109,9 +116,7 @@ func getJob(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	for i, test := range testData {
-		mu.Lock()
 		status := test.status
-		mu.Unlock()
 		if status == "pending" {
 			w.WriteHeader(testData[i].getJobRespCode)
 			_, _ = w.Write([]byte(testData[i].respBody))
@@ -129,11 +134,13 @@ func updateJobStatus(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewDecoder(r.Body).Decode(&status); err != nil {
 		return
 	}
-	mu.Lock()
+
 	if status == "complete" {
+		mu.Lock()
 		testData[jobID-1].status = "complete"
+		mu.Unlock()
+		fmt.Println("testData[jobID-1].Status=", "complete", "jobID-1=", jobID-1)
 	}
-	mu.Unlock()
 	w.WriteHeader(testData[jobID-1].updateJobRespCode)
 
 	body, err := json.Marshal(struct{}{})
