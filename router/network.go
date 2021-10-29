8f180574868e052257ed567880dd6f69274052e6
Sai Sankeerth <sanpj2292@github.com>
2021-10-29 17:32:05 +0530

    housinganywhere_hubspot: Hubspot batching API support in server commit

--- router/network.go
+++ router/network.go
@@ -89,6 +89,13 @@ func (network *NetHandleT) SendPost(structData integrations.PostParametersT) (st
 					panic(err)
 				}
 				payload = strings.NewReader(string(jsonValue))
+			case "JSON_ARRAY":
+				// support for JSON ARRAY
+				jsonListStr, ok := bodyValue["payload"].(string)
+				if !ok {
+					return 400, "400 Unable to parse json list. Unexpected transformer response"
+				}
+				payload = strings.NewReader(jsonListStr)
 			case "XML":
 				strValue, ok := bodyValue["payload"].(string)
 				if !ok {
