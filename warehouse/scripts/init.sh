76b727fc29385a65569a39530778a0bdbe8808f0
Akash Chetty <achetty.iitr@gmail.com>
2021-11-02 11:27:12 +0530

    Typo issue in init.sh

--- warehouse/scripts/init.sh
+++ warehouse/scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-apt-get -y update \
-apt-get -y install build-essential unixodbc unixodbc-dev wget unzip libsasl2-modules-gssapi-mit netcat \
-wget https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/odbc/2.6.18/SimbaSparkODBC-2.6.18.1030-Debian-64bit.zip \
-unzip SimbaSparkODBC-2.6.18.1030-Debian-64bit.zip \
-dpkg -i simbaspark_2.6.18.1030-2_amd64.deb \
+apt-get -y update
+apt-get -y install build-essential unixodbc unixodbc-dev wget unzip libsasl2-modules-gssapi-mit netcat
+wget https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/odbc/2.6.18/SimbaSparkODBC-2.6.18.1030-Debian-64bit.zip
+unzip SimbaSparkODBC-2.6.18.1030-Debian-64bit.zip
+dpkg -i simbaspark_2.6.18.1030-2_amd64.deb
 rm -rf SimbaSparkODBC-2.6.18.1030-Debian-64bit.zip simbaspark_2.6.18.1030-2_amd64.deb docs/
