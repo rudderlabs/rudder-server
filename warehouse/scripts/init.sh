552cf4d23cd6ead7c3ff3686421d2a435befc46f
Akash Chetty <achetty.iitr@gmail.com>
2021-11-25 08:01:26 +0530

    Update init.sh

--- warehouse/scripts/init.sh
+++ warehouse/scripts/init.sh
@@ -12,3 +12,4 @@ cd ..
 unzip SimbaSparkODBC-2.6.19.1033-Debian-64bit.zip
 dpkg -i simbaspark_2.6.19.1033-2_amd64.deb
 rm -rf SimbaSparkODBC-2.6.19.1033-Debian-64bit.zip simbaspark_2.6.19.1033-2_amd64.deb docs/ unixODBC-2.3.9 unixODBC-2.3.9.tar.gz
+
