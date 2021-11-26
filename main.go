a4aa90efa2f03c105f823cc5a835f2111efa5bd6
Akash Chetty <achetty.iitr@gmail.com>
2021-11-26 14:49:08 +0530

    sigpipe changes.

--- main.go
+++ main.go
@@ -211,7 +211,7 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	go func() {
 		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		signal.Notify(c, os.Interrupt, syscall.SIGPIPE)
 		<-c
 		time.Sleep(time.Minute * 2)
 		cancel()
