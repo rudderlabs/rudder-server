9c35f090fa9063e91955d54c46c17a9258864570
Rohith BCS <rohith.bcs@gmail.com>
2021-12-03 10:53:35 +0530

    Update main_test.go

--- regulation-worker/cmd/main_test.go
+++ regulation-worker/cmd/main_test.go
@@ -56,6 +56,7 @@ func run(m *testing.M) int {
 	os.Setenv("WORKSPACE_TOKEN", "1zzAnCvbknUuGogjQIBhkST0O4K")
 	os.Setenv("CONFIG_PATH", "./test_config.yaml")
 	config.Load()
+	logger.Init()
 	backendconfig.Init()
 	code := m.Run()
 
