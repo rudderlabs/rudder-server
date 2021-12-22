ed591f8a191521bc4c27eb87a9f0db4d81ae1313
Saurav Malani <sauravmalani1@gmail.com>
2021-12-22 15:50:40 +0530

    minor comment fix

--- regulation-worker/cmd/main_test.go
+++ regulation-worker/cmd/main_test.go
@@ -41,7 +41,6 @@ func handler() http.Handler {
 func run(m *testing.M) int {
 	svr := httptest.NewServer(handler())
 	defer svr.Close()
-	// workspaceID := "216Co97d9So9TkqphM0cxBzRxc3"
 	svcCtx, svcCancel := context.WithCancel(context.Background())
 	code := make(chan int, 1)
 	go func() {
