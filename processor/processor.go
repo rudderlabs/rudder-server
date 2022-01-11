411c420b0cb11636d83c5e39083055dafed3b7c0
Leonidas Vrachnis <leo.al.vra@gmail.com>
2022-01-11 11:42:34 +0530

    Remove spammy logs (#1561)

--- processor/processor.go
+++ processor/processor.go
@@ -1478,7 +1478,7 @@ func (proc *HandleT) Store(in storeMessage) {
 		in.procErrorJobs = append(in.procErrorJobs, jobs...)
 	}
 	if len(in.procErrorJobs) > 0 {
-		proc.logger.Info("[Processor] Total jobs written to proc_error: ", len(in.procErrorJobs))
+		proc.logger.Debug("[Processor] Total jobs written to proc_error: ", len(in.procErrorJobs))
 		err := proc.errorDB.Store(in.procErrorJobs)
 		if err != nil {
 			proc.logger.Errorf("Store into proc error table failed with error: %v", err)
@@ -1526,9 +1526,6 @@ func (proc *HandleT) Store(in storeMessage) {
 	proc.statRouterDBW.Count(len(destJobs))
 	proc.statBatchRouterDBW.Count(len(batchDestJobs))
 	proc.statProcErrDBW.Count(len(in.procErrorJobs))
-
-	proc.pStatsJobs.Print()
-	proc.pStatsDBW.Print()
 }
 
 type transformSrcDestOutput struct {
@@ -2002,8 +1999,6 @@ func (proc *HandleT) getJobs() []*jobsdb.JobT {
 	proc.statDBReadRequests.Observe(float64(len(unprocessedList)))
 	proc.statDBReadEvents.Observe(float64(totalEvents))
 	proc.statDBReadPayloadBytes.Observe(float64(totalPayloadBytes))
-
-	proc.pStatsDBR.Print()
 
 	return unprocessedList
 }
