89557bd237851f77aeed75f16c6c79caafff8360
Leonidas Vrachnis <leo.al.vra@gmail.com>
2022-01-22 10:08:07 +0530

    multi tenant noop implementation

--- services/multitenant/noop.go
+++ services/multitenant/noop.go
@@ -1,5 +1,11 @@
 package multitenant
 
+import (
+	"time"
+
+	"github.com/rudderlabs/rudder-server/utils/misc"
+)
+
 var NOOP = noop{}
 
 type noop struct{}
@@ -12,4 +18,16 @@ func (*noop) GenerateSuccessRateMap(destType string) (map[string]float64, map[st
 }
 
 func (*noop) AddToInMemoryCount(customerID string, destinationType string, count int, tableType string) {
+}
+
+func (*noop) GetRouterPickupJobs(destType string, recentJobInResultSet map[string]time.Time, sortedLatencyList []string, noOfWorkers int, routerTimeOut time.Duration, latencyMap map[string]misc.MovingAverage, jobQueryBatchSize int, successRateMap map[string]float64, drainedMap map[string]float64, timeGained float64) (map[string]int, map[string]float64) {
+	return nil, nil
+}
+
+func (*noop) RemoveFromInMemoryCount(customerID string, destinationType string, count int, tableType string) {
+	return
+}
+
+func (*noop) ReportProcLoopAddStats(stats map[string]map[string]int, timeTaken time.Duration, tableType string) {
+	return
 }
