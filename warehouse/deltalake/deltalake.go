2a5848a736c7a2c818768b5d9cdb516efdb9a476
Akash Chetty <achetty.iitr@gmail.com>
2022-02-10 16:14:34 +0530

    Added health call for databricks

--- warehouse/deltalake/deltalake.go
+++ warehouse/deltalake/deltalake.go
@@ -13,6 +13,7 @@ import (
 	"github.com/rudderlabs/rudder-server/warehouse/deltalake/databricks"
 	"github.com/rudderlabs/rudder-server/warehouse/utils"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health/grpc_health_v1"
 	"strings"
 	"time"
 )
@@ -45,6 +46,7 @@ var (
 	ssl                string
 	userAgent          string
 	grpcTimeout        time.Duration
+	healthTimeout      time.Duration
 )
 
 // Rudder data type mapping with Delta lake mappings.
@@ -116,6 +118,7 @@ func loadConfig() {
 	config.RegisterStringConfigVariable("1", &ssl, false, "Warehouse.deltalake.ssl")
 	config.RegisterStringConfigVariable("RudderStack", &userAgent, false, "Warehouse.deltalake.userAgent")
 	config.RegisterDurationConfigVariable(time.Duration(2), &grpcTimeout, false, time.Minute, "Warehouse.deltalake.grpcTimeout")
+	config.RegisterDurationConfigVariable(time.Duration(15), &healthTimeout, false, time.Second, "Warehouse.deltalake.healthTimeout")
 }
 
 // getDeltaLakeDataType returns datatype for delta lake which is mapped with rudder stack datatype
@@ -168,6 +171,11 @@ func checkAndIgnoreAlreadyExistError(errorCode string, ignoreError string) bool 
 
 // connect creates database connection with CredentialsT
 func (dl *HandleT) connect(cred *databricks.CredentialsT) (dbHandleT *databricks.DBHandleT, err error) {
+	if err := checkHealth(); err != nil {
+		err = fmt.Errorf("error connecting to databricks related deployement. Please contact Rudderstack support team")
+		return
+	}
+
 	connStat := stats.NewTaggedStat("warehouse.deltalake.grpcExecTime", stats.TimerType, map[string]string{
 		"destination": dl.Warehouse.Destination.ID,
 		"destType":    dl.Warehouse.Type,
@@ -211,10 +219,10 @@ func (dl *HandleT) connect(cred *databricks.CredentialsT) (dbHandleT *databricks
 	// Creating grpc connection using timeout context
 	conn, err := grpc.DialContext(tCtx, GetDatabricksConnectorURL(), grpc.WithInsecure(), grpc.WithBlock())
 	if err == context.DeadlineExceeded {
-		execTimeouts := stats.NewStat("warehouse.clickhouse.grpcTimeouts", stats.CountType)
+		execTimeouts := stats.NewStat("warehouse.deltalake.grpcTimeouts", stats.CountType)
 		execTimeouts.Count(1)
 
-		err = fmt.Errorf("connection timed out while initiating GRPC call with Databricks container. Please contact RudderStack team for more information")
+		err = fmt.Errorf("%s Connection timed out to Delta lake: %v", dl.GetLogIdentifier(), err)
 		return
 	}
 	if err != nil {
@@ -920,5 +928,38 @@ func GetDatabricksVersion() (databricksBuildVersion string) {
 		return
 	}
 	databricksBuildVersion = versionResponse.GetVersion()
+	return
+}
+
+// GetDatabricksVersion Gets the databricks version by making a grpc call to Version stub.
+func checkHealth() (err error) {
+	ctx := context.Background()
+	defer func() {
+		if err != nil {
+			healthTimeouts := stats.NewStat("warehouse.deltalake.healthTimeouts", stats.CountType)
+			healthTimeouts.Count(1)
+		}
+	}()
+
+	// Getting health timeout context
+	tCtx, cancel := context.WithTimeout(ctx, healthTimeout)
+	defer cancel()
+
+	// Creating grpc connection using timeout context
+	conn, err := grpc.DialContext(tCtx, GetDatabricksConnectorURL(), grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return
+	}
+
+	healthClient := grpc_health_v1.NewHealthClient(conn)
+	healthResponse, err := healthClient.Check(ctx, &grpc_health_v1.HealthCheckRequest{
+		Service: "",
+	})
+	if err != nil {
+		return
+	}
+	if healthResponse.GetStatus() != grpc_health_v1.HealthCheckResponse_SERVING {
+		err = fmt.Errorf("databricks Service is not up")
+	}
 	return
 }
