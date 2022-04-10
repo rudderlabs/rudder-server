357236e4f92826500dc54602790758ec54af88c3
Rohith BCS <rohith.bcs@gmail.com>
2022-04-10 20:25:41 +0530

    Remove testing stuff

--- services/streammanager/kafka/kafkamanager.go
+++ services/streammanager/kafka/kafkamanager.go
@@ -90,7 +90,6 @@ func loadCertificate() {
 
 func getDefaultConfiguration() *sarama.Config {
 	config := sarama.NewConfig()
-	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Net.DialTimeout = kafkaDialTimeout
 	config.Net.WriteTimeout = kafkaWriteTimeout
 	config.Net.ReadTimeout = kafkaWriteTimeout
