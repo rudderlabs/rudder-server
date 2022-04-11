3c71c73da0a332c7fb0f054b02d402e543b9bfc0
Aris Tzoumas <atzoumas@rudderstack.com>
2022-04-11 13:59:39 +0300

    refactor(jobsdb): use a request executor to encapsulate execution mode (concurrent or queued)
    In the previous implementation, each method that wanted to perform a read/write operation was repeating the same logic:
    
    Check if read/write should be done through a channel or not.
    If not, execute the function immediately.
    If a channel should be used, prepare another struct holding the request type, parameters & response channel, push this struct to the appropriate channel, the worker then would read the request, execute a different function depending on the request type and publish the response to the response channel.
    Now instead, we always create a command that holds the function to be executed and this is sent to an executor.
    The executor, depending on the runtime configuration of the system will either execute the function synchronously, or push it to the relevant channel and wait until a response is received.
    
    A few advantages introduced with this refactoring:
    
    DRY
    No need to copy function parameters around
    The executor doesn't need to know any details about the function being called

---
+++ jobsdb/queued_db_request.go
@@ -0,0 +1,76 @@
+package jobsdb
+
+import (
+	"fmt"
+)
+
+type queuedDbRequest struct {
+	execute  func() interface{}
+	response chan interface{}
+}
+
+func (jd *HandleT) executeDbRequest(c *dbRequest) interface{} {
+	totalTimeStat := jd.getTimerStat(fmt.Sprintf("%s_total_time", c.name), c.tags)
+	totalTimeStat.Start()
+	defer totalTimeStat.End()
+
+	var queueEnabled bool
+	var queueChannel chan *queuedDbRequest
+	switch c.reqType {
+	case readReqType:
+		queueEnabled = jd.enableReaderQueue
+		queueChannel = jd.readChannel
+	case writeReqType:
+		queueEnabled = jd.enableWriterQueue
+		queueChannel = jd.writeChannel
+	case undefinedReqType:
+		fallthrough
+	default:
+		panic(fmt.Errorf("unsupported command type: %d", c.reqType))
+	}
+
+	if queueEnabled {
+		waitTimeStat := jd.getTimerStat(fmt.Sprintf("%s_wait_time", c.name), c.tags)
+		waitTimeStat.Start()
+		respCh := make(chan interface{})
+		queueChannel <- &queuedDbRequest{execute: c.command, response: respCh}
+		waitTimeStat.End()
+		err := <-respCh
+		return err
+	}
+
+	return c.command()
+}
+
+type dbReqType int
+
+const (
+	undefinedReqType dbReqType = iota
+	readReqType
+	writeReqType
+)
+
+type dbRequest struct {
+	reqType dbReqType
+	name    string
+	tags    *StatTagsT
+	command func() interface{}
+}
+
+func newReadDbRequest(name string, tags *StatTagsT, command func() interface{}) *dbRequest {
+	return &dbRequest{
+		reqType: readReqType,
+		name:    name,
+		tags:    tags,
+		command: command,
+	}
+}
+
+func newWriteDbRequest(name string, tags *StatTagsT, command func() interface{}) *dbRequest {
+	return &dbRequest{
+		reqType: writeReqType,
+		name:    name,
+		tags:    tags,
+		command: command,
+	}
+}
