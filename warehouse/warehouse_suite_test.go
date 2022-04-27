7477a67e20f87688de724b0d67ab19aa623b59bb
Chandra shekar Varkala <chandra@rudderlabs.com>
2022-04-26 22:56:00 -0400

    chore: clean up buildspec & junit reporter

--- warehouse/warehouse_suite_test.go
+++ warehouse/warehouse_suite_test.go
@@ -5,11 +5,9 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	testutils "github.com/rudderlabs/rudder-server/utils/tests"
 )
 
 func TestUtils(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecsWithDefaultAndCustomReporters(t, "Warehouse Suite", []Reporter{testutils.NewJUnitReporter()})
+	RunSpecs(t, "Warehouse Suite")
 }
