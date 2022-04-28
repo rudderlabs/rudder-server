dab96217222069d49b63ea84969a68bbd8488864
chandumlg <54652834+chandumlg@users.noreply.github.com>
2022-04-28 02:04:33 -0400

    chore: removed unsed files (#1879)
    Co-authored-by: Chandra shekar Varkala <chandra@rudderlabs.com>

--- gateway/gateway_suite_test.go
+++ gateway/gateway_suite_test.go
@@ -5,11 +5,9 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	testutils "github.com/rudderlabs/rudder-server/utils/tests"
 )
 
 func TestGateway(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecsWithDefaultAndCustomReporters(t, "Gateway Suite", []Reporter{testutils.NewJUnitReporter()})
+	RunSpecs(t, "Gateway Suite")
 }
