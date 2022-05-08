8b6bb629f241299a92959e92d3b12de8901166bc
Akash Chetty <achetty.iitr@gmail.com>
2022-05-08 14:31:06 +0530

    Updated retry for sourceId and destinationType changes.

--- warehouse/retry.go
+++ warehouse/retry.go
@@ -1,35 +1,46 @@
 package warehouse
 
 import (
-	"database/sql"
 	"errors"
 	"fmt"
 	"github.com/lib/pq"
+	"github.com/rudderlabs/rudder-server/config"
+	"github.com/rudderlabs/rudder-server/utils/misc"
+	"strings"
 )
 
-type RetryReq struct {
-	WorkspaceID   string
-	DestinationID string
+var (
+	retryQueryPlaceHolder = "<retry>"
+)
 
+type RetryRequest struct {
+	WorkspaceID     string
+	SourceID        string
+	DestinationID   string
+	DestinationType string
 	IntervalInHours int64   // Optional, if provided we will retry based on the interval provided
 	UploadIds       []int64 // Optional, if provided we will retry the upload ids provided
-
-	API        UploadAPIT
-	ForceRetry bool
+	ForceRetry      bool
+	API             UploadAPIT
 }
 
-type RetryRes struct {
+type RetryResponse struct {
 	Message    string
 	StatusCode int32
 }
 
-func (retryReq *RetryReq) RetryWHUploads() (response RetryRes, err error) {
+func (retryReq *RetryRequest) RetryWHUploads() (response RetryResponse, err error) {
 	// Request validation
 	err = retryReq.validateReq()
 	defer func() {
 		if err != nil {
-			retryReq.API.log.Errorf("WH: Error occurred while retrying upload jobs with error: %s", err.Error())
-			response = RetryRes{
+			retryReq.API.log.Errorf(`WH: Error occurred while retrying upload jobs for workspaceId: %s, sourceId: %s, destinationId: %s with error: %s`,
+				retryReq.WorkspaceID,
+				retryReq.SourceID,
+				retryReq.DestinationID,
+				err.Error(),
+			)
+			response = RetryResponse{
 				Message:    err.Error(),
 				StatusCode: 400,
 			}
@@ -39,85 +50,145 @@ func (retryReq *RetryReq) RetryWHUploads() (response RetryRes, err error) {
 		return
 	}
 
-	// Retrying uploads
-	uploadsRetried, err := retryReq.retryUploads()
+	// Getting the corresponding sourceIDs from workspaceID (sourceIDsByWorkspace)
+	// Also, validating if the sources are empty or provided sourceId is present in the
+	// sources list ot not.
+	sourceIDs := retryReq.getSourceIDs()
+	if len(sourceIDs) == 0 {
+		err = fmt.Errorf("no sourceId's present to retry the jobs")
+		return
+	}
+	if !misc.ContainsString(sourceIDs, retryReq.SourceID) {
+		err = fmt.Errorf("no such sourceID exists")
+		return
+	}
+
+	// Retry request should trigger on these cases.
+	// 1. Either provide the retry interval.
+	// 2. Or provide the List of Upload id's that needs to be re-triggered.
+	uploadsRetried, err := retryReq.retryUploads(sourceIDs)
 	if err != nil {
 		return
 	}
 
-	response = RetryRes{
+	response = RetryResponse{
 		Message:    retryReq.successMessage(uploadsRetried),
 		StatusCode: 200,
 	}
 	return
 }
 
-func (retryReq *RetryReq) retryUploads() (rowsAffected int64, err error) {
-	var retryClause, statusClause string
-	if len(retryReq.UploadIds) != 0 {
-		retryClause = `id = ANY($1)`
-	} else {
-		retryClause = `destination_id = $1 AND created_at > NOW() - $2 * INTERVAL '1 HOUR'`
+func (retryReq *RetryRequest) getSourceIDs() (sourceIDs []string) {
+	sourceIDsByWorkspaceLock.RLock()
+	defer sourceIDsByWorkspaceLock.RUnlock()
+	var ok bool
+	if sourceIDs, ok = sourceIDsByWorkspace[retryReq.WorkspaceID]; !ok {
+		sourceIDs = []string{}
 	}
-	if !retryReq.ForceRetry {
-		statusClause = fmt.Sprintf("AND status = '%s'", Aborted)
+	return sourceIDs
+}
+
+func (retryReq *RetryRequest) retryUploads(sourceIDs []string) (rowsAffected int64, err error) {
+	var clausesQuery string
+	var clauses []string
+	var clausesArgs []interface{}
+
+	// Preparing clauses query
+	clauses, clausesArgs = retryReq.whereClauses(sourceIDs)
+	for i, clause := range clauses {
+		clausesQuery = clausesQuery + strings.Replace(clause, retryQueryPlaceHolder, fmt.Sprintf("$%d", i), 1) + " AND "
 	}
 
-	sqlPreparedStatement := fmt.Sprintf(`
+	// Preparing the prepared statement
+	preparedStatement := fmt.Sprintf(`
 		UPDATE wh_uploads
 		SET
 			metadata = metadata || '{"retried": true, "priority": 50}' || jsonb_build_object('nextRetryTime', NOW() - INTERVAL '1 HOUR'),
 			status = 'waiting',
 			updated_at = NOW()
-		WHERE %[1]s %[2]s`,
-		retryClause,
-		statusClause,
+		WHERE %[1]s`,
+		clausesQuery,
 	)
-	retryReq.API.log.Info(sqlPreparedStatement)
+	retryReq.API.log.Info(preparedStatement)
 
-	var res sql.Result
-	if len(retryReq.UploadIds) != 0 {
-		res, err = retryReq.API.dbHandle.Exec(sqlPreparedStatement, pq.Array(retryReq.UploadIds))
-	} else {
-		res, err = retryReq.API.dbHandle.Exec(sqlPreparedStatement, retryReq.DestinationID, retryReq.IntervalInHours)
-	}
+	// Executing the statement
+	result, err := retryReq.API.dbHandle.Exec(preparedStatement, clausesArgs)
 	if err != nil {
 		return
 	}
 
 	// Getting rows affected
-	rowsAffected, err = res.RowsAffected()
+	rowsAffected, err = result.RowsAffected()
 	return
 }
 
-func (retryReq *RetryReq) successMessage(uploadsRetried int64) string {
+func (retryReq *RetryRequest) whereClauses(sourceIDs []string) (clauses []string, clausesArgs []interface{}) {
+	// SourceID
+	if retryReq.SourceID != "" {
+		clauses = append(clauses, fmt.Sprintf(`source_id = %s`, retryQueryPlaceHolder))
+		clausesArgs = append(clausesArgs, retryReq.SourceID)
+	} else {
+		clauses = append(clauses, fmt.Sprintf(`source_id = ANY(%s)`, retryQueryPlaceHolder))
+		clausesArgs = append(clausesArgs, pq.Array(sourceIDs))
+	}
+
+	// DestinationID
+	if retryReq.DestinationID != "" {
+		clauses = append(clauses, fmt.Sprintf(`destination_id = %s`, retryQueryPlaceHolder))
+		clausesArgs = append(clausesArgs, retryReq.DestinationID)
+	}
+
+	// DestinationType
+	if retryReq.DestinationType != "" {
+		clauses = append(clauses, fmt.Sprintf(`destination_type = %s`, retryQueryPlaceHolder))
+		clausesArgs = append(clausesArgs, retryReq.DestinationType)
+	}
+
+	// Status deciding based on forcefully retrying
+	if !retryReq.ForceRetry {
+		clauses = append(clauses, fmt.Sprintf("status = %s", retryQueryPlaceHolder))
+		clausesArgs = append(clausesArgs, Aborted)
+	}
+
+	// UploadIDs or Retry Interval
+	if len(retryReq.UploadIds) != 0 {
+		clauses = append(clauses, fmt.Sprintf("id = ANY(%s)", retryQueryPlaceHolder))
+		clausesArgs = append(clausesArgs, pq.Array(retryReq.UploadIds))
+	} else {
+		clauses = append(clauses, fmt.Sprintf("created_at > NOW() - %s * INTERVAL '1 HOUR'", retryQueryPlaceHolder))
+		clausesArgs = append(clausesArgs, retryReq.GetRetryInterval())
+	}
+	return
+}
+
+func (retryReq *RetryRequest) successMessage(uploadsRetried int64) string {
 	if uploadsRetried == 0 {
 		return "No retried uploads to sync for this destination"
 	}
 	return fmt.Sprintf("Retried successfully %d syncs", uploadsRetried)
 }
 
-// Retry request should trigger on these cases.
-// 1. Either provide the retry interval.
-// 2. List of Upload id's that needs to be re-triggered.
-func (retryReq RetryReq) validateReq() (err error) {
+func (retryReq *RetryRequest) validateReq() (err error) {
 	if !retryReq.API.enabled || retryReq.API.log == nil || retryReq.API.dbHandle == nil {
-		err = errors.New("warehouse api's are not initialized")
+		err = errors.New("warehouse service is not initialized")
 		return
 	}
 
-	// Checking for either IntervalInHours or UploadIds present.
-	if retryReq.IntervalInHours == 0 && len(retryReq.UploadIds) == 0 {
-		err = errors.New("please verify either uploadIds or retry intervalInHours is provided in the request")
-		return
-	}
-
-	// Check for the valid destination id as per the connection map present in warehouse.
-	_, ok := connectionsMap[retryReq.DestinationID]
-	if !ok {
-		pkgLogger.Errorf(`Unauthorized request for workspaceId: %s in sourceId: %s`, retryReq.WorkspaceID, retryReq.DestinationID)
-		err = errors.New("unauthorized request")
+	if retryReq.SourceID == "" && retryReq.DestinationID == "" && retryReq.WorkspaceID == "" {
+		retryReq.API.log.Errorf(`WH: Please provide valid request parameters while retrying jobs for workspaceId: %s, sourceId: %s, destinationId: %s`,
+			retryReq.WorkspaceID,
+			retryReq.SourceID,
+			retryReq.DestinationID,
+		)
+		err = errors.New("please provide valid request parameters while retrying jobs with workspaceId or sourceId or destinationId")
 		return
 	}
 	return
+}
+
+func (retryReq *RetryRequest) GetRetryInterval() int64 {
+	if retryReq.IntervalInHours <= 0 {
+		return config.GetInt64("Warehouse.retryIntervalInHrs", 12)
+	}
+	return retryReq.IntervalInHours
 }
