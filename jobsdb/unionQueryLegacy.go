ad3e363cf349e8a37c8b1f6891a4f3dedf91fef4
Aris Tzoumas <atzoumas@rudderstack.com>
2022-05-09 10:33:15 +0300

    peer programming

--- jobsdb/unionQueryLegacy.go
+++ jobsdb/unionQueryLegacy.go
@@ -5,22 +5,46 @@ type MultiTenantLegacy struct {
 }
 
 func (mj *MultiTenantLegacy) GetAllJobs(workspaceCount map[string]int, params GetQueryParamsT, _ int) []*JobT {
+	var list []*JobT
 	toQuery := 0
 	for workspace := range workspaceCount {
 		toQuery += workspaceCount[workspace]
 	}
 	params.JobsLimit = toQuery
+	payloadLimitEnabled := params.PayloadSizeLimit > 0
 
 	retryList := mj.GetToRetry(params)
-	params.JobsLimit -= len(retryList)
-	waitList := mj.GetWaiting(params)
-	params.JobsLimit -= len(waitList)
-	unprocessedList := mj.GetUnprocessed(params)
-
-	var list []*JobT
 	list = append(list, retryList...)
-	list = append(list, waitList...)
-	list = append(list, unprocessedList...)
+	updateParams(&params, retryList)
+	if limitsReached(payloadLimitEnabled, params) {
+		return list
+	}
 
+	waitList := mj.GetWaiting(params)
+	list = append(list, waitList...)
+	updateParams(&params, waitList)
+	if limitsReached(payloadLimitEnabled, params) {
+		return list
+	}
+
+	unprocessedList := mj.GetUnprocessed(params)
+	list = append(list, unprocessedList...)
 	return list
+}
+
+func updateParams(params *GetQueryParamsT, jobs []*JobT) {
+	params.JobsLimit -= len(jobs)
+	params.PayloadSizeLimit -= getTotalPayload(jobs)
+}
+
+func getTotalPayload(jobs []*JobT) int64 {
+	var total int64
+	for i := range jobs {
+		total += jobs[i].PayloadSize
+	}
+	return total
+}
+
+func limitsReached(payloadLimitEnabled bool, params GetQueryParamsT) bool {
+	return params.JobsLimit <= 0 || (payloadLimitEnabled && params.PayloadSizeLimit <= 0)
 }
