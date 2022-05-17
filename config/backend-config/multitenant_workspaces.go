72a429c1721ed0d00c274098c799c14cd483f935
Kuldeep <kuldeep0020@outlook.com>
2022-05-17 14:01:25 +0530

    added comments

--- config/backend-config/multitenant_workspaces.go
+++ config/backend-config/multitenant_workspaces.go
@@ -74,7 +74,7 @@ func (workspaceConfig *MultiTenantWorkspacesConfig) Get(workspaces string) (Conf
 
 // getFromApi gets the workspace config from api
 func (workspaceConfig *MultiTenantWorkspacesConfig) getFromAPI(workspaceArr string) (ConfigT, bool) {
-	// added this to avoid unnecessary calls to backend config until workspace IDs are not present
+	// added this to avoid unnecessary calls to backend config and log better until workspace IDs are not present
 	if workspaceArr == workspaceConfig.Token {
 		pkgLogger.Infof("no workspace IDs provided, skipping backend config fetch")
 		return ConfigT{}, false
