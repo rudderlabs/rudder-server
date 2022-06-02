e28ca19bf39f58f3618d808c7f001aaeee9231cf
Leonidas Vrachnis <leo.al.vra@gmail.com>
2022-06-02 22:00:48 +0300

    chore: move ent builds to github actions

--- build/codecov.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-# Because we use codebuild as part of codepipeline, following code is required to fix CODEBUILD_SOURCE_VERSION
-# Example value of CODEBUILD_SOURCE_VERSION
-# CODEBUILD_SOURCE_VERSION="arn:aws:s3:::docker-codebuild-devnull/rudder-server_pr-366/source_out/NAn9t2N.zip"
-# Expected value of CODEBUILD_SOURCE_VERSION for pr to be picked correctly is 
-# CODEBUILD_SOURCE_VERSION="pr/366"
-
-bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
