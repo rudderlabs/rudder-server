201ae11d9ee76d44036efdc4035be1f0c7bef313
Sidddddarth <siddarth@rudderstack.com>
2022-06-03 16:57:13 +0530

    feat.RSourcesMultiTenant: pub-sub for sources jobs stats in a multi-tenant scenario

--- services/rsources/internal/extension/multitenant.go
+++ services/rsources/internal/extension/multitenant.go
@@ -3,6 +3,11 @@ package extension
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pq"
+
+	"github.com/rudderlabs/rudder-server/config"
+	"github.com/rudderlabs/rudder-server/jobsdb"
 )
 
 type multitenantExtension struct {
@@ -29,34 +34,55 @@ func (r *multitenantExtension) GetReadDB() *sql.DB {
 	return r.sharedDB
 }
 
-func (*multitenantExtension) setupStatsTable(_ context.Context) error {
+func (r *multitenantExtension) setupStatsTable(ctx context.Context) error {
+
 	// ## On local DB
-	// 0. stats table already created above during `newMultitenantExtension -> newDefaultExtension`
-	// 1. Create publication, 1 publication for all tables (ignore already exists error)
-	// 2. Add table to publication if not already (ignore is already member of publication error)
+
+	// Create publication, 1 publication for the stats table (ignore already exists error)
+	connectionName := config.GetEnv("JOBS_DB_HOST", "localhost") //using hostname
+	publicationCreationQuery := `CREATE PUBLICATION "$1" FOR TABLE rsources_stats`
+	_, err := r.Extension.(*standardExtension).localDB.ExecContext(ctx, publicationCreationQuery, connectionName)
+	if err != nil {
+		pqError := err.(pq.Error)
+		if pqError.Code != pq.ErrorCode("42710") { // duplicate
+			return err
+		}
+	}
 
 	// ## On remote DB
-	// 1. Create subscription, 1 subscription for all tables (ignore already exists error)
-	// 2. Create table if not exists
-	// 2. Add table to subscription if not already (ignore is already member of subscription error)
+
+	// Create table if not exists
+
+	// using this dummy-extension to create table in remote db
+	// TODO verify this is safe
+	dummyStandardExtension := &standardExtension{
+		localDB: r.sharedDB,
+	}
+	err = dummyStandardExtension.setupStatsTable(ctx) //check if dbname column defaults to proper value(not remote db hostname)
+	if err != nil {
+		return err
+	}
+
+	// Create subscription for the above publication (ignore already exists error)
+	subscriptionCreationQuery := `CREATE SUBSCRIPTION $2 CONNECTION $1 PUBLICATION $2`
+	_, err = r.sharedDB.ExecContext(ctx, subscriptionCreationQuery, jobsdb.GetConnectionString(), connectionName)
+	if err != nil {
+		pqError := err.(pq.Error)
+		if pqError.Code != pq.ErrorCode("42710") { // duplicate
+			return err
+		}
+	}
+
 	return nil
 }
 
 func (r *multitenantExtension) DropStats(ctx context.Context, jobRunId string) error {
-	err := r.Extension.DropStats(ctx, jobRunId)
-	if err != nil {
-		return err
-	}
-	// ## On local DB
-	// 1. Remove table from publication
-
-	// ## On remote DB
-	// 1. Remove table from subscription
-
-	return nil
+	return r.Extension.DropStats(ctx, jobRunId)
 }
 
-func (*multitenantExtension) CleanupLoop(_ context.Context) error {
-	// TODO
-	return nil
+func (r *multitenantExtension) CleanupLoop(ctx context.Context) error {
+	// if all workspaces are to be treated equally we can simply go this way
+	return r.Extension.CleanupLoop(ctx)
+
+	// TODO: in case CleanupLoop to be called wrt to a specific workspace(job_run_id)
 }
