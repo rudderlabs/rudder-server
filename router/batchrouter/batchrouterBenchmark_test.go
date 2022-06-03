0f280eafccb11d87201e6e3627616352814fbd6b
Akash Chetty <achetty.iitr@gmail.com>
2022-06-03 15:04:17 +0530

    Added random string

--- router/batchrouter/batchrouterBenchmark_test.go
+++ router/batchrouter/batchrouterBenchmark_test.go
@@ -1,10 +1,12 @@
 package batchrouter
 
 import (
+	"github.com/gofrs/uuid"
 	"github.com/golang/mock/gomock"
 	"github.com/rudderlabs/rudder-server/config"
 	backendconfig "github.com/rudderlabs/rudder-server/config/backend-config"
 	mocksFileManager "github.com/rudderlabs/rudder-server/mocks/services/filemanager"
+	"strings"
 	"testing"
 )
 
@@ -15,21 +17,24 @@ func Benchmark_GetStorageDateFormat(b *testing.B) {
 	mockCtrl := gomock.NewController(b)
 	mockFileManager := mocksFileManager.NewMockFileManager(mockCtrl)
 	destination := &DestinationT{
-		Source: backendconfig.SourceT{
-			ID: "sourceID",
-		},
-		Destination: backendconfig.DestinationT{
-			ID: "destinationID",
-		},
+		Source:      backendconfig.SourceT{},
+		Destination: backendconfig.DestinationT{},
 	}
 	folderName := ""
 
 	b.SetParallelism(2)
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
+			destination.Destination.ID = randomString()
+			destination.Source.ID = randomString()
+
 			mockFileManager.EXPECT().GetConfiguredPrefix().AnyTimes()
 			mockFileManager.EXPECT().ListFilesWithPrefix(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
 			_, _ = GetStorageDateFormat(mockFileManager, destination, folderName)
 		}
 	})
+}
+
+func randomString() string {
+	return strings.ReplaceAll(uuid.Must(uuid.NewV4()).String(), "-", "")
 }
