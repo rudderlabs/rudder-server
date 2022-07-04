a7de8a212383375a58d6d29338e390c6c3b5c806
Akash Chetty <achetty.iitr@gmail.com>
2022-07-04 18:24:28 +0530

    trackingplan_test.go code cleanup.

--- processor/trackingplan_test.go
+++ processor/trackingplan_test.go
@@ -8,59 +8,112 @@ import (
 )
 
 func TestReportViolations(t *testing.T) {
-	var (
-		validateEvent       *transformer.TransformerResponseT
-		trackingPlanId      string
-		trackingPlanVersion int
-	)
+	eventsFromTransformerResponse := func(response *transformer.ResponseT) (events []transformer.TransformerResponseT) {
+		events = append(events, response.Events...)
+		events = append(events, response.FailedEvents...)
+		return
+	}
 
-	// Propagate validation errors
-	validateEvent = &transformer.TransformerResponseT{
-		Metadata: transformer.MetadataT{
-			MergedTpConfig: map[string]interface{}{
-				"propagateValidationErrors": "true",
-			},
-		},
-		Output: map[string]interface{}{
-			"context": map[string]interface{}{},
-		},
-		ValidationErrors: []transformer.ValidationErrorT{
-			{
-				Type: "Datatype-Mismatch",
-				Meta: map[string]string{
-					"schemaPath":  "#/properties/properties/properties/price/type",
-					"instacePath": "/properties/price",
+	t.Run("Not propagating validation errors", func(t *testing.T) {
+		var (
+			trackingPlanId      string
+			trackingPlanVersion int
+		)
+
+		response := transformer.ResponseT{
+			Events: []transformer.TransformerResponseT{
+				{
+					Metadata: transformer.MetadataT{
+						MergedTpConfig: map[string]interface{}{
+							"propagateValidationErrors": "false",
+						},
+					},
+					Output: map[string]interface{}{
+						"context": map[string]interface{}{},
+					},
 				},
-				Message: "must be number",
 			},
-		},
-	}
-
-	trackingPlanId = "tp_2BFrdaslxH9A7B2hSDFKxw8wPN6knOb57"
-	trackingPlanVersion = 1
-
-	reportViolations(validateEvent, trackingPlanId, trackingPlanVersion)
-	eventContext, castOk := validateEvent.Output["context"].(map[string]interface{})
-	assert.True(t, castOk)
-	assert.Equal(t, eventContext["trackingPlanId"], trackingPlanId)
-	assert.Equal(t, eventContext["trackingPlanVersion"], trackingPlanVersion)
-	assert.Equal(t, eventContext["violationErrors"], validateEvent.ValidationErrors)
-
-	// Don't propagate validation error
-	validateEvent = &transformer.TransformerResponseT{
-		Metadata: transformer.MetadataT{
-			MergedTpConfig: map[string]interface{}{
-				"propagateValidationErrors": "false",
+			FailedEvents: []transformer.TransformerResponseT{
+				{
+					Metadata: transformer.MetadataT{
+						MergedTpConfig: map[string]interface{}{
+							"propagateValidationErrors": "false",
+						},
+					},
+					Output: map[string]interface{}{
+						"context": map[string]interface{}{},
+					},
+				},
 			},
-		},
-		Output: map[string]interface{}{
-			"context": map[string]interface{}{},
-		},
-	}
-	reportViolations(validateEvent, trackingPlanId, trackingPlanVersion)
-	eventContext, castOk = validateEvent.Output["context"].(map[string]interface{})
-	assert.True(t, castOk)
-	assert.Nil(t, eventContext["trackingPlanId"])
-	assert.Nil(t, eventContext["trackingPlanVersion"])
-	assert.Nil(t, eventContext["violationErrors"])
+		}
+
+		enhanceWithViolation(response, trackingPlanId, trackingPlanVersion)
+		for _, event := range eventsFromTransformerResponse(&response) {
+			eventContext, castOk := event.Output["context"].(map[string]interface{})
+			assert.True(t, castOk)
+			assert.Nil(t, eventContext["trackingPlanId"])
+			assert.Nil(t, eventContext["trackingPlanVersion"])
+			assert.Nil(t, eventContext["violationErrors"])
+		}
+	})
+
+	t.Run("Propagate validation errors", func(t *testing.T) {
+		response := transformer.ResponseT{
+			Events: []transformer.TransformerResponseT{
+				{
+					Metadata: transformer.MetadataT{
+						MergedTpConfig: map[string]interface{}{
+							"propagateValidationErrors": "true",
+						},
+					},
+					Output: map[string]interface{}{
+						"context": map[string]interface{}{},
+					},
+					ValidationErrors: []transformer.ValidationErrorT{
+						{
+							Type: "Datatype-Mismatch",
+							Meta: map[string]string{
+								"schemaPath":  "#/properties/properties/properties/price/type",
+								"instacePath": "/properties/price",
+							},
+							Message: "must be number",
+						},
+					},
+				},
+			},
+			FailedEvents: []transformer.TransformerResponseT{
+				{
+					Metadata: transformer.MetadataT{
+						MergedTpConfig: map[string]interface{}{
+							"propagateValidationErrors": "true",
+						},
+					},
+					Output: map[string]interface{}{
+						"context": map[string]interface{}{},
+					},
+					ValidationErrors: []transformer.ValidationErrorT{
+						{
+							Type: "Datatype-Mismatch",
+							Meta: map[string]string{
+								"schemaPath":  "#/properties/properties/properties/price/type",
+								"instacePath": "/properties/price",
+							},
+							Message: "must be number",
+						},
+					},
+				},
+			},
+		}
+		trackingPlanId := "tp_2BFrdaslxH9A7B2hSDFKxw8wPN6knOb57"
+		trackingPlanVersion := 1
+
+		enhanceWithViolation(response, trackingPlanId, trackingPlanVersion)
+		for _, event := range eventsFromTransformerResponse(&response) {
+			eventContext, castOk := event.Output["context"].(map[string]interface{})
+			assert.True(t, castOk)
+			assert.Equal(t, eventContext["trackingPlanId"], trackingPlanId)
+			assert.Equal(t, eventContext["trackingPlanVersion"], trackingPlanVersion)
+			assert.Equal(t, eventContext["violationErrors"], event.ValidationErrors)
+		}
+	})
 }
