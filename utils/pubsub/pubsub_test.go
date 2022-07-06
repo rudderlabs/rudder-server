306e0f26f3c688a13ef1bb905d27c42f178b69a1
Aris Tzoumas <atzoumas@rudderstack.com>
2022-07-06 21:41:20 +0300

    fix pubsub flaky test

--- utils/pubsub/pubsub_test.go
+++ utils/pubsub/pubsub_test.go
@@ -37,7 +37,13 @@ func TestPubSub_two_consumers_slow_and_fast(t *testing.T) {
 
 	// slow consumer
 	t.Log("slow consumer should consume only the latest one, 5")
+
 	v = <-slow
+
+	if v.Data.(int) == 2 { // due to pubsub internal channels, the slow consumer may consume 2 before consuming 5
+		v = <-slow
+	}
+
 	if v.Data.(int) != 5 {
 		t.Errorf("Expected slow consumer to have consumed 5, instead got %d.", v.Data.(int))
 	}
