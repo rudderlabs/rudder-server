8151faa536d111f752baacfa0a176fbd9e0687cb
Akash Chetty <achetty.iitr@gmail.com>
2022-07-17 00:15:02 +0530

    go fmt changes.

--- warehouse/query_builder.go
+++ warehouse/query_builder.go
@@ -12,7 +12,8 @@ func distinctDestinationRevisionIdsFromStagingFiles(d struct {
 	destinationID      string
 	startStagingFileID int64
 	endStagingFileID   int64
-}) (revisionIDs []string, err error) {
+},
+) (revisionIDs []string, err error) {
 	sqlStatement := fmt.Sprintf(`
 		SELECT
 		  DISTINCT metadata ->> 'destination_revision_id' AS destination_revision_id
