5bf246cce953e078720d3c3e92b8e33e408e0dc5
Akash Chetty <achetty.iitr@gmail.com>
2022-07-21 12:03:18 +0530

    Added test suite for slave for PickupStagingFileBucket

--- warehouse/slave.go
+++ warehouse/slave.go
@@ -160,7 +160,7 @@ func (jobRun *JobRunT) downloadStagingFile() error {
 
 	err := downloadTask(job.CurrentDestinationConfig, job.CurrentUseRudderStorage)
 	if err != nil {
-		if job.StagingDestinationRevisionID != job.CurrentDestinationRevisionID && job.StagingDestinationConfig != nil {
+		if PickupStagingFileBucket(&job) {
 			pkgLogger.Infof("[WH]: Starting processing staging file with revision config for StagingFileID: %d, CurrentDestinationRevisionID: %s, StagingDestinationRevisionID: %s, whIdentifier: %s", job.StagingFileID, job.CurrentDestinationRevisionID, job.StagingDestinationRevisionID, jobRun.whIdentifier)
 			err = downloadTask(job.StagingDestinationConfig, job.StagingUseRudderStorage)
 			if err != nil {
@@ -172,6 +172,10 @@ func (jobRun *JobRunT) downloadStagingFile() error {
 		}
 	}
 	return nil
+}
+
+func PickupStagingFileBucket(job *PayloadT) bool {
+	return job.StagingDestinationRevisionID != job.CurrentDestinationRevisionID && job.StagingDestinationConfig != nil
 }
 
 func (job *PayloadT) getDiscardsTable() string {
@@ -394,18 +398,7 @@ func processStagingFile(job PayloadT, workerIndex int) (loadFileUploadOutputs []
 	// This creates the file, so on successful creation remove it
 	err = jobRun.downloadStagingFile()
 	if err != nil {
-		// If error occurs with the current config and current revision is different from staging revision
-		// We retry with the staging revision config if it is present
-		if job.StagingDestinationRevisionID != job.CurrentDestinationRevisionID && job.StagingDestinationConfig != nil {
-			pkgLogger.Infof("[WH]: Starting processing staging file with revision config for StagingFileID: %d, CurrentDestinationRevisionID: %s, StagingDestinationRevisionID: %s, whIdentifier: %s", job.StagingFileID, job.CurrentDestinationRevisionID, job.StagingDestinationRevisionID, jobRun.whIdentifier)
-			err = jobRun.downloadStagingFile()
-			if err != nil {
-				job.sendDownloadStagingFileFailedStat()
-				return loadFileUploadOutputs, err
-			}
-		} else {
-			return loadFileUploadOutputs, err
-		}
+		return loadFileUploadOutputs, err
 	}
 
 	sortedTableColumnMap := job.getSortedColumnMapForAllTables()
---
+++ warehouse/slave_test.go
@@ -0,0 +1,44 @@
+package warehouse
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestPickupStagingFileBucket(t *testing.T) {
+	inputs := []struct {
+		job      *PayloadT
+		expected bool
+	}{
+		{
+			job:      &PayloadT{},
+			expected: false,
+		},
+		{
+			job: &PayloadT{
+				StagingDestinationRevisionID: "1liYatjkkCEVkEMYUmSWOE9eZ4n",
+				CurrentDestinationRevisionID: "1liYatjkkCEVkEMYUmSWOE9eZ4n",
+			},
+			expected: false,
+		},
+		{
+			job: &PayloadT{
+				StagingDestinationRevisionID: "1liYatjkkCEVkEMYUmSWOE9eZ4n",
+				CurrentDestinationRevisionID: "2liYatjkkCEVkEMYUmSWOE9eZ4n",
+			},
+			expected: false,
+		},
+		{
+			job: &PayloadT{
+				StagingDestinationRevisionID: "1liYatjkkCEVkEMYUmSWOE9eZ4n",
+				CurrentDestinationRevisionID: "2liYatjkkCEVkEMYUmSWOE9eZ4n",
+				StagingDestinationConfig:     map[string]string{},
+			},
+			expected: true,
+		},
+	}
+	for _, input := range inputs {
+		got := PickupStagingFileBucket(input.job)
+		require.Equal(t, got, input.expected)
+	}
+}
