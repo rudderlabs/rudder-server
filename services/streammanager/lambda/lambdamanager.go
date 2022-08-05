f6fa730ae86bbbeda4be5caa939f3332aea41d78
sandeepdigumarty <sandeepdsvs@gmail.com>
2022-08-05 10:28:35 +0530

    Updated error handling and added suggestions from review comments

--- services/streammanager/lambda/lambdamanager.go
+++ services/streammanager/lambda/lambdamanager.go
@@ -3,6 +3,8 @@ package lambda
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/tidwall/gjson"
 	"net/http"
 	"time"
@@ -14,11 +16,16 @@ import (
 	"github.com/rudderlabs/rudder-server/utils/logger"
 )
 
+var jsonfast = jsoniter.ConfigCompatibleWithStandardLibrary
+
 // Config is the config that is required to send data to Lambda
 type Config struct {
-	Region      string
-	AccessKeyID string
-	AccessKey   string
+	Region          string
+	AccessKeyID     string
+	SecretAccessKey string
+	InvocationType  string
+	ClientContext   string
+	Lambda          string
 }
 
 type Opts struct {
@@ -28,69 +35,111 @@ type Opts struct {
 var pkgLogger logger.LoggerI
 
 func init() {
-	pkgLogger = logger.NewLogger().Child("streammanager").Child("firehose")
+	pkgLogger = logger.NewLogger().Child("streammanager").Child("lambda")
 }
 
 // NewProducer creates a producer based on destination config
-func NewProducer(destinationConfig interface{}, o Opts) (lambda.Lambda, error) {
+func NewProducer(destinationConfig interface{}, o Opts) (*lambda.Lambda, error) {
 	var config Config
-	jsonConfig, err := json.Marshal(destinationConfig)
+	jsonConfig, err := jsonfast.Marshal(destinationConfig)
 	if err != nil {
-		return lambda.Lambda{}, fmt.Errorf("[Lambda] Error while marshalling destination config :: %w", err)
+		return nil, fmt.Errorf("[Lambda] error :: error while marshalling destination config :: %w", err)
 	}
-	err = json.Unmarshal(jsonConfig, &config)
+	err = jsonfast.Unmarshal(jsonConfig, &config)
 	if err != nil {
-		return lambda.Lambda{}, fmt.Errorf("[Lambda] error  :: error in Lambda while unmarshelling destination config:: %w", err)
+		return nil, fmt.Errorf("[Lambda] error :: error while unmarshelling destination config :: %w", err)
 	}
+	if config.Region == "" {
+		return nil, fmt.Errorf("[Lambda] error :: Region is a required property")
+	}
+
 	var s *session.Session
 	httpClient := &http.Client{
 		Timeout: o.Timeout,
 	}
-
-	if config.AccessKeyID == "" || config.AccessKey == "" {
-		s = session.Must(session.NewSession(&aws.Config{
+	if config.AccessKeyID == "" || config.SecretAccessKey == "" {
+		s, err = session.NewSession(&aws.Config{
 			Region:     aws.String(config.Region),
 			HTTPClient: httpClient,
-		}))
+		})
 	} else {
-		s = session.Must(session.NewSession(&aws.Config{
+		s, err = session.NewSession(&aws.Config{
 			HTTPClient:  httpClient,
 			Region:      aws.String(config.Region),
-			Credentials: credentials.NewStaticCredentials(config.AccessKeyID, config.AccessKey, ""),
-		}))
+			Credentials: credentials.NewStaticCredentials(config.AccessKeyID, config.SecretAccessKey, ""),
+		})
 	}
-	var lambdaClient *lambda.Lambda = lambda.New(s)
-	return *lambdaClient, nil
+	if err != nil {
+		return nil, fmt.Errorf("[Lambda] error :: error while creating session :: %w", err)
+	}
+
+	return lambda.New(s), nil
 }
 
 // Produce creates a producer and send data to Lambda.
-func Produce(jsonData json.RawMessage, producer, destConfig interface{}) (statusCode int, respStatus, responseMessage string) {
-	client, ok := producer.(lambda.Lambda)
+func Produce(jsonData json.RawMessage, producer, destConfig interface{}) (int, string, string) {
+	client, ok := producer.(*lambda.Lambda)
 	if !ok {
-		respStatus = "Failure"
-		responseMessage = "[Lambda] error :: Could not create producer"
-		pkgLogger.Errorf("[Lambda] error :: Could not create producer")
-		return 400, respStatus, responseMessage
+		return simpleErrorResponse("Could not create client")
 	}
-	parsedJSON := gjson.ParseBytes(jsonData)
-	data := parsedJSON.Get("message").String()
-	//clientContext := parsedJSON.Get("clientContext").String()
-	invocationType := parsedJSON.Get("invocationType").String()
-	lambdaFunction := parsedJSON.Get("lambda").String()
+	data := gjson.GetBytes(jsonData, "payload").String()
+	if data == "" {
+		return simpleErrorResponse("Invalid payload")
+	}
+
+	var config Config
+	jsonConfig, err := jsonfast.Marshal(destConfig)
+	if err != nil {
+		return simpleErrorResponse("error while marshalling destination config :: " + err.Error())
+	}
+	err = jsonfast.Unmarshal(jsonConfig, &config)
+	if err != nil {
+		return simpleErrorResponse("error while unmarshalling destination config :: " + err.Error())
+	}
+	if config.Lambda == "" {
+		return simpleErrorResponse("Lambda function name is a required property")
+	}
+	if config.InvocationType == "" {
+		return simpleErrorResponse("InvocationType is a required property")
+	}
 
 	var input lambda.InvokeInput
-	input.FunctionName = aws.String(lambdaFunction)
+	input.FunctionName = aws.String(config.Lambda)
 	input.Payload = []byte(data)
-	input.InvocationType = aws.String(invocationType)
-
-	res, err := client.Invoke(&input)
-	if err != nil {
-		statusCode = int(*res.StatusCode)
-		respStatus = "Failure"
-		responseMessage = "[Lambda] error while Invoking the function ::" + err.Error()
+	input.InvocationType = aws.String(config.InvocationType)
+	if config.ClientContext != "" {
+		input.ClientContext = aws.String(config.ClientContext)
 	}
-	statusCode = int(*res.StatusCode)
-	respStatus = "Success"
-	responseMessage = "Message delivered"
-	return statusCode, respStatus, responseMessage
+
+	_, err = client.Invoke(&input)
+	if err != nil {
+		return invokeErrorResponse(err)
+	}
+
+	responseMessage := fmt.Sprintf("Event delivered to Lambda : %s", config.Lambda)
+	return 200, "Success", responseMessage
+}
+
+func simpleErrorResponse(message string) (int, string, string) {
+	respStatus := "Failure"
+	returnMessage := "[Lambda] error :: " + message
+	pkgLogger.Errorf(returnMessage)
+	return 400, respStatus, returnMessage
+}
+
+func invokeErrorResponse(err error) (int, string, string) {
+	if awsErr, ok := err.(awserr.Error); ok {
+		switch awsErr.Code() {
+		case lambda.ErrCodeTooManyRequestsException, lambda.ErrCodeEC2ThrottledException:
+			return 429, awsErr.Code(), awsErr.Message()
+		case lambda.ErrCodeServiceException, lambda.ErrCodeResourceNotReadyException,
+			lambda.ErrCodeResourceConflictException, lambda.ErrCodeEFSMountTimeoutException,
+			lambda.ErrCodeEFSMountConnectivityException:
+			return 500, awsErr.Code(), awsErr.Message()
+		default:
+			return 400, awsErr.Code(), awsErr.Message()
+		}
+	}
+	responseMessage := "Invocation error" + err.Error()
+	return simpleErrorResponse(responseMessage)
 }
