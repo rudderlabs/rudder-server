05e288ddb7f9ab8213ef5b981e25060ddc8c1bc7
Akash Chetty <achetty.iitr@gmail.com>
2022-08-26 13:34:20 +0530

    Added warehouse test

---
+++ warehouse/warehouse_test.go
@@ -0,0 +1,12 @@
+package warehouse_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/rudderlabs/rudder-server/warehouse"
+)
+
+var _ = Describe("Warehouse", func() {
+
+})
