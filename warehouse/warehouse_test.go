17b7ee969bf96b7669f6a688684108aeb940d979
Akash Chetty <achetty.iitr@gmail.com>
2022-08-29 10:30:24 +0530

    make fmt changes

--- warehouse/warehouse_test.go
+++ warehouse/warehouse_test.go
@@ -2,11 +2,7 @@ package warehouse_test
 
 import (
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-
-	"github.com/rudderlabs/rudder-server/warehouse"
 )
 
 var _ = Describe("Warehouse", func() {
-
 })
