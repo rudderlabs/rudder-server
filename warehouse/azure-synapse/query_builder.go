ec7d12b279a3a62ad4a98350d00db28e37862f1f
Akash Chetty <achetty.iitr@gmail.com>
2022-08-30 00:43:15 +0530

    Added QueryBuilder

---
+++ warehouse/azure-synapse/query_builder.go
@@ -0,0 +1,40 @@
+package azuresynapse
+
+import (
+	"fmt"
+	"github.com/rudderlabs/rudder-server/warehouse/utils"
+)
+
+func dropDanglingTablesSQLStatement(namespace string) string {
+	return fmt.Sprintf(`
+		select
+		  table_name
+		from
+		  information_schema.tables
+		where
+		  table_schema = '%s'
+		  AND table_name like '%s%s';
+	`,
+		namespace,
+		warehouseutils.StagingTablePrefix(provider),
+		"%",
+	)
+}
+
+func fetchSchemaSQLStatement(namespace string) string {
+	return fmt.Sprintf(`
+		SELECT
+		  table_name,
+		  column_name,
+		  data_type
+		FROM
+		  INFORMATION_SCHEMA.COLUMNS
+		WHERE
+		  table_schema = '%s'
+		  and table_name not like '%s%s'
+	`,
+		namespace,
+		warehouseutils.StagingTablePrefix(provider),
+		"%",
+	)
+}
