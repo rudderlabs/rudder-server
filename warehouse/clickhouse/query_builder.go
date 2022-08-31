6e6913c7662aa42bb345a07bf02914cf031f945d
Akash Chetty <achetty.iitr@gmail.com>
2022-08-31 11:14:41 +0530

    Added query_builder.go for clickhouse.go

--- warehouse/clickhouse/query_builder.go
+++ warehouse/clickhouse/query_builder.go
@@ -1,1 +1,7 @@
 package clickhouse
+
+import "fmt"
+
+func createTemporaryTableSQLStatement(namespace, tableName, stagingTableName string) string {
+	return fmt.Sprintf(`CREATE TABLE %[1]q.%[2]q AS %[1]q.%[3]q`, namespace, stagingTableName, tableName)
+}
