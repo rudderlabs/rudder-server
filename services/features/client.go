247b9345b852373ccba78abdfd4a72a024324b7b
Leonidas Vrachnis <leo.al.vra@gmail.com>
2022-08-31 14:17:33 +0300

    draft proposal

---
+++ services/features/client.go
@@ -0,0 +1,54 @@
+package features
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/rudderlabs/rudder-server/utils/misc"
+)
+
+type NamespaceClient struct {
+	client      http.Client
+	URL         string
+	NamespaceID string
+	Auth        string
+}
+
+type payload struct {
+	Components []component `json:"components"`
+}
+
+type component struct {
+	Name     string   `json:"name"`
+	Features []string `json:"features"`
+}
+
+func (c *NamespaceClient) Send(ctx context.Context, registry *Registry) error {
+
+	url := fmt.Sprintf("%s/data-plane/namespaces/%s/settings", c.URL, c.NamespaceID)
+
+	payload := payload{
+		Components: []component{},
+	}
+
+	registry.Each(func(name string, features []string) {
+		payload.Components = append(payload.Components, component{
+			Name:     name,
+			Features: features,
+		})
+	})
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	_, statusCode := misc.HTTPCallWithRetryWithTimeout(url, body, 60)
+	if statusCode != 200 {
+		return fmt.Errorf("unexpected status code %d", statusCode)
+	}
+
+	return nil
+}
---
+++ services/features/registry.go
@@ -0,0 +1,37 @@
+package features
+
+import "sync"
+
+var (
+	DefaultRegistry = &Registry{}
+)
+
+type item struct {
+	Name     string
+	Features []string
+}
+
+type Registry struct {
+	store []item
+	mu    sync.RWMutex
+}
+
+func (r *Registry) Register(name string, features []string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.store = append(r.store, item{Name: name, Features: features})
+}
+
+func (r *Registry) Each(fn func(name string, features []string)) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	for _, item := range r.store {
+		fn(item.Name, item.Features)
+	}
+}
+
+func Register(name string, features []string) {
+	DefaultRegistry.Register(name, features)
+}
