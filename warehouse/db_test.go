c2e96536589d8c220f1cbbd09905f0816b82cdd4
Akash Chetty <achetty.iitr@gmail.com>
2022-09-12 10:34:08 +0530

    added more test case for db claseQueryArgs

--- warehouse/db_test.go
+++ warehouse/db_test.go
@@ -1,6 +1,7 @@
 package warehouse_test
 
 import (
+	"github.com/lib/pq"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/rudderlabs/rudder-server/warehouse"
@@ -15,5 +16,6 @@ var _ = Describe("Db", func() {
 		Entry(nil, []warehouse.FilterClause{}, "", nil),
 		Entry(nil, []warehouse.FilterClause{{Clause: "id = <noop>", ClauseArg: 1}}, "id = $1", []interface{}{1}),
 		Entry(nil, []warehouse.FilterClause{{Clause: "id = <noop>", ClauseArg: 1}, {Clause: "val = <noop>", ClauseArg: 2}}, "id = $1 AND val = $2", []interface{}{1, 2}),
+		Entry(nil, []warehouse.FilterClause{{Clause: "id = ANY(<noop>)", ClauseArg: pq.Array([]interface{}{1})}, {Clause: "val = <noop>", ClauseArg: 2}}, "id = ANY($1) AND val = $2", []interface{}{pq.GenericArray{A: []interface{}{1}}, 2}),
 	)
 })
