2f506c80235d75698b8542f97876c32f8aa2fd33
Akash Chetty <achetty.iitr@gmail.com>
2022-09-21 03:55:14 +0530

    added test for archiver

---
+++ warehouse/archiver_test.go
@@ -0,0 +1,170 @@
+package warehouse
+
+import (
+	"github.com/ory/dockertest/v3"
+	mock_logger "github.com/rudderlabs/rudder-server/mocks/utils/logger"
+
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/rudderlabs/rudder-server/testhelper"
+	"github.com/rudderlabs/rudder-server/testhelper/destination"
+)
+
+func archiveSQLStatement() string {
+	return `
+			BEGIN;
+			INSERT INTO wh_uploads (
+			  id, source_id, namespace, destination_id,
+			  destination_type, start_staging_file_id,
+			  end_staging_file_id, start_load_file_id,
+			  end_load_file_id, status, schema,
+			  error, metadata, first_event_at,
+			  last_event_at, created_at, updated_at
+			)
+			VALUES
+			  (
+				1, 'test-sourceID', 'test-namespace',
+				'test-destinationID', 'POSTGRES',
+				1, 1, 1, 1, 'exported_data', '{}', '{}', '{}',
+				now(), now(), now(), now()
+			  ),
+			  (
+				2, 'test-sourceID', 'test-namespace',
+				'test-destinationID', 'POSTGRES',
+				2, 2, 2, 2, 'exported_data',
+				'{}', '{}', '{}', now(), now(), now(),
+				now()
+			  ),
+			  (
+				3, 'test-sourceID', 'test-namespace',
+				'test-destinationID', 'POSTGRES',
+				3, 3, 3, 3, 'exported_data', '{}', '{}', '{}',
+				now(), now(), now(), now()
+			  ),
+			  (
+				4, 'test-sourceID', 'test-namespace',
+				'test-destinationID', 'POSTGRES',
+				4, 4, 4, 4, 'exported_data', '{}', '{}',
+				'{}', now(), now(), now(), now()
+			  );
+			INSERT INTO wh_staging_files (
+			  id, location, schema, source_id, destination_id,
+			  status, total_events, first_event_at,
+			  last_event_at, created_at, updated_at,
+			  metadata
+			)
+			VALUES
+			  (
+				1, 'rudder/rudder-warehouse-staging-logs/2EUralUySYUs7hgsdU1lFXRSm/2022-09-20/1663650685.2EUralsdsDyZjOKU1lFXRSm.eeadsb4-a066-42f4-a90b-460161378e1b.json.gz',
+				'{}', 'test-sourceID', 'test-destinationID',
+				'succeeded', 1, NOW(), NOW(), NOW(),
+				NOW(), '{}'
+			  ),
+			  (
+				2, 'rudder/rudder-warehouse-staging-logs/2EUralUySYUs7hgsdU1lFXRSm/2022-09-20/1663650685.2EUralsdsDyZjOKU1lFXRSm.eeadsb4-a066-42f4-a90b-460161378e1b.json.gz',
+				'{}', 'test-sourceID', 'test-destinationID',
+				'succeeded', 1, NOW(), NOW(), NOW(),
+				NOW(), '{}'
+			  ),
+			  (
+				3, 'rudder/rudder-warehouse-staging-logs/2EUralUySYUs7hgsdU1lFXRSm/2022-09-20/1663650685.2EUralsdsDyZjOKU1lFXRSm.eeadsb4-a066-42f4-a90b-460161378e1b.json.gz',
+				'{}', 'test-sourceID', 'test-destinationID',
+				'succeeded', 1, NOW(), NOW(), NOW(),
+				NOW(), '{}'
+			  ),
+			  (
+				4, 'rudder/rudder-warehouse-staging-logs/2EUralUySYUs7hgsdU1lFXRSm/2022-09-20/1663650685.2EUralsdsDyZjOKU1lFXRSm.eeadsb4-a066-42f4-a90b-460161378e1b.json.gz',
+				'{}', 'test-sourceID', 'test-destinationID',
+				'succeeded', 1, NOW(), NOW(), NOW(),
+				NOW(), '{}'
+			  );
+			INSERT INTO wh_load_files (
+			  id, staging_file_id, location, source_id,
+			  destination_id, destination_type,
+			  table_name, total_events, created_at,
+			  metadata
+			)
+			VALUES
+			  (
+				1, 1, 'rudder/rudder-warehouse-staging-logs/2EUralUySYUs7hgsdU1lFXRSm/2022-09-20/1663650685.2EUralsdsDyZjOKU1lFXRSm.eeadsb4-a066-42f4-a90b-460161378e1b.json.gz',
+				'test-sourceID', 'test-destinationID',
+				'POSTGRES', 'test-table', 1, NOW(),
+				'{}'
+			  ),
+			  (
+				2, 2, 'rudder/rudder-warehouse-staging-logs/2EUralUySYUs7hgsdU1lFXRSm/2022-09-20/1663650685.2EUralsdsDyZjOKU1lFXRSm.eeadsb4-a066-42f4-a90b-460161378e1b.json.gz',
+				'test-sourceID', 'test-destinationID',
+				'POSTGRES', 'test-table', 1, NOW(),
+				'{}'
+			  ),
+			  (
+				3, 3, 'rudder/rudder-warehouse-staging-logs/2EUralUySYUs7hgsdU1lFXRSm/2022-09-20/1663650685.2EUralsdsDyZjOKU1lFXRSm.eeadsb4-a066-42f4-a90b-460161378e1b.json.gz',
+				'test-sourceID', 'test-destinationID',
+				'POSTGRES', 'test-table', 1, NOW(),
+				'{}'
+			  ),
+			  (
+				4, 3, 'rudder/rudder-warehouse-staging-logs/2EUralUySYUs7hgsdU1lFXRSm/2022-09-20/1663650685.2EUralsdsDyZjOKU1lFXRSm.eeadsb4-a066-42f4-a90b-460161378e1b.json.gz',
+				'test-sourceID', 'test-destinationID',
+				'POSTGRES', 'test-table', 1, NOW(),
+				'{}'
+			  );
+			END;
+	`
+}
+
+var _ = Describe("Archiver", Ordered, func() {
+	var (
+		pgResource    *destination.PostgresResource
+		minioResource *destination.MINIOResource
+		err           error
+		cleanup       = &testhelper.Cleanup{}
+		es            = GinkgoT()
+		mockLogger    *mock_logger.MockLoggerI
+		mockCtrl      *gomock.Controller
+		prefix        = "test-prefix"
+	)
+
+	BeforeAll(func() {
+		pool, err := dockertest.NewPool("")
+		Expect(err).To(BeNil())
+
+		es.Setenv("RSERVER_WAREHOUSE_UPLOADS_ARCHIVAL_TIME_IN_DAYS", "0")
+
+		pgResource = setupWarehouseJobs(pool, GinkgoT(), cleanup)
+
+		minioResource, err = destination.SetupMINIO(pool, cleanup)
+		Expect(err).To(BeNil())
+
+		mockCtrl = gomock.NewController(es)
+		mockLogger = mock_logger.NewMockLoggerI(mockCtrl)
+		mockLogger.EXPECT().Errorf(gomock.Any(), gomock.Any()).MaxTimes(0)
+		mockLogger.EXPECT().Errorf(gomock.Any(), gomock.Any()).MinTimes(0)
+		mockLogger.EXPECT().Infof(gomock.Any(), gomock.Any()).AnyTimes()
+		mockLogger.EXPECT().Debugf(gomock.Any(), gomock.Any()).AnyTimes()
+
+		pkgLogger = mockLogger
+
+		es.Setenv("JOBS_BACKUP_STORAGE_PROVIDER", "MINIO")
+		es.Setenv("JOBS_BACKUP_STORAGE_PROVIDER", "MINIO")
+		es.Setenv("JOBS_BACKUP_BUCKET", minioResource.BucketName)
+		es.Setenv("JOBS_BACKUP_PREFIX", prefix)
+		es.Setenv("MINIO_ENDPOINT", minioResource.Endpoint)
+		es.Setenv("MINIO_ACCESS_KEY_ID", minioResource.AccessKey)
+		es.Setenv("MINIO_SECRET_ACCESS_KEY", minioResource.SecretKey)
+		es.Setenv("MINIO_SSL", "false")
+		es.Setenv("RUDDER_TMPDIR", es.TempDir())
+	})
+
+	AfterAll(func() {
+		cleanup.Run()
+	})
+
+	It("Archive uploads", func() {
+		_, err = pgResource.DB.Exec(archiveSQLStatement())
+		Expect(err).To(BeNil())
+
+		archiveUploads(dbHandle)
+	})
+})
---
+++ warehouse/warehousegrpc_test.go
@@ -0,0 +1,428 @@
+package warehouse
+
+import (
+	"context"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/ory/dockertest/v3"
+
+	proto "github.com/rudderlabs/rudder-server/proto/warehouse"
+	"github.com/rudderlabs/rudder-server/testhelper"
+	"github.com/rudderlabs/rudder-server/testhelper/destination"
+	"github.com/rudderlabs/rudder-server/utils/logger"
+	warehouseutils "github.com/rudderlabs/rudder-server/warehouse/utils"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"net/http"
+)
+
+func syncsSQLStatement() string {
+	return `
+			BEGIN;
+
+			INSERT INTO wh_uploads (
+			  id, source_id, namespace, destination_id,
+			  destination_type, start_staging_file_id,
+			  end_staging_file_id, start_load_file_id,
+			  end_load_file_id, status, schema,
+			  error, metadata, first_event_at,
+			  last_event_at, created_at, updated_at
+			)
+			VALUES
+			  (
+				1, 'test-sourceID', 'test-namespace',
+				'test-destinationID', 'POSTGRES',
+				1, 1, 1, 1, 'waiting', '{}', '{}', '{}',
+				now(), now(), now(), now()
+			  ),
+			  (
+				2, 'test-sourceID', 'test-namespace',
+				'test-destinationID', 'POSTGRES',
+				1, 1, 1, 1, 'exporting_data_failed',
+				'{}', '{}', '{}', now(), now(), now(),
+				now()
+			  ),
+			  (
+				3, 'test-sourceID', 'test-namespace',
+				'test-destinationID', 'POSTGRES',
+				1, 1, 1, 1, 'aborted', '{}', '{}', '{}',
+				now(), now(), now(), now()
+			  ),
+			  (
+				4, 'test-sourceID', 'test-namespace',
+				'test-destinationID', 'POSTGRES',
+				1, 1, 1, 1, 'exported_data', '{}', '{}',
+				'{}', now(), now(), now(), now()
+			  );
+
+			INSERT INTO wh_table_uploads (
+			  id, wh_upload_id, table_name, status,
+			  error, created_at, updated_at
+			)
+			VALUES
+			  (
+				1, 1, 'test-table', 'waiting', '',
+				NOW(), NOW()
+			  ),
+			  (
+				2, 2, 'test-table', 'failed', '',
+				NOW(), NOW()
+			  ),
+			  (
+				3, 3, 'test-table', 'failed', '',
+				NOW(), NOW()
+			  ),
+			  (
+				4, 4, 'test-table', 'succeeded', '',
+				NOW(), NOW()
+			  );
+			END;
+	`
+}
+
+var _ = Describe("WarehouseGrpc", func() {
+	var (
+		sourceID        = "test-sourceID"
+		destinationID   = "test-destinationID"
+		workspaceID     = "test-workspaceID"
+		destinationType = "POSTGRES"
+	)
+
+	Describe("Warehouse GRPC round trip", Ordered, func() {
+		Describe("Dedicated workspace", Ordered, func() {
+			var (
+				pgResource *destination.PostgresResource
+				err        error
+				cleanup    = &testhelper.Cleanup{}
+				w          *warehouseGrpc
+				c          context.Context
+				limit      = int32(2)
+				uploadID   = int64(1)
+			)
+
+			BeforeAll(func() {
+				pool, err := dockertest.NewPool("")
+				Expect(err).To(BeNil())
+
+				pgResource = setupWarehouseJobs(pool, GinkgoT(), cleanup)
+
+				pkgLogger = &logger.NOP{}
+				sourceIDsByWorkspace = map[string][]string{
+					workspaceID: {sourceID},
+				}
+				connectionsMap = map[string]map[string]warehouseutils.WarehouseT{
+					destinationID: {
+						sourceID: warehouseutils.WarehouseT{
+							Identifier: warehouseutils.GetWarehouseIdentifier(destinationType, sourceID, destinationID),
+						},
+					},
+				}
+
+				w = &warehouseGrpc{}
+				c = context.TODO()
+			})
+
+			AfterAll(func() {
+				cleanup.Run()
+			})
+
+			It("Init warehouse api", func() {
+				err = InitWarehouseAPI(pgResource.DB, &logger.NOP{})
+				Expect(err).To(BeNil())
+
+				_, err = pgResource.DB.Exec(syncsSQLStatement())
+				Expect(err).To(BeNil())
+			})
+
+			It("Getting health", func() {
+				res, err := w.GetHealth(c, &emptypb.Empty{})
+				Expect(err).To(BeNil())
+				Expect(res.Value).To(BeTrue())
+			})
+
+			It("Getting warehouse upload", func() {
+				res, err := w.GetWHUpload(c, &proto.WHUploadRequest{
+					UploadId:    uploadID,
+					WorkspaceId: workspaceID,
+				})
+				Expect(err).To(BeNil())
+				Expect(res).NotTo(BeNil())
+				Expect(res.Id).To(Equal(uploadID))
+				Expect(res.Tables).Should(HaveLen(1))
+				Expect(res.Tables[0].UploadId).To(Equal(uploadID))
+			})
+
+			Describe("Getting warehouse uploads", func() {
+				var req *proto.WHUploadsRequest
+
+				BeforeEach(func() {
+					req = &proto.WHUploadsRequest{
+						WorkspaceId:     workspaceID,
+						SourceId:        sourceID,
+						DestinationId:   destinationID,
+						DestinationType: destinationType,
+						Limit:           limit,
+					}
+				})
+
+				It("Waiting syncs", func() {
+					req.Status = "waiting"
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(1))
+				})
+				It("Succeeded syncs", func() {
+					req.Status = "success"
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(4))
+				})
+				It("Failed syncs", func() {
+					req.Status = "failed"
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(2))
+				})
+				It("Aborted syncs", func() {
+					req.Status = "aborted"
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(3))
+				})
+				It("No status", func() {
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(2))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(4))
+					Expect(res.Uploads[1].Id).To(BeEquivalentTo(3))
+				})
+				It("With offset", func() {
+					req.Offset = 3
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(1))
+				})
+			})
+
+			Describe("Count warehouse uploads to retry", func() {
+				var req *proto.RetryWHUploadsRequest
+
+				BeforeEach(func() {
+					req = &proto.RetryWHUploadsRequest{
+						WorkspaceId:     workspaceID,
+						SourceId:        sourceID,
+						DestinationId:   destinationID,
+						DestinationType: destinationType,
+					}
+				})
+
+				It("Interval in hours", func() {
+					req.IntervalInHours = 24
+
+					res, err := w.CountWHUploadsToRetry(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.StatusCode).To(BeEquivalentTo(http.StatusOK))
+					Expect(res.Count).To(BeEquivalentTo(1))
+				})
+				It("Interval in hours", func() {
+					req.UploadIds = []int64{3}
+
+					res, err := w.CountWHUploadsToRetry(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.StatusCode).To(BeEquivalentTo(http.StatusOK))
+					Expect(res.Count).To(BeEquivalentTo(1))
+				})
+			})
+
+			Describe("Retry warehouse uploads", func() {
+				var req *proto.RetryWHUploadsRequest
+
+				BeforeEach(func() {
+					req = &proto.RetryWHUploadsRequest{
+						WorkspaceId:     workspaceID,
+						SourceId:        sourceID,
+						DestinationId:   destinationID,
+						DestinationType: destinationType,
+					}
+				})
+
+				It("Interval in hours", func() {
+					req.IntervalInHours = 24
+
+					res, err := w.RetryWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.StatusCode).To(BeEquivalentTo(http.StatusOK))
+				})
+				It("Interval in hours", func() {
+					req.UploadIds = []int64{3}
+
+					res, err := w.RetryWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.StatusCode).To(BeEquivalentTo(http.StatusOK))
+				})
+			})
+
+			It("Triggering warehouse upload", func() {
+				res, err := w.TriggerWHUpload(c, &proto.WHUploadRequest{
+					UploadId:    3,
+					WorkspaceId: workspaceID,
+				})
+				Expect(err).To(BeNil())
+				Expect(res).NotTo(BeNil())
+				Expect(res.Message).To(Equal(TriggeredSuccessfully))
+				Expect(res.StatusCode).To(BeEquivalentTo(http.StatusOK))
+			})
+
+			It("Triggering warehouse uploads", func() {
+				res, err := w.TriggerWHUploads(c, &proto.WHUploadsRequest{
+					WorkspaceId:   workspaceID,
+					SourceId:      sourceID,
+					DestinationId: destinationID,
+				})
+				Expect(err).To(BeNil())
+				Expect(res).NotTo(BeNil())
+				Expect(res.Message).To(Equal(TriggeredSuccessfully))
+				Expect(res.StatusCode).To(BeEquivalentTo(http.StatusOK))
+			})
+		})
+
+		Describe("Multi-tenant workspace", Ordered, func() {
+			var (
+				pgResource *destination.PostgresResource
+				err        error
+				cleanup    = &testhelper.Cleanup{}
+				w          *warehouseGrpc
+				c          context.Context
+				limit      = int32(2)
+				es         = GinkgoT()
+			)
+
+			BeforeAll(func() {
+				pool, err := dockertest.NewPool("")
+				Expect(err).To(BeNil())
+
+				pgResource = setupWarehouseJobs(pool, es, cleanup)
+
+				es.Setenv("DEPLOYMENT_TYPE", "MULTITENANT")
+				es.Setenv("HOSTED_SERVICE_SECRET", "test-secret")
+
+				pkgLogger = &logger.NOP{}
+				sourceIDsByWorkspace = map[string][]string{
+					workspaceID: {sourceID},
+				}
+				connectionsMap = map[string]map[string]warehouseutils.WarehouseT{
+					destinationID: {
+						sourceID: warehouseutils.WarehouseT{
+							Identifier: warehouseutils.GetWarehouseIdentifier(destinationType, sourceID, destinationID),
+						},
+					},
+				}
+
+				w = &warehouseGrpc{}
+				c = context.TODO()
+			})
+
+			AfterAll(func() {
+				es.Setenv("DEPLOYMENT_TYPE", "")
+				es.Setenv("HOSTED_SERVICE_SECRET", "")
+
+				cleanup.Run()
+			})
+
+			It("Init warehouse api", func() {
+				err = InitWarehouseAPI(pgResource.DB, &logger.NOP{})
+				Expect(err).To(BeNil())
+
+				_, err = pgResource.DB.Exec(syncsSQLStatement())
+				Expect(err).To(BeNil())
+			})
+
+			Describe("Getting warehouse uploads", func() {
+				var req *proto.WHUploadsRequest
+
+				BeforeEach(func() {
+					req = &proto.WHUploadsRequest{
+						WorkspaceId:     workspaceID,
+						SourceId:        sourceID,
+						DestinationId:   destinationID,
+						DestinationType: destinationType,
+						Limit:           limit,
+					}
+				})
+
+				It("Waiting syncs", func() {
+					req.Status = "waiting"
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(1))
+				})
+				It("Succeeded syncs", func() {
+					req.Status = "success"
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(4))
+				})
+				It("Failed syncs", func() {
+					req.Status = "failed"
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(2))
+				})
+				It("Aborted syncs", func() {
+					req.Status = "aborted"
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(3))
+				})
+				It("No status", func() {
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(2))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(4))
+					Expect(res.Uploads[1].Id).To(BeEquivalentTo(3))
+				})
+				It("With offset", func() {
+					req.Offset = 3
+
+					res, err := w.GetWHUploads(c, req)
+					Expect(err).To(BeNil())
+					Expect(res).NotTo(BeNil())
+					Expect(res.Uploads).Should(HaveLen(1))
+					Expect(res.Uploads[0].Id).To(BeEquivalentTo(1))
+				})
+			})
+		})
+	})
+})
