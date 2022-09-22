ddbda8432fc00256fcda350a05a55b25a30a4e66
Leonidas Vrachnis <leo.al.vra@gmail.com>
2022-09-22 12:13:23 +0300

    exp: test to reproduce

--- router/batchrouter/batchrouterBenchmark_test.go
+++ router/batchrouter/batchrouterBenchmark_test.go
@@ -4,11 +4,15 @@ import (
 	"strings"
 	"testing"
 
+	jsonstd "encoding/json"
+
 	"github.com/gofrs/uuid"
 	"github.com/golang/mock/gomock"
 	"github.com/rudderlabs/rudder-server/config"
 	backendconfig "github.com/rudderlabs/rudder-server/config/backend-config"
+	"github.com/rudderlabs/rudder-server/jobsdb"
 	mocksFileManager "github.com/rudderlabs/rudder-server/mocks/services/filemanager"
+	"golang.org/x/sync/errgroup"
 )
 
 func Benchmark_GetStorageDateFormat(b *testing.B) {
@@ -38,4 +42,70 @@ func Benchmark_GetStorageDateFormat(b *testing.B) {
 
 func randomString() string {
 	return strings.ReplaceAll(uuid.Must(uuid.NewV4()).String(), "-", "")
+}
+
+func Benchmark_JSONUmarshal(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+
+		jobs := []*jobsdb.JobT{}
+		for i := 0; i < 100; i++ {
+			params := JobParametersT{
+				EventName: "test",
+				EventType: "track",
+				MessageID: string(uuid.Must(uuid.NewV4()).String()),
+
+				ReceivedAt: "2021-01-01T00:00:00Z",
+			}
+
+			p, _ := json.Marshal(params)
+
+			jobs = append(jobs, &jobsdb.JobT{
+				UUID:       uuid.Must(uuid.NewV4()),
+				JobID:      int64(i),
+				Parameters: p,
+			})
+		}
+
+		g := errgroup.Group{}
+		g.Go(func() error {
+
+			params := JobParametersT{
+				EventName: "test",
+				EventType: "track",
+				MessageID: string(uuid.Must(uuid.NewV4()).String()),
+			}
+
+			p, _ := json.Marshal(params)
+
+			for i := range jobs {
+				err := jsonstd.Unmarshal(p, &jobs[i].Parameters)
+				if err != nil {
+					b.Fatal(err)
+				}
+
+				jobs[i].Parameters = jobs[i].Parameters[:len(p):len(p)]
+			}
+
+			return nil
+		})
+
+		g.Go(func() error {
+
+			for i, _ := range jobs {
+				var params JobParametersT
+				json.Unmarshal(jobs[i].Parameters, &params)
+				// if err != nil {
+				// 	b.Fatal(err)
+				// }
+
+			}
+
+			return nil
+		})
+
+		g.Wait()
+
+	}
+
 }
