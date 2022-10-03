4665a90741b02ba4d29ebd0c696879251e8d7106
Francesco Casula <fra.casula@gmail.com>
2022-10-03 15:54:24 +0200

    chore: changing log entry

--- router/batchrouter/batchrouterBenchmark_test.go
+++ router/batchrouter/batchrouterBenchmark_test.go
@@ -1,19 +1,19 @@
 package batchrouter
 
 import (
+	jsonstd "encoding/json"
 	"strings"
 	"testing"
 
-	jsonstd "encoding/json"
-
 	"github.com/gofrs/uuid"
 	"github.com/golang/mock/gomock"
+	"golang.org/x/sync/errgroup"
+
 	"github.com/rudderlabs/rudder-server/config"
 	backendconfig "github.com/rudderlabs/rudder-server/config/backend-config"
 	"github.com/rudderlabs/rudder-server/jobsdb"
 	mocksFileManager "github.com/rudderlabs/rudder-server/mocks/services/filemanager"
 	"github.com/rudderlabs/rudder-server/utils/logger"
-	"golang.org/x/sync/errgroup"
 )
 
 func Benchmark_GetStorageDateFormat(b *testing.B) {
@@ -45,26 +45,23 @@ func randomString() string {
 	return strings.ReplaceAll(uuid.Must(uuid.NewV4()).String(), "-", "")
 }
 
-// Benchmark_JSONUmarshal tries to reproduce a panic encountered with jsoniter
-func Benchmark_JSONUmarshal(b *testing.B) {
+// Benchmark_JSONUnmarshal tries to reproduce a panic encountered with jsoniter
+func Benchmark_JSONUnmarshal(b *testing.B) {
 	config.Load()
 	logger.Init()
 	Init()
 
 	for i := 0; i < b.N; i++ {
-
-		jobs := []*jobsdb.JobT{}
+		var jobs []*jobsdb.JobT
 		for i := 0; i < 100; i++ {
 			params := JobParametersT{
-				EventName: "test",
-				EventType: "track",
-				MessageID: uuid.Must(uuid.NewV4()).String(),
-
+				EventName:  "test",
+				EventType:  "track",
+				MessageID:  uuid.Must(uuid.NewV4()).String(),
 				ReceivedAt: "2021-01-01T00:00:00Z",
 			}
 
 			p, _ := json.Marshal(params)
-
 			jobs = append(jobs, &jobsdb.JobT{
 				UUID:       uuid.Must(uuid.NewV4()),
 				JobID:      int64(i),
@@ -81,7 +78,6 @@ func Benchmark_JSONUmarshal(b *testing.B) {
 			}
 
 			p, _ := json.Marshal(params)
-
 			for i := range jobs {
 				err := jsonstd.Unmarshal(p, &jobs[i].Parameters)
 				if err != nil {
@@ -93,16 +89,14 @@ func Benchmark_JSONUmarshal(b *testing.B) {
 
 			return nil
 		})
-
 		g.Go(func() error {
 			for i := range jobs {
 				var params JobParametersT
-				_ = tryUnmarshalJSON(jobs[i].Parameters, &params)
+				_ = tryUnmarshalJSON(jobs[i].JobID, jobs[i].Parameters, &params)
 			}
 
 			return nil
 		})
-
 		_ = g.Wait()
 	}
 }
