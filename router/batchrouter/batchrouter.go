b3f023cb3c6f06396fce36fe94d9b07b58ed10ed
saurav-malani <sauravmalani1@gmail.com>
2022-10-04 16:27:42 +0530

    deep source fix

--- router/batchrouter/batchrouter.go
+++ router/batchrouter/batchrouter.go
@@ -855,7 +855,9 @@ func (brt *HandleT) sendJobsToStorage(batchJobs BatchJobsT) {
 	if err != nil {
 		panic(fmt.Errorf("BRT: %s: file open failed : %s", brt.destType, err.Error()))
 	}
-	defer file.Close()
+	defer func() {
+		_ = file.Close()
+	}()
 	var jobString string
 	writeAtBytes := brt.asyncDestinationStruct[destinationID].Size
 
@@ -1517,8 +1519,8 @@ func (brt *HandleT) trackRequestMetrics(batchReqDiagnostics batchRequestMetric) 
 	if diagnostics.EnableBatchRouterMetric {
 		brt.batchRequestsMetricLock.Lock()
 		if brt.batchRequestsMetric == nil {
-			var batchRequestsMetric []batchRequestMetric
-			brt.batchRequestsMetric = append(batchRequestsMetric, batchReqDiagnostics)
+			brt.batchRequestsMetric = []batchRequestMetric{}
+			brt.batchRequestsMetric = append(brt.batchRequestsMetric, batchReqDiagnostics)
 		} else {
 			brt.batchRequestsMetric = append(brt.batchRequestsMetric, batchReqDiagnostics)
 		}
