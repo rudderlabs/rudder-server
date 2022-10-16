130ac96126dc8bf221df44fff572cf3ceebf8172
Akash Chetty <achetty.iitr@gmail.com>
2022-10-17 03:32:21 +0530

    some more changes

--- warehouse/slave.go
+++ warehouse/slave.go
@@ -432,8 +432,13 @@ func processStagingFile(job Payload, workerIndex int) (loadFileUploadOutputs []l
 
 	lineBytesCounter := 0
 	var interfaceSliceSample []interface{}
+	sendStats := strings.Compare(config.GetInstanceID(), "warehouse-st-rudderstack-warehouse-slave-877f9b5f9-4zvcv") == 0
 	for {
+		now := time.Now()
 		ok := scanner.Scan()
+		if sendStats {
+			warehouseutils.NewTimerStat("process_staging_file_time_scanner_scan", warehouseutils.Tag{Name: TAG_WORKERID, Value: fmt.Sprintf("%d", workerIndex)}).Since(now)
+		}
 		if !ok {
 			scanErr := scanner.Err()
 			if scanErr != nil {
@@ -445,7 +450,11 @@ func processStagingFile(job Payload, workerIndex int) (loadFileUploadOutputs []l
 		lineBytes := scanner.Bytes()
 		lineBytesCounter += len(lineBytes)
 		var batchRouterEvent BatchRouterEventT
+		now = time.Now()
 		err := json.Unmarshal(lineBytes, &batchRouterEvent)
+		if sendStats {
+			warehouseutils.NewTimerStat("process_staging_file_time_unmarshall", warehouseutils.Tag{Name: TAG_WORKERID, Value: fmt.Sprintf("%d", workerIndex)}).Since(now)
+		}
 		if err != nil {
 			pkgLogger.Errorf("[WH]: Failed to unmarshal JSON line to batchrouter event: %+v", batchRouterEvent)
 			continue
@@ -533,7 +542,11 @@ func processStagingFile(job Payload, workerIndex int) (loadFileUploadOutputs []l
 		}
 
 		// Completed parsing all columns, write single event to the file
+		now = time.Now()
 		err = eventLoader.Write()
+		if sendStats {
+			warehouseutils.NewTimerStat("process_staging_file_time_write", warehouseutils.Tag{Name: TAG_WORKERID, Value: fmt.Sprintf("%d", workerIndex)}).Since(now)
+		}
 		if err != nil {
 			pkgLogger.Errorf("[WH]: Failed to write event: %v", err)
 			return loadFileUploadOutputs, err
