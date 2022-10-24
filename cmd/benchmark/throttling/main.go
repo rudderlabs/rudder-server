ac1268713a36d3a3d342c193a84885e04abdf604
Francesco Casula <fra.casula@gmail.com>
2022-10-24 17:28:37 +0200

    chore: adding benchmark support

---
+++ cmd/benchmark/throttling/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"syscall"
+	"time"
+
+	"github.com/go-redis/redis/v9"
+
+	"github.com/rudderlabs/rudder-server/internal/throttling"
+	"github.com/rudderlabs/rudder-server/testhelper/rand"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGKILL, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	var (
+		key         = getEnvString("KEY", rand.UniqueString(10))
+		rate        = getEnvInt64("RATE", 10000)
+		window      = getEnvInt64("WINDOW", 60)
+		duration    = getEnvInt64("DURATION", 180) // 3 minutes
+		concurrency = getEnvInt64("CONCURRENCY", 5000)
+		redisAddr   = getEnvString("REDIS_ADDR", "localhost:6379")
+	)
+
+	rc := redis.NewClient(&redis.Options{
+		Addr: redisAddr,
+	})
+	defer func() {
+		if err := rc.Close(); err != nil {
+			log.Printf("Error while closing Redis client: %v", err)
+		}
+	}()
+	if err := rc.Ping(ctx); err != nil {
+		log.Printf("Error while pinging Redis: %v", err)
+		return
+	}
+
+	l, err := throttling.New(throttling.WithRedisClient(rc))
+	if err != nil {
+		log.Printf("Could not create throttling client: %v", err)
+		return
+	}
+
+	var (
+		wg               sync.WaitGroup
+		start            = time.Now()
+		numberOfRequests int32
+		maxNoOfRoutines  = make(chan struct{}, concurrency)
+		runFor           = time.NewTimer(time.Duration(duration) * time.Second)
+	)
+	defer func() {
+		elapsed := time.Since(start)
+		log.Println("Number of requests:", numberOfRequests)
+		log.Println("Elapsed time:", elapsed)
+		log.Println("RPS:", float64(numberOfRequests)/elapsed.Seconds())
+	}()
+	defer wg.Wait()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Context done:", ctx.Err())
+			return
+		case <-runFor.C:
+			log.Println("Timer triggered")
+			return
+		default:
+			maxNoOfRoutines <- struct{}{}
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				defer func() { <-maxNoOfRoutines }()
+				defer func() { atomic.AddInt32(&numberOfRequests, 1) }()
+				_, _, err := l.Limit(ctx, 1, rate, window, key)
+				if err != nil {
+					log.Printf("Could not limit: %v", err)
+					return
+				}
+			}()
+		}
+	}
+}
+
+func getEnvString(envVar, defaultValue string) string {
+	v, ok := os.LookupEnv(envVar)
+	if !ok {
+		return defaultValue
+	}
+	return v
+}
+
+func getEnvInt64(envVar string, defaultValue int64) int64 {
+	v, ok := os.LookupEnv(envVar)
+	if !ok {
+		return defaultValue
+	}
+	i, err := strconv.ParseInt(v, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
