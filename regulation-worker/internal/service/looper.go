2297a44704584bcdd81e5f15594e704644fbbe12
Abhimanyu Babbar <abhimanyu.babbar89@gmail.com>
2022-10-27 11:18:40 +0530

    Added support to read the interval between loops from env

--- regulation-worker/internal/service/looper.go
+++ regulation-worker/internal/service/looper.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	backoff "github.com/cenkalti/backoff/v4"
@@ -19,13 +22,19 @@ type Looper struct {
 
 func (l *Looper) Loop(ctx context.Context) error {
 	pkgLogger.Infof("running regulation worker in infinite loop")
+
+	interval, err := getenvInt("INTERVAL_IN_MINUTES", 10)
+	if err != nil {
+		return fmt.Errorf("reading value: %s from env: %s", "INTERVAL_IN_MINUTES", err.Error())
+	}
+
 	for {
 
 		err := l.Svc.JobSvc(ctx)
 
 		if err == model.ErrNoRunnableJob {
 			pkgLogger.Debugf("no runnable job found... sleeping")
-			if err := misc.SleepCtx(ctx, 1*time.Minute); err != nil {
+			if err := misc.SleepCtx(ctx, time.Duration(interval)*time.Minute); err != nil {
 				pkgLogger.Debugf("context cancelled... exiting infinite loop %v", err)
 				return nil
 			}
@@ -37,4 +46,12 @@ func (l *Looper) Loop(ctx context.Context) error {
 			return err
 		}
 	}
+}
+
+func getenvInt(key string, fallback int) (int, error) {
+	k := os.Getenv(key)
+	if len(k) == 0 {
+		return fallback, nil
+	}
+	return strconv.Atoi(k)
 }
