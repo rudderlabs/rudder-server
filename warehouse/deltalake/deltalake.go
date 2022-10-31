916f845952910a598deb264861af4e7ebdcf8c61
Akash Chetty <achetty.iitr@gmail.com>
2022-10-31 10:50:55 +0530

    code cleanup

--- warehouse/deltalake/deltalake.go
+++ warehouse/deltalake/deltalake.go
@@ -1236,13 +1236,35 @@ func primaryKey(tableName string) string {
 func stagingSqlStatement(namespace, tableName, stagingTableName string, columnKeys []string) (sqlStatement string) {
 	pk := primaryKey(tableName)
 	if tableName == warehouseutils.UsersTable {
-		sqlStatement = fmt.Sprintf(`SELECT %[3]s FROM %[1]s.%[2]s`,
+		sqlStatement = fmt.Sprintf(`
+			SELECT
+			  %[3]s
+			FROM
+			  %[1]s.%[2]s
+		`,
 			namespace,
 			stagingTableName,
 			columnNames(columnKeys),
 		)
 	} else {
-		sqlStatement = fmt.Sprintf(`SELECT * FROM ( SELECT *, row_number() OVER (PARTITION BY %[3]s ORDER BY RECEIVED_AT DESC) AS _rudder_staging_row_number FROM %[1]s.%[2]s ) AS q WHERE _rudder_staging_row_number = 1`,
+		sqlStatement = fmt.Sprintf(`
+			SELECT
+			  *
+			FROM
+			  (
+				SELECT
+				  *,
+				  row_number() OVER (
+					PARTITION BY %[3]s
+					ORDER BY
+					  RECEIVED_AT DESC
+				  ) AS _rudder_staging_row_number
+				FROM
+				  %[1]s.%[2]s
+			  ) AS q
+			WHERE
+			  _rudder_staging_row_number = 1
+		`,
 			namespace,
 			stagingTableName,
 			pk,
@@ -1251,17 +1273,24 @@ func stagingSqlStatement(namespace, tableName, stagingTableName string, columnKe
 	return
 }
 
-func mergeableLTSQLStatement(namespace, tableName, stagingTableName string, columnKeys []string, partitionQuery string) (sqlStatement string) {
+func mergeableLTSQLStatement(namespace, tableName, stagingTableName string, columnKeys []string, partitionQuery string) string {
 	pk := primaryKey(tableName)
 	if partitionQuery != "" {
 		partitionQuery += " AND"
 	}
 	stagingTableSqlStatement := stagingSqlStatement(namespace, tableName, stagingTableName, columnKeys)
-	sqlStatement = fmt.Sprintf(`MERGE INTO %[1]s.%[2]s AS MAIN
-                                       USING ( %[3]s ) AS STAGING
-									   ON %[8]s MAIN.%[4]s = STAGING.%[4]s
-									   WHEN MATCHED THEN UPDATE SET %[5]s
-									   WHEN NOT MATCHED THEN INSERT (%[6]s) VALUES (%[7]s);`,
+	sqlStatement := fmt.Sprintf(`
+		MERGE INTO %[1]s.%[2]s AS MAIN USING (%[3]s) AS STAGING ON %[8]s MAIN.%[4]s = STAGING.%[4]s
+		WHEN MATCHED THEN
+		UPDATE
+		SET
+		  %[5]s
+		  WHEN NOT MATCHED THEN
+		INSERT
+		  (%[6]s)
+		VALUES
+		  (%[7]s);
+		`,
 		namespace,
 		tableName,
 		stagingTableSqlStatement,
@@ -1271,17 +1300,23 @@ func mergeableLTSQLStatement(namespace, tableName, stagingTableName string, colu
 		stagingColumnNames(columnKeys),
 		partitionQuery,
 	)
-	return
+	return sqlStatement
 }
 
-func appendableLTSQLStatement(namespace, tableName, stagingTableName string, columnKeys []string) (sqlStatement string) {
+func appendableLTSQLStatement(namespace, tableName, stagingTableName string, columnKeys []string) string {
 	stagingTableSqlStatement := stagingSqlStatement(namespace, tableName, stagingTableName, columnKeys)
-	sqlStatement = fmt.Sprintf(`INSERT INTO %[1]s.%[2]s (%[4]s) SELECT %[4]s FROM ( %[5]s );`,
+	sqlStatement := fmt.Sprintf(`
+		INSERT INTO %[1]s.%[2]s (%[4]s)
+		SELECT
+		  %[4]s
+		FROM
+		  (%[5]s);
+		`,
 		namespace,
 		tableName,
 		stagingTableName,
 		columnNames(columnKeys),
 		stagingTableSqlStatement,
 	)
-	return
+	return sqlStatement
 }
