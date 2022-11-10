97caefbe38d8ed9346b59f57846dc0a0074fd55f
Francesco Casula <fra.casula@gmail.com>
2022-11-10 17:15:47 +0100

    chore: tuning test

--- internal/throttling/throttling_test.go
+++ internal/throttling/throttling_test.go
@@ -179,8 +179,8 @@ func TestGCRABurstAsRate(t *testing.T) {
 
 	require.GreaterOrEqual(t, passed, rate)
 	require.LessOrEqual(
-		t, time.Since(start), time.Duration(window*int64(time.Second))/4,
-		"we should've been able to make the required request in less than 1/4th of the window due to the burst setting",
+		t, time.Since(start), time.Duration(window*int64(time.Second))/3,
+		"we should've been able to make the required request in less than 1/3rd of the window due to the burst setting",
 	)
 }
 
