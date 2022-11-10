3bbba27955708106843b5999af3c66ef2938d0e5
Francesco Casula <fra.casula@gmail.com>
2022-11-10 14:42:48 +0100

    chore: simplifying throttler option

--- router/throttler/options.go
+++ router/throttler/options.go
@@ -3,17 +3,11 @@ package throttler
 import "github.com/rudderlabs/rudder-server/utils/logger"
 
 // Option is a functional option for the Client, see With* functions for reference
-type Option interface {
-	apply(client *Client)
-}
-
-type withOption struct{ setup func(*Client) }
-
-func (w withOption) apply(c *Client) { w.setup(c) }
+type Option func(client *Client)
 
 // WithLogger allows to setup a logger for the Client
 func WithLogger(logger logger.Logger) Option {
-	return withOption{setup: func(c *Client) {
+	return func(c *Client) {
 		c.logger = logger
-	}}
+	}
 }
