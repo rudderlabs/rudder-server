f835845d05f02318df8c800bc125f80462b6570d
Francesco Casula <fra.casula@gmail.com>
2022-11-28 09:52:59 +0100

    chore: changing logging error

--- router/factory.go
+++ router/factory.go
@@ -97,7 +97,7 @@ func (f *Factory) initThrottlerFactory() {
 		return
 	}
 	if err != nil {
-		f.Logger.Errorf("Failed to create throttling algorithm: %s", err)
+		f.Logger.Errorf("Failed to create throttler: %s", err)
 		return
 	}
 
