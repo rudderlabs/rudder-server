28e55663322d53ff2e6907c5ac4635f6d6f0954b
Aris Tzoumas <atzoumas@rudderstack.com>
2022-12-06 09:24:20 +0200

    fixup! failing executing jobs(except gateway) instead of deleting them

--- jobsdb/jobsdb.go
+++ jobsdb/jobsdb.go
@@ -46,7 +46,6 @@ import (
 
 	"github.com/rudderlabs/rudder-server/config"
 	"github.com/rudderlabs/rudder-server/services/fileuploader"
-	"github.com/rudderlabs/rudder-server/services/metric"
 	"github.com/rudderlabs/rudder-server/services/stats"
 	"github.com/rudderlabs/rudder-server/utils/misc"
 
@@ -521,41 +520,6 @@ func (jd *HandleT) assert(cond bool, errorString string) {
 		jd.logger.Fatal(jd.dsEmptyResultCache)
 		panic(fmt.Errorf("[[ %s ]]: %s", jd.tablePrefix, errorString))
 	}
-}
-
-func (jd *HandleT) Status() interface{} {
-	statusObj := map[string]interface{}{
-		"dataset-list":    jd.getDSList(),
-		"dataset-ranges":  jd.getDSRangeList(),
-		"backups-enabled": jd.BackupSettings.isBackupEnabled(),
-	}
-	emptyResults := make(map[string]interface{})
-	for ds, entry := range jd.dsEmptyResultCache {
-		emptyResults[ds.JobTable] = entry
-	}
-	statusObj["empty-results-cache"] = emptyResults
-
-	pendingEventMetrics := metric.Instance.
-		GetRegistry(metric.PUBLISHED_METRICS).
-		GetMetricsByName(fmt.Sprintf(metric.JOBSDB_PENDING_EVENTS_COUNT, jd.tablePrefix))
-
-	if len(pendingEventMetrics) == 0 {
-		return statusObj
-	}
-
-	var pendingEvents []map[string]interface{}
-	for _, pendingEvent := range pendingEventMetrics {
-		count := pendingEvent.Value.(metric.Gauge).IntValue()
-		if count != 0 {
-			pendingEvents = append(pendingEvents, map[string]interface{}{
-				"tags":  pendingEvent.Tags,
-				"count": count,
-			})
-		}
-	}
-	statusObj["pending-events"] = pendingEvents
-
-	return statusObj
 }
 
 type jobStateT struct {
@@ -3363,104 +3327,6 @@ func (jd *HandleT) getImportingList(ctx context.Context, params GetQueryParamsT)
 }
 
 /*
-deleteJobStatus deletes the latest status of a batch of jobs
-This is only done during recovery, which happens during the server start.
-So, we don't have to worry about dsEmptyResultCache
-*/
-func (jd *HandleT) deleteJobStatus() {
-	err := jd.WithUpdateSafeTx(context.TODO(), func(tx UpdateSafeTx) error {
-		tags := statTags{CustomValFilters: []string{jd.tablePrefix}}
-		queryStat := jd.getTimerStat("jobsdb_delete_job_status_time", &tags)
-		queryStat.Start()
-		defer queryStat.End()
-
-		dsList := jd.getDSList()
-
-		for _, ds := range dsList {
-			ds := ds
-			if err := jd.deleteJobStatusDSInTx(tx.SqlTx(), ds); err != nil {
-				return err
-			}
-			tx.Tx().AddSuccessListener(func() {
-				jd.dropDSFromCache(ds)
-			})
-		}
-
-		return nil
-	})
-	jd.assertError(err)
-}
-
-func (jd *HandleT) deleteJobStatusDSInTx(txHandler transactionHandler, ds dataSetT) error {
-	tags := statTags{CustomValFilters: []string{jd.tablePrefix}}
-	queryStat := jd.getTimerStat("jobsdb_delete_job_status_ds_time", &tags)
-	queryStat.Start()
-	defer queryStat.End()
-
-	_, err := txHandler.Exec(
-		fmt.Sprintf(
-			`DELETE FROM %[1]q
-				WHERE id = ANY(
-					SELECT id from "v_last_%[1]s" where job_state='executing'
-				)`,
-			ds.JobStatusTable,
-		),
-	)
-	return err
-}
-
-/*
-failExecuting sets the state of the executing jobs to failed
-This is only done during recovery, which happens during the server start.
-So, we don't have to worry about dsEmptyResultCache
-*/
-func (jd *HandleT) failExecuting() {
-	err := jd.WithUpdateSafeTx(context.TODO(), func(tx UpdateSafeTx) error {
-		queryStat := jd.getTimerStat(
-			"jobsdb_fail_executing_time",
-			&statTags{CustomValFilters: []string{jd.tablePrefix}},
-		)
-		queryStat.Start()
-		defer queryStat.End()
-
-		dsList := jd.getDSList()
-
-		for _, ds := range dsList {
-			ds := ds
-			err := jd.failExecutingDSInTx(tx.SqlTx(), ds)
-			if err != nil {
-				return err
-			}
-			tx.Tx().AddSuccessListener(func() {
-				jd.dropDSFromCache(ds)
-			})
-		}
-		return nil
-	})
-	jd.assertError(err)
-}
-
-func (jd *HandleT) failExecutingDSInTx(txHandler transactionHandler, ds dataSetT) error {
-	queryStat := jd.getTimerStat(
-		"jobsdb_fail_executing_ds_time",
-		&statTags{CustomValFilters: []string{jd.tablePrefix}},
-	)
-	queryStat.Start()
-	defer queryStat.End()
-
-	_, err := txHandler.Exec(
-		fmt.Sprintf(
-			`UPDATE %[1]q SET job_state='failed'
-				WHERE id = ANY(
-					SELECT id from "v_last_%[1]s" where job_state='executing'
-				)`,
-			ds.JobStatusTable,
-		),
-	)
-	return err
-}
-
-/*
 GetProcessed returns events of a given state. This does not update any state itself and
 realises on the caller to update it. That means that successive calls to GetProcessed("failed")
 can return the same set of events. It is the responsibility of the caller to call it from
@@ -3627,49 +3493,6 @@ getExecuting returns events which  in executing state
 */
 func (jd *HandleT) getExecuting(ctx context.Context, params GetQueryParamsT) (JobsResult, error) { // skipcq: CRT-P0003
 	return jd.GetProcessed(ctx, params)
-}
-
-/*
-FailExecuting fails events whose latest job state is executing.
-
-This is only done during recovery, which happens during the server start.
-*/
-func (jd *HandleT) FailExecuting() {
-	tags := statTags{
-		CustomValFilters: []string{jd.tablePrefix},
-	}
-	command := func() interface{} {
-		jd.failExecuting()
-		return nil
-	}
-	_ = jd.executeDbRequest(newWriteDbRequest("fail_executing", &tags, command))
-}
-
-/*
-DeleteExecuting deletes events whose latest job state is executing.
-This is only done during recovery, which happens during the server start.
-*/
-func (jd *HandleT) DeleteExecuting() {
-	tags := statTags{CustomValFilters: []string{jd.tablePrefix}}
-	command := func() interface{} {
-		jd.deleteJobStatus()
-		return nil
-	}
-	_ = jd.executeDbRequest(newWriteDbRequest("delete_job_status", &tags, command))
-}
-
-/*
-Ping returns health check for pg database
-*/
-func (jd *HandleT) Ping() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	rows, err := jd.dbHandle.QueryContext(ctx, `SELECT 'Rudder DB Health Check'::text as message`)
-	if err != nil {
-		return err
-	}
-	_ = rows.Close()
-	return nil
 }
 
 func (jd *HandleT) getMaxIDForDs(ds dataSetT) int64 {
