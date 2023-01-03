2c4d50c8c91fdf028e91973167c47a49acbdc35a
Rohith BCS <rohith.bcs@gmail.com>
2023-01-03 14:41:28 +0530

    chore: review comments

--- services/debugger/cache/cache.go
+++ services/debugger/cache/cache.go
@@ -3,44 +3,33 @@ package cache
 //go:generate mockgen -destination=../../../mocks/services/debugger/cache/mock_cache.go -package=mocks_cache github.com/rudderlabs/rudder-server/services/debugger/cache CacheAny
 
 import (
+	"fmt"
 	"github.com/rudderlabs/rudder-server/services/debugger/cache/embeddedcache"
 	"github.com/rudderlabs/rudder-server/services/debugger/cache/listcache"
 	"github.com/rudderlabs/rudder-server/utils/logger"
 )
 
-var pkgLogger logger.Logger
+type cacheType int8
 
-type CacheAny interface {
-	Cache[any]
+const (
+	MemoryCacheType cacheType = iota
+	BadgerCacheType
+)
+
+type Cache[T any] interface {
+	Update(key string, value T)
+	ReadAndPopData(key string) []T
 }
 
-type Cache[E any] interface {
-	Update(key string, value E)
-	ReadAndPopData(key string) []E
-}
-
-type Impl[E any] struct{}
-
-type FactoryImpl[E any] struct{}
-
-type Factory[E any] interface {
-	New(cacheType string) Cache[E]
-}
-
-func init() {
-	pkgLogger = logger.NewLogger().Child("cache")
-}
-
-func (FactoryImpl[E]) New(cacheType string) Cache[E] {
-	switch cacheType {
-	case "memory":
-		pkgLogger.Info("Using in-memory cache")
-		return &listcache.ListCache[E]{}
-	case "goqpue":
-		pkgLogger.Info("Using goque cache")
-		return &embeddedcache.EmbeddedCache[E]{}
+func New[T any](ct cacheType, l logger.Logger) (Cache[T], error) {
+	switch ct {
+	case MemoryCacheType:
+		l.Info("Using in-memory cache")
+		return &listcache.ListCache[T]{}, nil
+	case BadgerCacheType:
+		l.Info("Using badger cache")
+		return &embeddedcache.EmbeddedCache[T]{}, nil
 	default:
-		pkgLogger.Info("Using default in-memory cache")
-		return &listcache.ListCache[E]{}
+		return nil, fmt.Errorf("cache type %v out of the known domain", ct)
 	}
 }
