496e1f21bfa7f1e2017c43b533052a04bac3cf94
Sai Sankeerth <sanpj2292@github.com>
2023-01-09 12:28:55 +0530

    fix: chunking high-value
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- router/router.go
+++ router/router.go
@@ -522,7 +522,7 @@ func (worker *workerT) workerProcess() {
 // Method for chunking using generics in golang
 func chunkBy[T any](items []T, chunkSize int) (chunks [][]T) {
 	for chunkSize < len(items) {
-		items, chunks = items[chunkSize:], append(chunks, items[0:1:chunkSize])
+		items, chunks = items[chunkSize:], append(chunks, items[0:chunkSize:chunkSize])
 	}
 	return append(chunks, items)
 }
