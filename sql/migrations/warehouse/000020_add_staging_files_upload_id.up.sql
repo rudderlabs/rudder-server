33e0072089acced0ab0398a96abf2f0a95eb5be4
Leonidas Vrachnis <leo.al.vra@gmail.com>
2023-02-01 11:37:54 +0100

    chore: add index on staging files upload_id

--- sql/migrations/warehouse/000020_add_staging_files_upload_id.up.sql
+++ sql/migrations/warehouse/000020_add_staging_files_upload_id.up.sql
@@ -5,3 +5,4 @@
 
 ALTER TABLE wh_staging_files ADD COLUMN IF NOT EXISTS upload_id INTEGER REFERENCES wh_uploads (id) ON DELETE SET NULL(upload_id);
 
+CREATE INDEX wh_staging_files_upload_id_index ON wh_staging_files(upload_id);
