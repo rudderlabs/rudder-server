f973498f5db207e8ad6ecaad52acdd22ec70272a
Priyam <priyam@rudderstack.com>
2023-03-07 18:15:17 +0530

    test develop-rudder-transformer

--- testhelper/destination/transformer.go
+++ testhelper/destination/transformer.go
@@ -22,15 +22,15 @@ func SetupTransformer(pool *dockertest.Pool, d cleaner) (*TransformerResource, e
 	// pulls an image first to make sure we don't have an old cached version locally,
 	// then it creates a container based on it and runs it
 	err := pool.Client.PullImage(docker.PullImageOptions{
-		Repository: "rudderlabs/rudder-transformer",
-		Tag:        "latest",
+		Repository: "rudderlabs/develop-rudder-transformer",
+		Tag:        "chore.refactor-typescript",
 	}, docker.AuthConfiguration{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to pull image: %w", err)
 	}
 	transformerContainer, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository:   "rudderlabs/rudder-transformer",
-		Tag:          "latest",
+		Repository:   "rudderlabs/develop-rudder-transformer",
+		Tag:          "chore.refactor-typescript",
 		ExposedPorts: []string{"9090"},
 		Env: []string{
 			"CONFIG_BACKEND_URL=https://api.rudderstack.com",
