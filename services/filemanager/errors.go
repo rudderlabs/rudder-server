6c3a5f2676eb2f04dc82d515b55cfb1e10cb2c32
Akash Chetty <achetty.iitr@gmail.com>
2023-03-16 21:15:53 +0530

    error code refactoring

--- services/filemanager/errors.go
+++ services/filemanager/errors.go
@@ -6,20 +6,18 @@ import (
 )
 
 func suppressMinorErrors(err error) error {
-	if err != nil {
-		if azError, ok := err.(azblob.StorageError); ok { // This error is a Service-specific
-			switch azError.ServiceCode() { // Compare serviceCode to ServiceCodeXxx constants
-			case azblob.ServiceCodeContainerAlreadyExists:
-				pkgLogger.Debug("Received 409. Container already exists")
-				return nil
-			}
+	switch err := err.(type) {
+	case azblob.StorageError:
+		switch err.ServiceCode() { // Compare serviceCode to ServiceCodeXxx constants
+		case azblob.ServiceCodeContainerAlreadyExists:
+			pkgLogger.Debug("Received 409. Container already exists")
+			return nil
 		}
-		if minioError, ok := err.(minio.ErrorResponse); ok {
-			switch minioError.Code {
-			case "BucketAlreadyOwnedByYou":
-				pkgLogger.Debug("Received 409. Bucket already exists")
-				return nil
-			}
+	case minio.ErrorResponse:
+		switch err.Code {
+		case "BucketAlreadyOwnedByYou":
+			pkgLogger.Debug("Received 409. Bucket already exists")
+			return nil
 		}
 	}
 	return err
