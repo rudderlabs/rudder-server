196558082280270ebfcc36cff3eac161163de18d
Rohith BCS <rohith.bcs@gmail.com>
2023-03-17 14:59:23 +0530

    chore: fix namespace bug (#3110)

---
+++ warehouse/testdata/sql/namespace_test.sql
@@ -0,0 +1,6 @@
+BEGIN;
+INSERT INTO wh_schemas(id,wh_upload_id, source_id, namespace, destination_id, destination_type,
+                        schema, error, created_at, updated_at)
+VALUES (1,1, 'test-sourceID', 'test-namespace', 'test-destinationID', 'POSTGRES','{}', NULL,
+        '2022-12-06 15:23:37.100685', '2022-12-06 15:23:37.100685');
+COMMIT;
