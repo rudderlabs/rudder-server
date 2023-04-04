22a282ff59a14e9af0c42648b2270cd3f7e96ba5
Rohith BCS <rohith.bcs@gmail.com>
2023-04-04 13:19:06 +0530

    chore: gofumpt fixes

--- enterprise/replay/dumpsloader.go
+++ enterprise/replay/dumpsloader.go
@@ -24,6 +24,7 @@ import (
 )
 
 const DumpsLoaderFinishPath = "dumps-loader-done"
+
 var reFilename = regexp.MustCompile(`^\d+\.[^.]+(\d+)\.(\d+)-(\d+)`)
 
 // DumpsLoaderHandleT - dumps-loader handle
