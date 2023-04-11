3f02385822c8be4843eb8942ef4916214467727c
Aris Tzoumas <atzoumas@rudderstack.com>
2023-04-11 12:19:42 +0300

    chore: debugging

--- internal/pulsar/pulsar_test.go
+++ internal/pulsar/pulsar_test.go
@@ -1,9 +1,13 @@
 package pulsar
 
 import (
+	"context"
+	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 
+	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/ory/dockertest/v3"
 	"github.com/rudderlabs/rudder-go-kit/config"
 	"github.com/rudderlabs/rudder-go-kit/logger"
@@ -13,14 +17,15 @@ import (
 
 func Test_Pulsar(t *testing.T) {
 	var (
-		topic = "mytopic"
-		// payload          = []byte("test-message")
-		// key              = "test-key"
-		// subscriptionName = "test-subscription"
-		// counter          uint32
+		topic            = "test-topic"
+		payload          = []byte("test-message")
+		key              = "test-key"
+		subscriptionName = "test-subscription"
+		counter          uint32
 	)
 	pulsarContainer := PulsarResource(t)
-	time.Sleep(10 * time.Second)
+	time.Sleep(5 * time.Second)
+
 	conf := config.New()
 	conf.Set("Pulsar.Client.url", pulsarContainer.URL)
 	conf.Set("Pulsar.Producer.topic", topic)
@@ -32,47 +37,47 @@ func Test_Pulsar(t *testing.T) {
 	require.NotNil(t, producer)
 	defer producer.Close()
 
-	// consumer, err := client.Client.Subscribe(pulsar.ConsumerOptions{
-	// 	Topic:            topic,
-	// 	SubscriptionName: subscriptionName,
-	// })
-	// require.NoError(t, err)
-	// defer consumer.Close()
-	// err = producer.SendMessage(context.Background(), key, "", payload)
-	// require.NoError(t, err)
-	// msg, err := consumer.Receive(context.Background())
-	// require.NoError(t, err)
-	// require.Equal(t, msg.Payload(), payload)
-	// require.Equal(t, msg.Key(), key)
+	consumer, err := client.Client.Subscribe(pulsar.ConsumerOptions{
+		Topic:            topic,
+		SubscriptionName: subscriptionName,
+	})
+	require.NoError(t, err)
+	defer consumer.Close()
+	err = producer.SendMessage(context.Background(), key, "", payload)
+	require.NoError(t, err)
+	msg, err := consumer.Receive(context.Background())
+	require.NoError(t, err)
+	require.Equal(t, msg.Payload(), payload)
+	require.Equal(t, msg.Key(), key)
 
-	// for i := 0; i < 10; i++ {
-	// 	producer.SendMessageAsync(context.Background(), key, "", []byte(fmt.Sprintf("test-message-%d", i)), func(id pulsar.MessageID, message *pulsar.ProducerMessage, err error) {
-	// 		require.NoError(t, err)
-	// 		atomic.AddUint32(&counter, 1)
-	// 	})
-	// }
-	// err = producer.Flush()
-	// require.NoError(t, err)
-	// require.Equal(t, atomic.LoadUint32(&counter), uint32(10))
-	// for i := 0; i < 10; i++ {
-	// 	msg, err := consumer.Receive(context.Background())
-	// 	require.NoError(t, err)
-	// 	require.Equal(t, msg.Payload(), []byte(fmt.Sprintf("test-message-%d", i)))
-	// 	require.Equal(t, msg.Key(), key)
-	// }
+	for i := 0; i < 10; i++ {
+		producer.SendMessageAsync(context.Background(), key, "", []byte(fmt.Sprintf("test-message-%d", i)), func(id pulsar.MessageID, message *pulsar.ProducerMessage, err error) {
+			require.NoError(t, err)
+			atomic.AddUint32(&counter, 1)
+		})
+	}
+	err = producer.Flush()
+	require.NoError(t, err)
+	require.Equal(t, atomic.LoadUint32(&counter), uint32(10))
+	for i := 0; i < 10; i++ {
+		msg, err := consumer.Receive(context.Background())
+		require.NoError(t, err)
+		require.Equal(t, msg.Payload(), []byte(fmt.Sprintf("test-message-%d", i)))
+		require.Equal(t, msg.Key(), key)
+	}
 }
 
-//func Test_PulsarInterface(t *testing.T) {
-//	topic := "test-topic"
-//	pulsarContainer := PulsarResource(t)
-//	conf := config.New()
-//	conf.Set("Pulsar.Client.url", pulsarContainer.URL)
-//	conf.Set("Pulsar.Producer.topic", topic)
-//	producer, err := New(conf)
-//	require.NoError(t, err)
-//	require.NotNil(t, producer)
-//	defer producer.Close()
-//}
+func Test_PulsarInterface(t *testing.T) {
+	topic := "test-topic"
+	pulsarContainer := PulsarResource(t)
+	conf := config.New()
+	conf.Set("Pulsar.Client.url", pulsarContainer.URL)
+	conf.Set("Pulsar.Producer.topic", topic)
+	producer, err := New(conf)
+	require.NoError(t, err)
+	require.NotNil(t, producer)
+	defer producer.Close()
+}
 
 // PulsarResource returns a pulsar container resource
 func PulsarResource(t *testing.T) *resource.PulsarResource {
