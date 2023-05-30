332342744b2f7e77ac78ac961628c5a065d2ca74
Akash Chetty <achetty.iitr@gmail.com>
2023-05-30 15:06:41 +0530

    chore: cleanup for integration tests for warehouse (#3412)

--- warehouse/integrations/deltalake-native/deltalake_test.go
+++ warehouse/integrations/deltalake-native/deltalake_test.go
@@ -76,10 +76,6 @@ func TestIntegration(t *testing.T) {
 	}
 
 	c := testcompose.New(t, compose.FilePaths([]string{"testdata/docker-compose.yml", "../testdata/docker-compose.jobsdb.yml"}))
-
-	t.Cleanup(func() {
-		c.Stop(context.Background())
-	})
 	c.Start(context.Background())
 
 	misc.Init()
