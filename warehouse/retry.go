ba0427d4fb0e5eb58d4f2b405a60a3dd19af68ae
Francesco Casula <fra.casula@gmail.com>
2023-07-07 17:58:15 +0200

    fix: retry

--- warehouse/retry.go
+++ warehouse/retry.go
@@ -131,13 +131,8 @@ func (retryReq *RetryRequest) UploadsToRetry(ctx context.Context) (response Retr
 	return
 }
 
-func (retryReq *RetryRequest) getSourceIDs() (sourceIDs []string) {
-	sourceIDsByWorkspaceLock.RLock()
-	defer sourceIDsByWorkspaceLock.RUnlock()
-	var ok bool
-	if sourceIDs, ok = sourceIDsByWorkspace[retryReq.WorkspaceID]; !ok {
-		sourceIDs = []string{}
-	}
+func (retryReq *RetryRequest) getSourceIDs() []string {
+	sourceIDs, _ := bcManager.SourceIDsByWorkspace()[retryReq.WorkspaceID] // TODO remove global variable
 	return sourceIDs
 }
 
