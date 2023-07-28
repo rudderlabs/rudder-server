5cbd901a3008ef3ed31e79a8ac95051f33ce973c
Pavan Chaithanya <pavan@rudderstack.com>
2023-07-28 11:43:39 +0530

    chore: add custom buckets to gateway.user_suppression_age

--- runner/buckets.go
+++ runner/buckets.go
@@ -4,4 +4,7 @@ var customBuckets = map[string][]float64{
 	"gateway.response_time": {
 		0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 60,
 	},
+	"gateway.user_suppression_age": {
+		86400, 2592000, 5184000, 7776000, // 1 day, 1 month, 2 months, 3 months
+	},
 }
