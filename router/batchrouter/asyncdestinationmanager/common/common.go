7aa9b60ee06b82303cbc7674f3d98f62fcc34e0b
chandumlg <54652834+chandumlg@users.noreply.github.com>
2023-08-02 22:41:06 -0500

    fix: batch router attemptnos (#3696)
    * fix: initial commit
    
    * chore: clean up
    
    * chore: clean up
    
    * chore: clean up
    
    * chore: clean up
    
    ---------
    
    Co-authored-by: shrouti1507 <shrouti.gangopadhyay@gmail.com>
    Co-authored-by: Chandra shekar Varkala <chandra@rudderlabs.com>

--- router/batchrouter/asyncdestinationmanager/common/common.go
+++ router/batchrouter/asyncdestinationmanager/common/common.go
@@ -77,7 +77,6 @@ type AsyncUploadT struct {
 
 type UploadStruct struct {
 	ImportId string                 `json:"importId"`
-	PollUrl  string                 `json:"pollURL"`
 	Metadata map[string]interface{} `json:"metadata"`
 }
 
@@ -86,7 +85,6 @@ type MetaDataT struct {
 }
 type ImportParameters struct {
 	ImportId string    `json:"importId"`
-	PollUrl  *string   `json:"pollURL"`
 	MetaData MetaDataT `json:"metadata"`
 }
 
@@ -105,6 +103,7 @@ type AsyncDestinationStruct struct {
 	RsourcesStats        rsources.StatsCollector
 	Destination          *backendconfig.DestinationT
 	Manager              AsyncDestinationManager
+	AttemptNums          map[int64]int
 }
 
 type AsyncFailedPayload struct {
