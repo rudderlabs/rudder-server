f1c3ac1aba71fd861c06db300cc6618b8b0c6acd
Akash Chetty <achetty.iitr@gmail.com>
2023-08-10 15:53:53 +0530

    chore: remove Init and exported struct variables from Archiver

--- runner/runner.go
+++ runner/runner.go
@@ -47,7 +47,6 @@ import (
 	"github.com/rudderlabs/rudder-server/utils/misc"
 	"github.com/rudderlabs/rudder-server/utils/types/deployment"
 	"github.com/rudderlabs/rudder-server/warehouse"
-	warehousearchiver "github.com/rudderlabs/rudder-server/warehouse/archive"
 	"github.com/rudderlabs/rudder-server/warehouse/encoding"
 	warehouseutils "github.com/rudderlabs/rudder-server/warehouse/utils"
 	"github.com/rudderlabs/rudder-server/warehouse/validations"
@@ -340,7 +339,6 @@ func runAllInit() {
 	warehouse.Init()
 	warehouse.Init4()
 	warehouse.Init6()
-	warehousearchiver.Init()
 	validations.Init()
 	webhook.Init()
 	asyncdestinationmanager.Init()
