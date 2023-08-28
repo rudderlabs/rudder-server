aa187e20e55fbb13adb6ecaf35ac6cef4ce15316
siddarth.msv <82795818+Sidddddarth@users.noreply.github.com>
2023-08-28 18:01:36 +0530

    Update router/worker.go
    using `strconv.FormatBool` instead of `fmt.Sprint`
    
    Co-authored-by: Leonidas Vrachnis <leo.al.vra@gmail.com>

--- router/worker.go
+++ router/worker.go
@@ -820,7 +820,7 @@ func (w *worker) sendRouterResponseCountStat(status *jobsdb.JobStatusT, destinat
 		"alert": strconv.FormatBool(alert),
 		// To specify at which point failure happened
 		"errorAt": errorAt,
-		"retry":   fmt.Sprint(status.AttemptNum > 1),
+		"retry":   strconv.FormatBool(status.AttemptNum > 1),
 	})
 	routerResponseStat.Count(1)
 }
