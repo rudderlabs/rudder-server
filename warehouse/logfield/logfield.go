ce2ed332a45b3deafc7284e2629fb21a3f756aed
Francesco Casula <fracasula@users.noreply.github.com>
2023-08-28 10:42:19 +0200

    chore: warehouse tests race detection (#3773)
    * chore: warehouse tests race detection
    
    * temp
    
    * temp
    
    * temp
    
    * chore: fix defer
    
    * chore: removed unused lock
    
    * fix: unlocking of RWMutex
    
    * fix: unlocking of RWMutex
    
    * chore: fixing err data race
    
    * chore: removing parallel
    
    * chore: go-kit v0.15.6
    
    * chore: removing uploadLock
    
    * chore: removing uploadSchema mutex
    
    * chore: removing TODO
    
    * chore: go-kit v0.15.7

--- warehouse/logfield/logfield.go
+++ warehouse/logfield/logfield.go
@@ -29,5 +29,6 @@ const (
 	QueryExecutionTime         = "queryExecutionTime"
 	StagingTableName           = "stagingTableName"
 	TotalRows                  = "totalRows"
+	WarehouseID                = "warehouseID"
 	SampleDuplicateMessages    = "sampleDuplicateMessages"
 )
