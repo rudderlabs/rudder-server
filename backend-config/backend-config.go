e6c7b5cbc984d14430c0e7c3eb9ec3de81516cb0
kanishkkatara <kanishk@rudderstack.com>
2023-09-18 22:39:14 +0530

    fix: fix in workspace config creation

--- backend-config/backend-config.go
+++ backend-config/backend-config.go
@@ -156,6 +156,7 @@ func filterProcessorEnabledDestinations(config ConfigT) ConfigT {
 		source.Destinations = destinations
 		modifiedConfig.Sources = append(modifiedConfig.Sources, source)
 	}
+	modifiedConfig.Settings.EventAuditEnabled = config.Settings.EventAuditEnabled
 	return modifiedConfig
 }
 
