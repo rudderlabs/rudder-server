1fa2f45bf26981bff5552cb9c775f81e5ada7629
Akash Chetty <achetty.iitr@gmail.com>
2023-09-19 19:37:56 +0530

    fix: error message for deltalake during test connection (#3883)

--- warehouse/utils/utils.go
+++ warehouse/utils/utils.go
@@ -184,10 +184,9 @@ var DiscardsSchema = map[string]string{
 }
 
 const (
-	LoadFileTypeCsv       = "csv"
-	LoadFileTypeJson      = "json"
-	LoadFileTypeParquet   = "parquet"
-	TestConnectionTimeout = 15 * time.Second
+	LoadFileTypeCsv     = "csv"
+	LoadFileTypeJson    = "json"
+	LoadFileTypeParquet = "parquet"
 )
 
 func Init() {
