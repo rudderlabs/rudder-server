b80e5e1071c7b70ccd343df1b5779c48459cf7de
Aris Tzoumas <atzoumas@rudderstack.com>
2023-09-25 23:46:35 +0300

    fix: loopvar issue

--- backend-config/replay_types_test.go
+++ backend-config/replay_types_test.go
@@ -20,7 +20,12 @@ func TestApplyReplayConfig(t *testing.T) {
 					},
 					Destinations: []DestinationT{
 						{
-							ID: "d-1",
+							ID:                 "d-1",
+							IsProcessorEnabled: true,
+						},
+						{
+							ID:                 "d-2",
+							IsProcessorEnabled: false,
 						},
 					},
 				},
@@ -56,6 +61,7 @@ func TestApplyReplayConfig(t *testing.T) {
 		require.Equal(t, map[string]interface{}{}, c.Sources[1].Config)
 		require.Len(t, c.Sources[1].Destinations, 1)
 		require.Equal(t, "er-d-1", c.Sources[1].Destinations[0].ID)
+		require.True(t, c.Sources[1].Destinations[0].IsProcessorEnabled)
 	})
 
 	t.Run("Invalid Replay Config", func(t *testing.T) {
