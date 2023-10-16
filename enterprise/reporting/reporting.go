d46f9e6a66e476318b8f07321cd2f1731e052736
Mihir Khambhati <inmihir11@gmail.com>
2023-10-16 15:46:49 +0530

    fix: add error log for reporting metrics

--- enterprise/reporting/reporting.go
+++ enterprise/reporting/reporting.go
@@ -368,6 +368,8 @@ func (r *DefaultReporter) mainLoop(ctx context.Context, c types.SyncerConfig) {
 		if len(reports) == 0 {
 			if err == nil {
 				lastReportedAtTime.Store(loopStart)
+			} else {
+				fmt.Println(err)
 			}
 			select {
 			case <-ctx.Done():
