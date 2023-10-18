7e01db9015ecc80c0ab575e29a4fbfb9a92e2023
ItsSudip <sudip.paul1997@gmail.com>
2023-10-18 20:52:41 +0530

    add logic to consider some warning statusCode as success

--- router/batchrouter/asyncdestinationmanager/eloqua/types.go
+++ router/batchrouter/asyncdestinationmanager/eloqua/types.go
@@ -19,14 +19,16 @@ type EloquaService interface {
 }
 
 type EloquaBulkUploader struct {
-	destName      string
-	logger        logger.Logger
-	authorization string
-	baseEndpoint  string
-	fileSizeLimit int64
-	eventsLimit   int64
-	service       EloquaService
-	jobToCSVMap   map[int64]int64
+	destName          string
+	logger            logger.Logger
+	authorization     string
+	baseEndpoint      string
+	fileSizeLimit     int64
+	eventsLimit       int64
+	service           EloquaService
+	jobToCSVMap       map[int64]int64
+	uniqueKeys        []string
+	successStatusCode []string
 }
 type DestinationConfig struct {
 	CompanyName              string   `json:"companyName"`
