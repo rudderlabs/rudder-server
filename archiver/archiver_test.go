ea14151356d06c450fd0f8cb0ba809e3fddfd105
Rohith BCS <rohith.bcs@gmail.com>
2023-10-30 16:23:39 +0530

    Apply suggestions from code review
    Co-authored-by: Akash Chetty <achetty.iitr@gmail.com>

--- archiver/archiver_test.go
+++ archiver/archiver_test.go
@@ -255,24 +255,24 @@ type jdWrapper struct {
 	queries *int32
 }
 
-func (jd jdWrapper) GetDistinctParameterValues(_ context.Context, _ string) ([]string, error) {
+func (jd jdWrapper) GetDistinctParameterValues(context.Context, string) ([]string, error) {
 	atomic.AddInt32(jd.queries, 1)
 	return []string{}, nil
 }
 
 func (jd jdWrapper) GetUnprocessed(
-	_ context.Context,
-	_ jobsdb.GetQueryParams,
+	context.Context,
+	jobsdb.GetQueryParams,
 ) (jobsdb.JobsResult, error) {
 	atomic.AddInt32(jd.queries, 1)
 	return jobsdb.JobsResult{}, nil
 }
 
 func (jd jdWrapper) UpdateJobStatus(
-	_ context.Context,
-	_ []*jobsdb.JobStatusT,
-	_ []string,
-	_ []jobsdb.ParameterFilterT,
+	context.Context,
+	[]*jobsdb.JobStatusT,
+	[]string,
+	[]jobsdb.ParameterFilterT,
 ) error {
 	atomic.AddInt32(jd.queries, 1)
 	return nil
