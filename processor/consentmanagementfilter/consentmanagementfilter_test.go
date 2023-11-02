9027cbf7c4706a7ca4ec57d6a913d86ea07c592e
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2023-11-02 15:31:58 +0530

    test: add more unit tests for consentmanagementfilter

--- processor/consentmanagementfilter/consentmanagementfilter_test.go
+++ processor/consentmanagementfilter/consentmanagementfilter_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	backendconfig "github.com/rudderlabs/rudder-server/backend-config"
 	"github.com/rudderlabs/rudder-server/utils/types"
 )
 
@@ -140,6 +141,188 @@ func TestGetConsentManagementInfo(t *testing.T) {
 	for _, testCase := range testCases {
 		t.Run(testCase.description, func(t *testing.T) {
 			actual := GetConsentManagementInfo(testCase.input)
+			require.Equal(t, testCase.expected, actual)
+		})
+	}
+}
+
+func TestGetGenericConsentManagementData(t *testing.T) {
+	type testCaseT struct {
+		description string
+		input       *backendconfig.DestinationT
+		expected    map[string]GenericConsentManagementProviderData
+	}
+
+	defGenericConsentManagementData := make(map[string]GenericConsentManagementProviderData)
+	testCases := []testCaseT{
+		{
+			description: "should return empty generic consent management data when no consent management config is present",
+			input: &backendconfig.DestinationT{
+				Config: map[string]interface{}{},
+			},
+			expected: defGenericConsentManagementData,
+		},
+		{
+			description: "should return empty generic consent management data when consent management config is malformed",
+			input: &backendconfig.DestinationT{
+				Config: map[string]interface{}{
+					"consentManagement": make(chan int),
+				},
+			},
+			expected: defGenericConsentManagementData,
+		},
+		{
+			description: "should return empty generic consent management data when consent management config is not as per the expected structure",
+			input: &backendconfig.DestinationT{
+				Config: map[string]interface{}{
+					"consentManagement": map[string]interface{}{ // this should be an array
+						"provider": "dummy",
+					},
+				},
+			},
+			expected: defGenericConsentManagementData,
+		},
+		{
+			description: "should return empty generic consent management data when consent management config entries count is 0",
+			input: &backendconfig.DestinationT{
+				Config: map[string]interface{}{
+					"consentManagement": []interface{}{},
+				},
+			},
+			expected: defGenericConsentManagementData,
+		},
+		{
+			description: "should return generic consent management data when consent management config entries are properly defined",
+			input: &backendconfig.DestinationT{
+				Config: map[string]interface{}{
+					"consentManagement": []interface{}{
+						map[string]interface{}{
+							"provider": "oneTrust",
+							"consents": []map[string]interface{}{
+								{
+									"consent": "consent category 1",
+								},
+								{
+									"consent": "",
+								},
+								{
+									"consent": "consent category 2",
+								},
+							},
+						},
+						map[string]interface{}{
+							"provider": "ketch",
+							"consents": []map[string]interface{}{
+								{
+									"consent": "purpose 1",
+								},
+								{
+									"consent": "",
+								},
+								{
+									"consent": "purpose 2",
+								},
+							},
+						},
+						map[string]interface{}{
+							"provider": "custom",
+							"consents": []map[string]interface{}{
+								{
+									"consent": "custom consent 1",
+								},
+								{
+									"consent": "",
+								},
+								{
+									"consent": "custom consent 2",
+								},
+								{
+									"consent": "",
+								},
+							},
+							"resolutionStrategy": "or",
+						},
+					},
+				},
+			},
+			expected: map[string]GenericConsentManagementProviderData{
+				"oneTrust": {
+					Consents: []string{
+						"consent category 1",
+						"consent category 2",
+					},
+				},
+				"ketch": {
+					Consents: []string{
+						"purpose 1",
+						"purpose 2",
+					},
+				},
+				"custom": {
+					Consents: []string{
+						"custom consent 1",
+						"custom consent 2",
+					},
+					ResolutionStrategy: "or",
+				},
+			},
+		},
+		{
+			description: "should return generic consent management data excluding the providers with empty consents",
+			input: &backendconfig.DestinationT{
+				Config: map[string]interface{}{
+					"consentManagement": []interface{}{
+						map[string]interface{}{
+							"provider": "oneTrust",
+							"consents": []map[string]interface{}{
+								{
+									"consent": "consent category 1",
+								},
+								{
+									"consent": "",
+								},
+								{
+									"consent": "consent category 2",
+								},
+							},
+						},
+						map[string]interface{}{
+							"provider": "ketch",
+							"consents": []map[string]interface{}{
+								{
+									"consent": "",
+								},
+							},
+						},
+						map[string]interface{}{
+							"provider": "custom",
+							"consents": []map[string]interface{}{
+								{
+									"consent": "",
+								},
+								{
+									"consent": "",
+								},
+							},
+							"resolutionStrategy": "or",
+						},
+					},
+				},
+			},
+			expected: map[string]GenericConsentManagementProviderData{
+				"oneTrust": {
+					Consents: []string{
+						"consent category 1",
+						"consent category 2",
+					},
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			actual := GetGenericConsentManagementData(testCase.input)
 			require.Equal(t, testCase.expected, actual)
 		})
 	}
