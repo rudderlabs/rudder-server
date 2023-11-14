73ad5337da94e3886f65b5e31b88f9a858f94d0e
Francesco Casula <fra.casula@gmail.com>
2023-11-14 15:10:42 +0100

    chore: improving warehouse destination config

--- warehouse/internal/model/warehouse.go
+++ warehouse/internal/model/warehouse.go
@@ -2,6 +2,17 @@ package model
 
 import backendconfig "github.com/rudderlabs/rudder-server/backend-config"
 
+type DestinationConfigSetting interface{ string() string }
+
+type destConfSetting string
+
+func (s destConfSetting) string() string { return string(s) }
+
+var (
+	PreferAppendSetting     DestinationConfigSetting = destConfSetting("preferAppend")
+	UseRudderStorageSetting DestinationConfigSetting = destConfSetting("useRudderStorage")
+)
+
 type Warehouse struct {
 	WorkspaceID string
 	Source      backendconfig.SourceT
@@ -11,12 +22,23 @@ type Warehouse struct {
 	Identifier  string
 }
 
-func (w *Warehouse) GetBoolDestinationConfig(key string) bool {
+func (w *Warehouse) GetBoolDestinationConfig(key DestinationConfigSetting) bool {
 	destConfig := w.Destination.Config
-	if destConfig[key] != nil {
-		if val, ok := destConfig[key].(bool); ok {
+	if destConfig[key.string()] != nil {
+		if val, ok := destConfig[key.string()].(bool); ok {
 			return val
 		}
 	}
 	return false
+}
+
+func (w *Warehouse) GetPreferAppendSetting() bool {
+	destConfig := w.Destination.Config
+	value, ok := destConfig[PreferAppendSetting.string()].(bool)
+	if !ok {
+		// when the value is not defined it is important we choose the right default
+		// in order to maintain backward compatibility for existing destinations
+		return w.Type == "BQ" // defaulting to true for BQ, false for other destination types
+	}
+	return value
 }
