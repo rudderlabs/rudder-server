a243c272621438cff52c40e08e801b5d450ab2fe
gane5hvarma <raghavag772@gmail.com>
2023-12-18 13:46:24 -0500

    change eventstream to kebab case

--- enterprise/reporting/event_stats.go
+++ enterprise/reporting/event_stats.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 
 	"github.com/rudderlabs/rudder-go-kit/stats"
+	"github.com/rudderlabs/rudder-server/jobsdb"
 	. "github.com/rudderlabs/rudder-server/utils/tx" //nolint:staticcheck
 	"github.com/rudderlabs/rudder-server/utils/types"
 )
@@ -13,7 +14,7 @@ type EventStatsReporter struct {
 	configSubscriber *configSubscriber
 }
 
-const EventStream = "EventStream"
+const EventStream = "event-stream"
 
 func NewEventStatsReporter(configSubscriber *configSubscriber, stats stats.Stats) *EventStatsReporter {
 	return &EventStatsReporter{
@@ -26,9 +27,14 @@ const EventsProcessedMetricName = "events_processed_total"
 
 func (es *EventStatsReporter) Record(metrics []*types.PUReportedMetric) {
 	for index := range metrics {
-		var sourceCategory = metrics[index].ConnectionDetails.SourceCategory
+		sourceCategory := metrics[index].ConnectionDetails.SourceCategory
 		if sourceCategory == "" {
 			sourceCategory = EventStream
+		}
+		terminal := strconv.FormatBool(metrics[index].PUDetails.TerminalPU)
+		status := metrics[index].StatusDetail.Status
+		if status == jobsdb.Aborted.State {
+			terminal = "true"
 		}
 		tags := stats.Tags{
 			"workspaceId":     es.configSubscriber.WorkspaceIDFromSource(metrics[index].ConnectionDetails.SourceID),
@@ -37,9 +43,9 @@ func (es *EventStatsReporter) Record(metrics []*types.PUReportedMetric) {
 			"reportedBy":      metrics[index].PUDetails.PU,
 			"sourceCategory":  sourceCategory,
 			"statusCode":      strconv.Itoa(metrics[index].StatusDetail.StatusCode),
-			"terminal":        strconv.FormatBool(metrics[index].PUDetails.TerminalPU),
+			"terminal":        terminal,
 			"destinationType": es.configSubscriber.GetDestDetail(metrics[index].ConnectionDetails.DestinationID).destType,
-			"status":          metrics[index].StatusDetail.Status,
+			"status":          status,
 		}
 		es.stats.NewTaggedStat(EventsProcessedMetricName, stats.CountType, tags).Count(int(metrics[index].StatusDetail.Count))
 	}
