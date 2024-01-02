25bc4d7cf2b7b4ed6cca9cd67ba06409eb5ab8ad
Francesco Casula <fra.casula@gmail.com>
2024-01-02 10:23:24 +0100

    chore: custom buckets for gw request size

--- runner/buckets.go
+++ runner/buckets.go
@@ -1,6 +1,15 @@
 package runner
 
 var customBuckets = map[string][]float64{
+	"gateway.request_size": {
+		10,       // 10 bytes
+		100,      // 100 bytes
+		1000,     // 1kb
+		10000,    // 10kb
+		100000,   // 100kb
+		1000000,  // 1mb
+		10000000, // 10mb
+	},
 	"gateway.response_time": {
 		0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 60,
 	},
