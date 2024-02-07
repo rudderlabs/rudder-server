ed301a3685c937880fdecd258d61c4f688b7ed93
siddarth.msv <82795818+Sidddddarth@users.noreply.github.com>
2024-02-07 12:36:29 +0530

    fix: jobs not draining as fast as we'd like because of rate-limiting etc. (#4327)

--- router/handle.go
+++ router/handle.go
@@ -186,8 +186,9 @@ func (rt *Handle) pickup(ctx context.Context, partition string, workers []*worke
 	}
 
 	type reservedJob struct {
-		slot *workerSlot
-		job  *jobsdb.JobT
+		slot        *workerSlot
+		job         *jobsdb.JobT
+		drainReason string
 	}
 
 	var statusList []*jobsdb.JobStatusT
@@ -212,7 +213,7 @@ func (rt *Handle) pickup(ctx context.Context, partition string, workers []*worke
 		rt.logger.Debugf("[DRAIN DEBUG] counts  %v final jobs length being processed %v", rt.destType, len(reservedJobs))
 		assignedTime := time.Now()
 		for _, reservedJob := range reservedJobs {
-			reservedJob.slot.Use(workerJob{job: reservedJob.job, assignedAt: assignedTime})
+			reservedJob.slot.Use(workerJob{job: reservedJob.job, assignedAt: assignedTime, drainReason: reservedJob.drainReason})
 		}
 		pickupCount += len(reservedJobs)
 		reservedJobs = nil
@@ -238,7 +239,7 @@ func (rt *Handle) pickup(ctx context.Context, partition string, workers []*worke
 			firstJob = job
 		}
 		lastJob = job
-		slot, err := rt.findWorkerSlot(ctx, workers, job, blockedOrderKeys)
+		workerJobSlot, err := rt.findWorkerSlot(ctx, workers, job, blockedOrderKeys)
 		if err == nil {
 			traceParent := gjson.GetBytes(job.Parameters, "traceparent").String()
 			if traceParent != "" {
@@ -269,7 +270,7 @@ func (rt *Handle) pickup(ctx context.Context, partition string, workers []*worke
 				WorkspaceId:   job.WorkspaceId,
 			}
 			statusList = append(statusList, &status)
-			reservedJobs = append(reservedJobs, reservedJob{slot: slot, job: job})
+			reservedJobs = append(reservedJobs, reservedJob{slot: workerJobSlot.slot, job: job, drainReason: workerJobSlot.drainReason})
 			if shouldFlush() {
 				flush()
 			}
@@ -516,7 +517,12 @@ func (rt *Handle) getQueryParams(partition string, pickUpCount int) jobsdb.GetQu
 	return params
 }
 
-func (rt *Handle) findWorkerSlot(ctx context.Context, workers []*worker, job *jobsdb.JobT, blockedOrderKeys map[string]struct{}) (*workerSlot, error) {
+type workerJobSlot struct {
+	slot        *workerSlot
+	drainReason string
+}
+
+func (rt *Handle) findWorkerSlot(ctx context.Context, workers []*worker, job *jobsdb.JobT, blockedOrderKeys map[string]struct{}) (*workerJobSlot, error) {
 	if rt.backgroundCtx.Err() != nil {
 		return nil, types.ErrContextCancelled
 	}
@@ -537,23 +543,29 @@ func (rt *Handle) findWorkerSlot(ctx context.Context, workers []*worker, job *jo
 			"destinationId": parameters.DestinationID,
 		}).Increment()
 	}
+	abortedJob, abortReason := rt.drainOrRetryLimitReached(job) // if job's aborted, then send it to its worker right away
 	if eventOrderingDisabled {
 		availableWorkers := lo.Filter(workers, func(w *worker, _ int) bool { return w.AvailableSlots() > 0 })
 		if len(availableWorkers) == 0 {
 			return nil, types.ErrWorkerNoSlot
 		}
+		slot := availableWorkers[rand.Intn(len(availableWorkers))].ReserveSlot()
+		if slot == nil {
+			return nil, types.ErrWorkerNoSlot
+		}
+		if abortedJob {
+			return &workerJobSlot{slot: slot, drainReason: abortReason}, nil
+		}
 		if rt.shouldBackoff(job) {
+			slot.Release()
 			return nil, types.ErrJobBackoff
 		}
 		if rt.shouldThrottle(ctx, job, parameters) {
+			slot.Release()
 			return nil, types.ErrDestinationThrottled
 		}
 
-		if slot := availableWorkers[rand.Intn(len(availableWorkers))].ReserveSlot(); slot != nil { // skipcq: GSC-G404
-			return slot, nil
-		}
-		return nil, types.ErrWorkerNoSlot
-
+		return &workerJobSlot{slot: slot}, nil
 	}
 
 	// checking if the orderKey is in blockedOrderKeys. If yes, returning nil.
@@ -564,7 +576,7 @@ func (rt *Handle) findWorkerSlot(ctx context.Context, workers []*worker, job *jo
 	}
 
 	//#JobOrder (see other #JobOrder comment)
-	if rt.shouldBackoff(job) { // backoff
+	if !abortedJob && rt.shouldBackoff(job) { // backoff
 		blockedOrderKeys[orderKey] = struct{}{}
 		return nil, types.ErrJobBackoff
 	}
@@ -586,14 +598,57 @@ func (rt *Handle) findWorkerSlot(ctx context.Context, workers []*worker, job *jo
 		return nil, types.ErrBarrierExists
 	}
 	rt.logger.Debugf("EventOrder: job %d of orderKey %s is allowed to be processed", job.JobID, orderKey)
-	if rt.shouldThrottle(ctx, job, parameters) {
+	if !abortedJob && rt.shouldThrottle(ctx, job, parameters) {
 		blockedOrderKeys[orderKey] = struct{}{}
 		worker.barrier.Leave(orderKey, job.JobID)
 		slot.Release()
 		return nil, types.ErrDestinationThrottled
 	}
-	return slot, nil
+	return &workerJobSlot{slot: slot, drainReason: abortReason}, nil
 	//#EndJobOrder
+}
+
+// checks if job is configured to drain or if it's retry limit is reached
+func (rt *Handle) drainOrRetryLimitReached(job *jobsdb.JobT) (bool, string) {
+	drain, reason := rt.drainer.Drain(job)
+	if drain {
+		return true, reason
+	}
+	retryLimitReached := rt.retryLimitReached(&job.LastJobStatus)
+	if retryLimitReached {
+		return true, "retry limit reached"
+	}
+	return false, ""
+}
+
+func (rt *Handle) retryLimitReached(status *jobsdb.JobStatusT) bool {
+	respStatusCode, _ := strconv.Atoi(status.ErrorCode)
+	switch respStatusCode {
+	case types.RouterTimedOutStatusCode,
+		types.RouterUnMarshalErrorCode: // 5xx errors
+		return false
+	}
+
+	if respStatusCode < http.StatusInternalServerError {
+		return false
+	}
+
+	firstAttemptedAtTime := time.Now()
+	if firstAttemptedAt := gjson.GetBytes(status.ErrorResponse, "firstAttemptedAt").Str; firstAttemptedAt != "" {
+		if t, err := time.Parse(misc.RFC3339Milli, firstAttemptedAt); err == nil {
+			firstAttemptedAtTime = t
+		}
+	}
+
+	maxFailedCountForJob := rt.reloadableConfig.maxFailedCountForJob.Load()
+	retryTimeWindow := rt.reloadableConfig.retryTimeWindow.Load()
+	if gjson.GetBytes(status.JobParameters, "source_job_run_id").Str != "" {
+		maxFailedCountForJob = rt.reloadableConfig.maxFailedCountForSourcesJob.Load()
+		retryTimeWindow = rt.reloadableConfig.sourcesRetryTimeWindow.Load()
+	}
+
+	return time.Since(firstAttemptedAtTime) > retryTimeWindow &&
+		status.AttemptNum >= maxFailedCountForJob // retry time window exceeded
 }
 
 func (*Handle) shouldBackoff(job *jobsdb.JobT) bool {
