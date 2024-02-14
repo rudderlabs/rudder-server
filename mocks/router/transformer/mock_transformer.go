85445ebef2c75d946020bfa8c53bec08bd40fba4
Sai Sankeerth <sanpj2292@github.com>
2024-02-14 17:18:48 +0530

    chore: generate mocks
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- mocks/router/transformer/mock_transformer.go
+++ mocks/router/transformer/mock_transformer.go
@@ -45,21 +45,21 @@ func (m *MockTransformer) ProxyRequest(arg0 context.Context, arg1 *transformer.P
 }
 
 // ProxyRequest indicates an expected call of ProxyRequest.
-func (mr *MockTransformerMockRecorder) ProxyRequest(arg0, arg1 interface{}) *gomock.Call {
+func (mr *MockTransformerMockRecorder) ProxyRequest(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ProxyRequest", reflect.TypeOf((*MockTransformer)(nil).ProxyRequest), arg0, arg1)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ProxyRequest", reflect.TypeOf((*MockTransformer)(nil).ProxyRequest), arg0, arg1, arg2)
 }
 
 // Transform mocks base method.
-func (m *MockTransformer) Transform(arg0 string, arg1 *types.TransformMessageT) []types.DestinationJobT {
+func (m *MockTransformer) Transform(arg0 string, arg1 *types.TransformMessageT, arg2 bool) []types.DestinationJobT {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Transform", arg0, arg1)
+	ret := m.ctrl.Call(m, "Transform", arg0, arg1, arg2)
 	ret0, _ := ret[0].([]types.DestinationJobT)
 	return ret0
 }
 
 // Transform indicates an expected call of Transform.
-func (mr *MockTransformerMockRecorder) Transform(arg0, arg1 interface{}) *gomock.Call {
+func (mr *MockTransformerMockRecorder) Transform(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Transform", reflect.TypeOf((*MockTransformer)(nil).Transform), arg0, arg1)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Transform", reflect.TypeOf((*MockTransformer)(nil).Transform), arg0, arg1, arg2)
 }
