16b5a94590ec281e68ead1036d013d1c67d4d89b
mihir <mihir20121997@gmail.com>
2024-02-26 20:43:43 +0530

    addressed comments

--- cmd/backupfilemigrator/file_migrator.go
+++ cmd/backupfilemigrator/file_migrator.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"compress/gzip"
 	"context"
 	"encoding/json"
 	"fmt"
 	"os"
-	path2 "path"
+	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 
-	kitConfig "github.com/rudderlabs/rudder-go-kit/config"
+	kitconfig "github.com/rudderlabs/rudder-go-kit/config"
+
+	"github.com/rudderlabs/rudder-go-kit/bytesize"
+
 	"github.com/rudderlabs/rudder-server/utils/types"
 
 	jsoniter "github.com/json-iterator/go"
-
-	"github.com/pkg/errors"
 
 	"github.com/samber/lo"
 
@@ -26,15 +29,19 @@ import (
 	"github.com/tidwall/gjson"
 
 	"github.com/rudderlabs/rudder-go-kit/filemanager"
-	"github.com/rudderlabs/rudder-go-kit/logger"
+	kitlogger "github.com/rudderlabs/rudder-go-kit/logger"
 	"github.com/rudderlabs/rudder-server/enterprise/replay/utils"
+)
+
+const (
+	gatewayJobsFilePrefix = "gw_jobs_"
 )
 
 var jsonfast = jsoniter.ConfigCompatibleWithStandardLibrary
 
 type fileMigrator struct {
 	conf        *config
-	logger      logger.Logger
+	logger      kitlogger.Logger
 	fileManager filemanager.FileManager
 }
 
@@ -48,41 +55,47 @@ type config struct {
 // list all the files that needs to be migrated
 func (m *fileMigrator) listFilePathToMigrate(ctx context.Context) []string {
 	listOfFiles := make([]string, 0)
-	iter := filemanager.IterateFilesWithPrefix(ctx,
+	iterator := filemanager.IterateFilesWithPrefix(ctx,
 		m.conf.backupFileNamePrefix,
 		"",
 		100,
 		m.fileManager,
 	)
-	for iter.Next() {
-		object := iter.Get()
+	for iterator.Next() {
+		object := iterator.Get()
 		filePath := object.Key
-		if strings.Contains(filePath, "gw_jobs_") {
-			startTimeMilli := m.conf.startTime.UnixNano() / int64(time.Millisecond)
-			endTimeMilli := m.conf.endTime.UnixNano() / int64(time.Millisecond)
-			key := object.Key
+		if !strings.Contains(filePath, gatewayJobsFilePrefix) {
+			continue
+		}
 
-			// file name should be of format gw_jobs_9710.974705928.974806056.1604871241214.1604872598504.gz
-			tokens := strings.Split(key, "gw_jobs_")
-			tokens = strings.Split(tokens[1], ".")
-			if _, err := strconv.Atoi(tokens[0]); err != nil {
-				continue
-			}
+		startTimeMilli := m.conf.startTime.UnixNano() / int64(time.Millisecond)
+		endTimeMilli := m.conf.endTime.UnixNano() / int64(time.Millisecond)
 
-			// gw dump file name format gw_jobs_<table_index>.<start_job_id>.<end_job_id>.<min_created_at>_<max_created_at>.gz
-			// ex: gw_jobs_9710.974705928.974806056.1604871241214.1604872598504.gz
-			minJobCreatedAt, maxJobCreatedAt, err := utils.GetMinMaxCreatedAt(object.Key)
-			var pass bool
-			if err == nil {
-				pass = maxJobCreatedAt >= startTimeMilli && minJobCreatedAt <= endTimeMilli
-			} else {
-				m.logger.Infof("gw dump name(%s) is not of the expected format. Parse failed with error %w", object.Key, err)
-				m.logger.Info("Falling back to comparing start and end time stamps with gw dump last modified.")
-				pass = object.LastModified.After(m.conf.startTime) && object.LastModified.Before(m.conf.endTime)
-			}
-			if pass {
-				listOfFiles = append(listOfFiles, object.Key)
-			}
+		// file name should be of format gw_jobs_9710.974705928.974806056.1604871241214.1604872598504.gz
+		tokens := strings.Split(filePath, gatewayJobsFilePrefix)
+		if len(tokens) < 2 {
+			m.logger.Warnn("invalid file name format", kitlogger.NewStringField("filePath", filePath))
+			continue
+		}
+		tokens = strings.Split(tokens[1], ".")
+		if _, err := strconv.Atoi(tokens[0]); err != nil {
+			m.logger.Warnn("invalid table name in filename", kitlogger.NewStringField("filePath", filePath))
+			continue
+		}
+
+		// gw dump file name format gw_jobs_<table_index>.<start_job_id>.<end_job_id>.<min_created_at>_<max_created_at>.gz
+		// ex: gw_jobs_9710.974705928.974806056.1604871241214.1604872598504.gz
+		minJobCreatedAt, maxJobCreatedAt, err := utils.GetMinMaxCreatedAt(object.Key)
+		var pass bool
+		if err == nil {
+			pass = maxJobCreatedAt >= startTimeMilli && minJobCreatedAt <= endTimeMilli
+		} else {
+			m.logger.Infof("gw dump name(%s) is not of the expected format. Parse failed with error %w", object.Key, err)
+			m.logger.Info("Falling back to comparing start and end time stamps with gw dump last modified.")
+			pass = object.LastModified.After(m.conf.startTime) && object.LastModified.Before(m.conf.endTime)
+		}
+		if pass {
+			listOfFiles = append(listOfFiles, object.Key)
 		}
 	}
 	return listOfFiles
@@ -93,17 +106,17 @@ func (m *fileMigrator) convertToNewFormat(lineBytes []byte, createdAt time.Time)
 	var gatewayBatchReq []types.SingularEventT
 	err := jsonfast.Unmarshal([]byte(gjson.GetBytes(lineBytes, "event_payload.batch").String()), &gatewayBatchReq)
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to unmarshall gateway events")
+		return nil, fmt.Errorf("unable to unmarshall gateway events: %w", err)
 	}
-	listOfNewEvents := make([]*newFileFormat, 0)
-	userId := gjson.GetBytes(lineBytes, "user_id").String()
+	listOfNewEvents := make([]*newFileFormat, 0, len(gatewayBatchReq))
+	userID := gjson.GetBytes(lineBytes, "user_id").String()
 	for _, singleEvent := range gatewayBatchReq {
 		payloadBytes, err := json.Marshal(singleEvent)
 		if err != nil {
-			return nil, errors.Wrap(err, "unable to marshall single event")
+			return nil, fmt.Errorf("unable to marshall single event: %w", err)
 		}
 		j := &newFileFormat{}
-		j.UserID = userId
+		j.UserID = userID
 		j.EventPayload = payloadBytes
 		j.CreatedAt = createdAt
 		j.MessageID = misc.GetStringifiedData(singleEvent["messageId"])
@@ -118,7 +131,7 @@ func (m *fileMigrator) uploadFile(ctx context.Context, jobs []*newFileFormat, so
 	lastJobCreatedAt := jobs[len(jobs)-1].CreatedAt.UTC()
 
 	gzWriter := fileuploader.NewGzMultiFileWriter()
-	localFilePath := path2.Join(
+	localFilePath := path.Join(
 		lo.Must(misc.CreateTMPDIR()),
 		"rudder-backups",
 		sourceId,
@@ -129,21 +142,21 @@ func (m *fileMigrator) uploadFile(ctx context.Context, jobs []*newFileFormat, so
 		jobBytes, err := json.Marshal(job)
 		if err != nil {
 			_ = gzWriter.Close()
-			return errors.Wrap(err, "failed to marshal job")
+			return fmt.Errorf("failed to marshal job: %w", err)
 		}
 		if _, err := gzWriter.Write(localFilePath, append(jobBytes, '\n')); err != nil {
 			_ = gzWriter.Close()
-			return errors.Wrap(err, "write to local file failed")
+			return fmt.Errorf("write to local file failed: %w", err)
 		}
 	}
 	if err := gzWriter.Close(); err != nil {
-		return errors.Wrap(err, "failed to close writer")
+		return fmt.Errorf("failed to close writer: %w", err)
 	}
 	defer func() { _ = os.Remove(localFilePath) }()
 
 	localFile, err := os.Open(localFilePath)
 	if err != nil {
-		return errors.Wrap(err, "failed to open local file")
+		return fmt.Errorf("failed to open local file: %w", err)
 	}
 	defer func() { _ = localFile.Close() }()
 	prefixes := []string{
@@ -155,7 +168,7 @@ func (m *fileMigrator) uploadFile(ctx context.Context, jobs []*newFileFormat, so
 	}
 	_, err = m.fileManager.Upload(ctx, localFile, prefixes...)
 	if err != nil {
-		return errors.Wrap(err, "failed to upload file")
+		return fmt.Errorf("failed to upload file: %w", err)
 	}
 	return nil
 }
@@ -163,35 +176,108 @@ func (m *fileMigrator) uploadFile(ctx context.Context, jobs []*newFileFormat, so
 // download file locally
 func (m *fileMigrator) downloadFile(ctx context.Context, filePath string) (string, error) {
 	filePathTokens := strings.Split(filePath, "/")
-	// backup file is present in folder named instance id
 	// e.g. rudder-saas/dummy/dummy-v0-rudderstack-10/gw_jobs_11796.317963152.317994396.1703547948443.1703548519552.dummy-workspace-id.gz
 	var err error
 	dumpDownloadPathDirName := "/rudder-s3-dumps/"
-	tmpdirPath := strings.TrimSuffix(kitConfig.GetString("RUDDER_TMPDIR", "/tmp"), "/")
-	if tmpdirPath == "" {
-		tmpdirPath, err = os.UserHomeDir()
-		if err != nil {
-			return "", errors.Wrap(err, "failed to get user home directory")
-		}
-	}
-	path := fmt.Sprintf(
-		`%v%v%v`, tmpdirPath, dumpDownloadPathDirName, filePathTokens[len(filePathTokens)-1])
-
-	err = os.MkdirAll(filepath.Dir(path), os.ModePerm)
+	tmpdirPath, err := misc.CreateTMPDIR()
 	if err != nil {
-		return "", errors.Wrap(err, "failed to make local directory")
+		return "", fmt.Errorf("failed to create tmp directory: %w", err)
+	}
+	tempPath := path.Join(tmpdirPath, dumpDownloadPathDirName, filePathTokens[len(filePathTokens)-1])
+
+	err = os.MkdirAll(filepath.Dir(tempPath), os.ModePerm)
+	if err != nil {
+		return "", fmt.Errorf("failed to make local directory: %w", err)
 	}
 
-	file, err := os.Create(path)
+	file, err := os.Create(tempPath)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to make local file")
+		return "", fmt.Errorf("failed to make local file: %w", err)
 	}
 
 	err = m.fileManager.Download(ctx, file, filePath)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to download file")
+		return "", fmt.Errorf("failed to download file: %w", err)
 	}
-	m.logger.Debugf("file downloaded at %s", path)
+	m.logger.Debugf("file downloaded at %s", tempPath)
 	defer func() { _ = file.Close() }()
-	return path, nil
+	return tempPath, nil
+}
+
+func (m *fileMigrator) processFile(ctx context.Context, filePath string) error {
+	filePathTokens := strings.Split(filePath, "/")
+	// file path should be in format like {prefix}/{instance_id}/gw_jobs_<table_index>.<start_job_id>.<end_job_id>.<min_created_at>_<max_created_at>.gz
+	// e.g. dummy/dummy-v0-rudderstack-10/gw_jobs_11796.317963152.317994396.1703547948443.1703548519552.workspace.gz
+	if len(filePathTokens) < 3 {
+		return fmt.Errorf("file path is in invalid format")
+	}
+	instanceID := filePathTokens[len(filePathTokens)-2]
+	workspaceID := strings.Split(filePathTokens[len(filePathTokens)-1], ".")[len(strings.Split(filePathTokens[len(filePathTokens)-1], "."))-2]
+
+	localFilePath, err := m.downloadFile(ctx, filePath)
+	if err != nil {
+		return fmt.Errorf("failed to download file locally, err: %w", err)
+	}
+	defer func() { _ = os.Remove(localFilePath) }()
+
+	rawFile, err := os.Open(localFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file, err: %w", err)
+	}
+
+	reader, err := gzip.NewReader(rawFile)
+	if err != nil {
+		return fmt.Errorf("failed to get new gzip reader, err: %w", err)
+	}
+
+	sc := bufio.NewScanner(reader)
+	// default scanner buffer maxCapacity is 64K
+	// set it to higher value to avoid read stop on read size error
+	maxCapacity := kitconfig.GetInt64("FileMigrator.maxScannerCapacity", 10*bytesize.MB) // 10MB
+	buf := make([]byte, maxCapacity)
+	sc.Buffer(buf, int(maxCapacity))
+
+	defer func() { _ = rawFile.Close() }()
+
+	eventsToDump := make(map[string][]*newFileFormat)
+	for sc.Scan() {
+		lineBytes := sc.Bytes()
+		copyLineBytes := make([]byte, len(lineBytes))
+		copy(copyLineBytes, lineBytes)
+		timeStamp := gjson.GetBytes(copyLineBytes, "created_at").String()
+		createdAt, err := time.Parse(time.RFC3339Nano, timeStamp)
+		if err != nil {
+			return fmt.Errorf("failed to parse created_at, err: %w", err)
+		}
+		if !(m.conf.startTime.Before(createdAt) && m.conf.endTime.After(createdAt)) {
+			continue
+		}
+
+		// convert to new format
+		newFormatFileEntries, err := m.convertToNewFormat(copyLineBytes, createdAt)
+		if err != nil {
+			return fmt.Errorf("failed to convert to new file format, err: %w", err)
+		}
+		sourceID := gjson.GetBytes(copyLineBytes, "parameters.source_id").String()
+		if eventsToDump[sourceID] == nil {
+			eventsToDump[sourceID] = make([]*newFileFormat, 0)
+		}
+		// prepare batch dump to a new file
+		eventsToDump[sourceID] = append(eventsToDump[sourceID], newFormatFileEntries...)
+		// save to new file
+		if len(eventsToDump[sourceID]) >= m.conf.uploadBatchSize {
+			err := m.uploadFile(ctx, eventsToDump[sourceID], sourceID, workspaceID, instanceID)
+			if err != nil {
+				return fmt.Errorf("failed to upload file, sourceID:%s, err: %w", sourceID, err)
+			}
+			delete(eventsToDump, sourceID)
+		}
+	}
+	for sourceID, jobs := range eventsToDump {
+		err := m.uploadFile(ctx, jobs, sourceID, workspaceID, instanceID)
+		if err != nil {
+			return fmt.Errorf("failed to upload file, sourceID:%s, err: %w", sourceID, err)
+		}
+	}
+	return nil
 }
--- cmd/backupfilemigrator/main.go
+++ cmd/backupfilemigrator/main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"bufio"
-	"compress/gzip"
 	"context"
 	"encoding/json"
-	"flag"
+	"fmt"
 	"os"
+	"os/signal"
+	"sort"
 	"strings"
+	"syscall"
 	"time"
-
-	"github.com/tidwall/gjson"
 
 	obskit "github.com/rudderlabs/rudder-observability-kit/go/labels"
 
-	kitConfig "github.com/rudderlabs/rudder-go-kit/config"
+	"github.com/urfave/cli/v2"
+
+	kitconfig "github.com/rudderlabs/rudder-go-kit/config"
 	"github.com/rudderlabs/rudder-go-kit/filemanager"
-	"github.com/rudderlabs/rudder-go-kit/logger"
+	kitlogger "github.com/rudderlabs/rudder-go-kit/logger"
 	"github.com/rudderlabs/rudder-server/utils/filemanagerutil"
 )
 
@@ -27,38 +28,87 @@ type newFileFormat struct {
 	MessageID    string          `json:"messageId"`
 }
 
-// step 1: fetch files to migrate
+var app = &cli.App{
+	Name:  "migration-tool",
+	Usage: "Backup file migration tool",
+	Commands: []*cli.Command{
+		{
+			Name:  "migrate",
+			Usage: "Backup file migration",
+			Flags: []cli.Flag{
+				&cli.TimestampFlag{
+					Name:     "startTime",
+					Layout:   time.RFC3339Nano,
+					Usage:    "start time from which we need to convert files in RFC3339 format",
+					Required: true,
+				},
+				&cli.TimestampFlag{
+					Name:     "endTime",
+					Layout:   time.RFC3339Nano,
+					Usage:    "end time upto which we need to convert files in RFC3339 format",
+					Required: true,
+				},
+				&cli.IntFlag{
+					Name:     "uploadBatchSize",
+					Usage:    "no. of messages to upload in single file",
+					Required: true,
+				},
+				&cli.StringFlag{
+					Name:     "backupFileNamePrefix",
+					Usage:    "prefix to identify files in the bucket",
+					Required: true,
+				},
+			},
+			Action: func(c *cli.Context) error {
+				return migrate(c)
+			},
+		},
+	},
+}
+
+func init() {
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+}
+
 func main() {
-	ctx := context.Background()
-	log := logger.NewLogger().Child("backup-file-migrator")
-	conf := kitConfig.Default
-
-	var startTimeStr string
-	var endTimeStr string
-	var uploadBatchSize int
-	var backupFileNamePrefix string
-	flag.StringVar(&startTimeStr, "startTime", "", "start time from which we need to convert files in RFC3339 format")
-	flag.StringVar(&endTimeStr, "endTime", "", "end time upto which we need to convert files in RFC3339 format")
-	flag.IntVar(&uploadBatchSize, "uploadBatchSize", 1000, "no. of messages to upload in single file")
-	flag.StringVar(&backupFileNamePrefix, "backupFileNamePrefix", "", "prefix to identify files in the bucket")
-	flag.Parse()
-
-	startTime, err := time.Parse(time.RFC3339Nano, strings.TrimSpace(startTimeStr))
-	if err != nil {
-		log.Errorn("unable to parse start time", logger.NewStringField("startTime", startTimeStr), obskit.Error(err))
-		return
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	endTime, err := time.Parse(time.RFC3339Nano, strings.TrimSpace(endTimeStr))
-	if err != nil {
-		log.Errorn("unable to parse end time", logger.NewStringField("endTime", endTimeStr), obskit.Error(err))
-		return
-	}
+}
+
+func migrate(c *cli.Context) error {
+	ctx := c.Context
+	startTime := c.Timestamp("startTime")
+	endTime := c.Timestamp("endTime")
+	uploadBatchSize := c.Int("uploadBatchSize")
+	backupFileNamePrefix := c.String("backupFileNamePrefix")
+	logger := kitlogger.NewLogger().Child("backup-file-migrator")
+	conf := kitconfig.Default
+
+	return run(ctx, conf, logger, *startTime, *endTime, uploadBatchSize, backupFileNamePrefix)
+}
+
+func run(
+	ctx context.Context,
+	conf *kitconfig.Config,
+	logger kitlogger.Logger,
+	startTime time.Time,
+	endTime time.Time,
+	uploadBatchSize int,
+	backupFileNamePrefix string,
+) error {
+	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
 	backupFileNamePrefix = strings.TrimSpace(backupFileNamePrefix)
 	if backupFileNamePrefix == "" {
-		log.Errorn("backupFileNamePrefix should not be empty")
+		logger.Errorn("backupFileNamePrefix should not be empty")
+		return fmt.Errorf("backupFileNamePrefix should not be empty")
 	}
 
-	storageProvider := kitConfig.GetString("JOBS_BACKUP_STORAGE_PROVIDER", "S3")
+	storageProvider := kitconfig.GetString("JOBS_BACKUP_STORAGE_PROVIDER", "S3")
 	fileManagerOpts := filemanagerutil.ProviderConfigOpts(ctx, storageProvider, conf)
 	fileManagerOpts.Prefix = backupFileNamePrefix
 	fileManager, err := filemanager.New(&filemanager.Settings{
@@ -67,8 +117,7 @@ func main() {
 		Conf:     conf,
 	})
 	if err != nil {
-		log.Errorf("error creating file manager: %s", err.Error())
-		return
+		return fmt.Errorf("creating file manager, err: %w", err)
 	}
 
 	migratorConfig := &config{
@@ -80,7 +129,7 @@ func main() {
 
 	migrator := &fileMigrator{
 		conf:        migratorConfig,
-		logger:      log,
+		logger:      logger,
 		fileManager: fileManager,
 	}
 	// list all files to migrate
@@ -88,87 +137,11 @@ func main() {
 
 	// step 2: download data from file
 	for _, filePath := range listOfFiles {
-		filePathTokens := strings.Split(filePath, "/")
-		// file path should be in format like {prefix}/{instance_id}/gw_jobs_<table_index>.<start_job_id>.<end_job_id>.<min_created_at>_<max_created_at>.gz
-		// e.g. dummy/dummy-v0-rudderstack-10/gw_jobs_11796.317963152.317994396.1703547948443.1703548519552.workspace.gz
-		if len(filePathTokens) < 3 {
-			log.Errorn("file path is in invalid format", logger.NewStringField("filePath", filePath))
-			continue
-		}
-		instanceID := filePathTokens[len(filePathTokens)-2]
-		workspaceID := strings.Split(filePathTokens[len(filePathTokens)-1], ".")[len(strings.Split(filePathTokens[len(filePathTokens)-1], "."))-2]
-
-		localFilePath, err := migrator.downloadFile(ctx, filePath)
+		err := migrator.processFile(ctx, filePath)
 		if err != nil {
-			log.Errorn("error downloading file locally", logger.NewStringField("filePath", filePath), obskit.Error(err))
-			continue
-		}
-		defer func() { _ = os.Remove(localFilePath) }()
-
-		rawf, err := os.Open(localFilePath)
-		if err != nil {
-			log.Errorn("failed to open file", obskit.Error(err))
-			continue // failed to open file
-		}
-
-		reader, err := gzip.NewReader(rawf)
-		if err != nil {
-			log.Errorn("failed to get new gzip reader", obskit.Error(err))
-			continue // failed to read gzip file
-		}
-
-		sc := bufio.NewScanner(reader)
-		// default scanner buffer maxCapacity is 64K
-		// set it to higher value to avoid read stop on read size error
-		maxCapacity := 10240 * 1024 // 10MB
-		buf := make([]byte, maxCapacity)
-		sc.Buffer(buf, maxCapacity)
-
-		defer func() { _ = rawf.Close() }()
-
-		eventsToDump := make(map[string][]*newFileFormat)
-		for sc.Scan() {
-			lineBytes := sc.Bytes()
-			copyLineBytes := make([]byte, len(lineBytes))
-			copy(copyLineBytes, lineBytes)
-			timeStamp := gjson.GetBytes(copyLineBytes, "created_at").String()
-			createdAt, err := time.Parse(time.RFC3339Nano, timeStamp)
-			if err != nil {
-				log.Errorf("failed to parse created at: %s", err)
-				continue
-			}
-			if !(startTime.Before(createdAt) && endTime.After(createdAt)) {
-				continue
-			}
-
-			// step 3: convert to new format
-			newFormatFileEntries, err := migrator.convertToNewFormat(copyLineBytes, createdAt)
-			if err != nil {
-				log.Errorn("failed to get new file entries", obskit.Error(err))
-				return
-			}
-			sourceID := gjson.GetBytes(copyLineBytes, "parameters.source_id").String()
-			if eventsToDump[sourceID] == nil {
-				eventsToDump[sourceID] = make([]*newFileFormat, 0)
-			}
-			// step 4: prepare batch dump to a new file
-			eventsToDump[sourceID] = append(eventsToDump[sourceID], newFormatFileEntries...)
-			// step 5: save to new file
-			if len(eventsToDump[sourceID]) == uploadBatchSize {
-				err := migrator.uploadFile(ctx, eventsToDump[sourceID], sourceID, workspaceID, instanceID)
-				if err != nil {
-					log.Errorn("failed to upload files", obskit.Error(err))
-					return
-				}
-				delete(eventsToDump, sourceID)
-			}
-		}
-		for sourceID, jobs := range eventsToDump {
-			err := migrator.uploadFile(ctx, jobs, sourceID, workspaceID, instanceID)
-			if err != nil {
-				log.Errorn("failed to upload files", obskit.Error(err))
-				return
-			}
+			logger.Errorn("fail to process file", obskit.Error(err), kitlogger.NewStringField("filePath", filePath))
+			return err
 		}
 	}
+	return nil
 }
