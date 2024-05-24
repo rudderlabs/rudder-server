99d085d92fc0c61f10c50299c52c209430899c6e
Leonidas Vrachnis <leo.al.vra@gmail.com>
2024-05-24 12:32:08 +0200

    chore(tooling): add go vuln and tidy tool packages (#4705)

--- scripts/install-golangci-lint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-VERSION=$1
-[ -z "${VERSION}" ] && VERSION="v1.58.2"
-GOPATH=$(go env GOPATH)
-[ -f "${GOPATH}/bin/golangci-lint-${VERSION}" ] && echo "golangci-lint ${VERSION} is already installed" || \
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/${VERSION}/install.sh | sh -s -- -b "${GOPATH}"/bin ${VERSION} && \
-cp "${GOPATH}"/bin/golangci-lint "${GOPATH}"/bin/golangci-lint-${VERSION}
