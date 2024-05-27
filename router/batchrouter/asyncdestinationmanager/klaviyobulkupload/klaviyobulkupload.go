0b6801ea48d6ed5594b6a5a47f9bd81bdd65d6f6
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2024-05-28 03:32:45 +0530

    chore: refactor, move type declarations

--- router/batchrouter/asyncdestinationmanager/klaviyobulkupload/klaviyobulkupload.go
+++ router/batchrouter/asyncdestinationmanager/klaviyobulkupload/klaviyobulkupload.go
@@ -23,108 +23,6 @@ import (
 
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
-type Uploader interface {
-	Upload(*common.AsyncDestinationStruct) common.AsyncUploadOutput
-}
-
-type HttpClient interface {
-	Do(req *http.Request) (*http.Response, error)
-}
-
-type Poller interface {
-	Poll(input common.AsyncPoll) common.PollStatusResponse
-}
-
-type UploadStats interface {
-	GetUploadStats(common.GetUploadStatsInput) common.GetUploadStatsResponse
-}
-
-type KlaviyoBulkUploader struct {
-	destName             string
-	destinationConfig    map[string]interface{}
-	logger               logger.Logger
-	Client               *http.Client
-	jobIdToIdentifierMap map[string]int64
-}
-
-type UploadResp struct {
-	Data struct {
-		Id string `json:"id"`
-	} `json:"data"`
-}
-
-type PollResp struct {
-	Data struct {
-		Id         string `json:"id"`
-		Attributes struct {
-			Total_count     int    `json:"total_count"`
-			Completed_count int    `json:"completed_count"`
-			Failed_count    int    `json:"failed_count"`
-			Status          string `json:"status"`
-		} `json:"attributes"`
-	} `json:"data"`
-}
-
-type UploadStatusResp struct {
-	Data []struct {
-		Type       string `json:"type"`
-		ID         string `json:"id"`
-		Attributes struct {
-			Code   string `json:"code"`
-			Title  string `json:"title"`
-			Detail string `json:"detail"`
-			Source struct {
-				Pointer string `json:"pointer"`
-			} `json:"source"`
-			OriginalPayload struct {
-				Id          string `json:"id"`
-				AnonymousId string `json:"anonymous_id"`
-			} `json:"original_payload"`
-		} `json:"attributes"`
-		Links struct {
-			Self string `json:"self"`
-		} `json:"links"`
-	} `json:"data"`
-	Links struct {
-		Self  string `json:"self"`
-		First string `json:"first"`
-		Last  string `json:"last"`
-		Prev  string `json:"prev"`
-		Next  string `json:"next"`
-	} `json:"links"`
-}
-
-type Payload struct {
-	Data Data `json:"data"`
-}
-
-type Data struct {
-	Type          string         `json:"type"`
-	Attributes    Attributes     `json:"attributes"`
-	Relationships *Relationships `json:"relationships,omitempty"`
-}
-
-type Attributes struct {
-	Profiles Profiles `json:"profiles"`
-}
-
-type Profiles struct {
-	Data []map[string]interface{} `json:"data"`
-}
-
-type Relationships struct {
-	Lists Lists `json:"lists"`
-}
-
-type Lists struct {
-	Data []List `json:"data"`
-}
-
-type List struct {
-	Type string `json:"type"`
-	ID   string `json:"id"`
-}
-
 func createFinalPayload(combinedProfiles []map[string]interface{}, listId string) Payload {
 	payload := Payload{
 		Data: Data{
@@ -450,10 +348,6 @@ func (kbu *KlaviyoBulkUploader) Upload(asyncDestStruct *common.AsyncDestinationS
 	outputJSON, err := json.MarshalIndent(combinedPayload, "", "  ")
 	if err != nil {
 		return kbu.generateKlaviyoErrorOutput("Error while marshaling combined JSON.", err, importingJobIDs, destinationID)
-	}
-	outputFilePath := "combined_payload.json"
-	if err := os.WriteFile(outputFilePath, outputJSON, 0o644); err != nil {
-		return kbu.generateKlaviyoErrorOutput("Error while writing JSON to file.", err, importingJobIDs, destinationID)
 	}
 	uploadURL := "https://a.klaviyo.com/api/profile-bulk-import-jobs/"
 	client := &http.Client{}
