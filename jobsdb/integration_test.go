6c03965a38cdd5461e0b593f23ffac1707c7d8ab
Leonidas Vrachnis <leo.al.vra@gmail.com>
2024-06-06 11:10:15 +0200

    add more tests

--- jobsdb/integration_test.go
+++ jobsdb/integration_test.go
@@ -3,6 +3,7 @@ package jobsdb
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 	"sync"
@@ -1032,29 +1033,40 @@ func TestJobsDB_SanitizeJSON(t *testing.T) {
 	toValidUTF8Tests := []struct {
 		in  string
 		out string
+		err error
 	}{
-		{`\u0000`, ""},
-		{`\u0000☺\u0000b☺`, "☺b☺"},
+		{`\u0000`, "", nil},
+		{`\u0000☺\u0000b☺`, "☺b☺", nil},
 		// NOTE: we are not handling the following:
 		// {"\u0000", ""},
 		// {"\u0000☺\u0000b☺", "☺b☺"},
 
-		{"", ""},
-		{"abc", "abc"},
-		{"\uFDDD", "\uFDDD"},
-		{"a\xffb", "a" + ch(1) + "b"},
-		{"a\xffb\uFFFD", "a" + ch(1) + "b\uFFFD"},
-		{"a☺\xffb☺\xC0\xAFc☺\xff", "a☺" + ch(1) + "b☺" + ch(2) + "c☺" + ch(1)},
-		{"\xC0\xAF", ch(2)},
-		{"\xE0\x80\xAF", ch(3)},
-		{"\xed\xa0\x80", ch(3)},
-		{"\xed\xbf\xbf", ch(3)},
-		{"\xF0\x80\x80\xaf", ch(4)},
-		{"\xF8\x80\x80\x80\xAF", ch(5)},
-		{"\xFC\x80\x80\x80\x80\xAF", ch(6)},
+		{"", "", nil},
+		{"abc", "abc", nil},
+		{"\uFDDD", "\uFDDD", nil},
+		{"a\xffb", "a" + ch(1) + "b", nil},
+		{"a\xffb\uFFFD", "a" + ch(1) + "b\uFFFD", nil},
+		{"a☺\xffb☺\xC0\xAFc☺\xff", "a☺" + ch(1) + "b☺" + ch(2) + "c☺" + ch(1), nil},
+		{"\xC0\xAF", ch(2), nil},
+		{"\xE0\x80\xAF", ch(3), nil},
+		{"\xed\xa0\x80", ch(3), nil},
+		{"\xed\xbf\xbf", ch(3), nil},
+		{"\xF0\x80\x80\xaf", ch(4), nil},
+		{"\xF8\x80\x80\x80\xAF", ch(5), nil},
+		{"\xFC\x80\x80\x80\x80\xAF", ch(6), nil},
 
 		// {"\ud800", ""},
-		{`\ud800`, ch(1)},
+		{`\ud800`, ch(1), nil},
+		{`\uDEAD`, ch(1), nil},
+
+		{`\uD83D\ub000`, string([]byte{239, 191, 189, 235, 128, 128}), nil},
+		{`\uD83D\ude04`, "😄", nil},
+
+		{`\u4e2d\u6587`, "中文", nil},
+		{`\ud83d\udc4a`, "\xf0\x9f\x91\x8a", nil},
+
+		{`\U0001f64f`, ch(1), errors.New(`readEscapedChar: invalid escape char after`)},
+		{`\uD83D\u00`, ch(1), errors.New(`readU4: expects 0~9 or a~f, but found`)},
 	}
 
 	err := jobDB.Setup(ReadWrite, false, strings.ToLower(rand.String(5)))
@@ -1077,6 +1089,12 @@ func TestJobsDB_SanitizeJSON(t *testing.T) {
 		}}
 
 		err := jobDB.Store(context.Background(), jobs)
+		if tt.err != nil {
+			require.Error(t, err, "should error")
+			require.Contains(t, err.Error(), tt.err.Error(), "should contain error")
+			continue
+		}
+
 		require.NoError(t, err)
 
 		unprocessedJob, err := jobDB.GetUnprocessed(context.Background(), GetQueryParams{
