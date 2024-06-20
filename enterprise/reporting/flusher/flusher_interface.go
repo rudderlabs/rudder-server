1c4e546e5f13938a8ed811194f944815a7b942a3
Satish Kumar <satish@rudderstack.com>
2024-06-20 14:42:17 +0530

    comments

--- enterprise/reporting/flusher/flusher_interface.go
+++ enterprise/reporting/flusher/flusher_interface.go
@@ -20,17 +20,21 @@ type Flusher interface {
 
 	mainLoopOnce(ctx context.Context) error
 
+	// Step 1. Get the time range to flush
 	getTimeRangeToFlush(ctx context.Context) (minReportedAt, maxReportedAt time.Time)
 
 	calculateMaxReportedAt(minReportedAt time.Time) time.Time
 
+	// Step 2. Get the reports in batches and aggregate them in the app
 	getReportsInBatchesAndAggregateInApp(ctx context.Context, minReportedAt, maxReportedAt time.Time) map[string]interface{}
 
 	addReportToAggregate(aggregatedReports map[string]interface{}, report map[string]interface{}) error
 
 	getLabelKeyForAggregation(report map[string]interface{}) string
 
+	// Step 3. Flush the aggregated reports to the reporting service
 	flushAggregatedReports(ctx context.Context, aggregatedReports map[string]interface{}) error
 
+	// Step 4. Delete the flushed reports from the database
 	deleteReports(ctx context.Context, minReportedAt, maxReportedAt time.Time) error
 }
