7d8e5894e5707b57f266f60cdf65ea60fc18b9ca
Sai Sankeerth <sanpj2292@github.com>
2024-06-20 18:06:57 +0530

    chore: lint error fix - 1

--- gateway/response/response.go
+++ gateway/response/response.go
@@ -47,7 +47,7 @@ const (
 	// NonIdentifiableRequest - Request neither has anonymousId nor userId
 	NonIdentifiableRequest = "Request neither has anonymousId nor userId"
 	// ErrorInMarshal - Error while marshalling
-	ErrorInMarshal = "Error while marshalling"
+	ErrorInMarshal              = "Error while marshalling"
 	ErrorInMarshallingReqHeader = "error while marshalling request headers"
 	// ErrorInParseForm - Error during parsing form
 	ErrorInParseForm = "Error during parsing form"
