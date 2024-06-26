999a44357835b85ba208e1e243bbb5a8b278d798
mihir <mihir20121997@gmail.com>
2024-06-26 18:57:14 +0530

    updated implementation as per review comments

--- enterprise/trackedusers/factory.go
+++ enterprise/trackedusers/factory.go
@@ -1,11 +1,17 @@
 package trackedusers
 
-import "github.com/rudderlabs/rudder-go-kit/logger"
+import (
+	"github.com/rudderlabs/rudder-go-kit/config"
+	"github.com/rudderlabs/rudder-go-kit/logger"
+)
 
 type Factory struct {
 	Log logger.Logger
 }
 
-func (f *Factory) Setup(dbConn string) (DataCollector, error) {
-	return NewUniqueUsersCollector(f.Log, dbConn)
+func (f *Factory) Setup(conf *config.Config) (UsersReporter, error) {
+	if !conf.GetBool("TrackedUsers.enabled", false) {
+		return NewNoopDataCollector(), nil
+	}
+	return NewUniqueUsersReporter(f.Log, conf)
 }
