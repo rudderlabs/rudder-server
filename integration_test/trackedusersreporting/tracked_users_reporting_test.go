4777e682ed94c6df18db2ab2736ef447eb31b4db
Mihir Gandhi <mihir20121997@gmail.com>
2024-08-14 10:52:56 +0530

    chore: store anonymous ids in userID hll to optimise storage (#4988)

--- integration_test/trackedusersreporting/tracked_users_reporting_test.go
+++ integration_test/trackedusersreporting/tracked_users_reporting_test.go
@@ -234,6 +234,9 @@ func TestTrackedUsersReporting(t *testing.T) {
 		{anonymousID: "anon-2"},
 	}, "identify", writeKey, url)
 	require.NoError(t, err)
+	err = sendAliasEvent("user-1", "user-2", writeKey, url)
+	require.NoError(t, err)
+	eventsCount++
 
 	require.Eventuallyf(t, func() bool {
 		return tc.webhook.RequestsCount() == eventsCount
@@ -244,9 +247,9 @@ func TestTrackedUsersReporting(t *testing.T) {
 	}, 1*time.Minute, 5*time.Second, "data not reported to reporting service")
 
 	cardinalityMap := tc.reportingServer.getCardinalityFromReportingServer()
-	require.Equal(t, 2, cardinalityMap[workspaceID][sourceID].userIDCount)
-	require.Equal(t, 2, cardinalityMap[workspaceID][sourceID].anonIDCount)
-	require.Equal(t, 2, cardinalityMap[workspaceID][sourceID].identifiedUsersCount)
+	require.Equal(t, 4, cardinalityMap[workspaceID][sourceID].userIDCount)
+	require.Equal(t, 0, cardinalityMap[workspaceID][sourceID].anonIDCount)
+	require.Equal(t, 3, cardinalityMap[workspaceID][sourceID].identifiedUsersCount)
 	cancel()
 	require.NoError(t, wg.Wait())
 }
@@ -369,6 +372,53 @@ func runRudderServer(
 		err = fmt.Errorf("rudder-server exited with a non-0 exit code: %d", c)
 	}
 	return
+}
+
+// nolint: bodyclose
+func sendAliasEvent(userID, previousID, writeKey,
+	url string,
+) error {
+	payload := []byte(fmt.Sprintf(`
+			{
+			  "batch": [
+				{
+				  "userId": %[1]q,
+				  "type": "alias",
+				  "previousId": %[2]q,
+				  "anonymousId": %[1]q,
+				  "context": {
+					"traits": {
+					  "trait1": "new-val"
+					},
+					"ip": "14.5.67.21",
+					"library": {
+					  "name": "http"
+					}
+				  },
+				  "timestamp": "2020-02-02T00:23:09.544Z"
+				}
+			  ]
+			}`,
+		userID,
+		previousID,
+	))
+	req, err := http.NewRequest(http.MethodPost, url+"/v1/batch", bytes.NewReader(payload))
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth(writeKey, "password")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		b, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed to send event to rudder server, status code: %d: %s", resp.StatusCode, string(b))
+	}
+	kithttputil.CloseResponse(resp)
+	return nil
 }
 
 // nolint: bodyclose
