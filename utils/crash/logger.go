92d01b7dd27e3f7d44b9edbd092b0511754a03a4
Leonidas Vrachnis <leo.al.vra@gmail.com>
2024-08-22 14:07:42 +0200

    pr fixes

--- utils/crash/logger.go
+++ utils/crash/logger.go
@@ -9,7 +9,7 @@ import (
 	"github.com/rudderlabs/rudder-go-kit/logger"
 )
 
-type PanicLogger struct {
+type panicLogger struct {
 	notifyOnce sync.Once
 	logger     logger.Logger
 
@@ -17,8 +17,8 @@ type PanicLogger struct {
 }
 
 // UsingLogger uses the provided logger to log panics.
-func UsingLogger(logger logger.Logger, opts PanicWrapperOpts) *PanicLogger {
-	return &PanicLogger{
+func UsingLogger(logger logger.Logger, opts PanicWrapperOpts) *panicLogger {
+	return &panicLogger{
 		logger: logger.Child("panic"),
 		opts:   opts,
 	}
@@ -26,7 +26,7 @@ func UsingLogger(logger logger.Logger, opts PanicWrapperOpts) *PanicLogger {
 
 // Notify returns a function that should be deferred to capture panics.
 // It will do a Fatal log with the stack trace and panic value.
-func (p *PanicLogger) Notify(team string) func() {
+func (p *panicLogger) Notify(team string) func() {
 	return func() {
 		if r := recover(); r != nil {
 			p.notifyOnce.Do(func() {
@@ -39,7 +39,6 @@ func (p *PanicLogger) Notify(team string) func() {
 					"releaseStage", p.opts.ReleaseStage,
 					"appType", p.opts.AppType,
 				)
-				logger.Sync()
 			})
 			panic(r)
 		}
@@ -49,7 +48,7 @@ func (p *PanicLogger) Notify(team string) func() {
 // Handler creates an http Handler that captures any panics that
 // happen. It then repanics so that the default http Server panic handler can
 // handle the panic too.
-func (p *PanicLogger) Handler(h http.Handler) http.Handler {
+func (p *panicLogger) Handler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer p.Notify("")()
 		h.ServeHTTP(w, r)
--- utils/crash/noop.go
+++ utils/crash/noop.go
@@ -4,10 +4,10 @@ import "net/http"
 
 type NOOP struct{}
 
-func (n *NOOP) Notify(team string) func() {
+func (*NOOP) Notify(team string) func() {
 	return func() {}
 }
 
-func (n *NOOP) Handler(h http.Handler) http.Handler {
+func (*NOOP) Handler(h http.Handler) http.Handler {
 	return h
 }
