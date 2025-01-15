35f8eeed1ac9da219cbafaf2a5ba853ef7e26808
mihir <mihir20121997@gmail.com>
2025-01-15 12:34:31 +0530

    chore: add stage in transformer logs

--- processor/transformer/transformer.go
+++ processor/transformer/transformer.go
@@ -537,6 +537,7 @@ func (trans *handle) doPost(ctx context.Context, rawJSON []byte, url, stage stri
 			trans.logger.Warnn(
 				"JS HTTP connection error",
 				logger.NewErrorField(err),
+				logger.NewStringField("stage", stage),
 				logger.NewIntField("attempts", int64(retryCount)),
 			)
 		},
