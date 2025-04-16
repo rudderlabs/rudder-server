5e6eb8145a6540ad4e05934fb97860e54850bfd3
Aris Tzoumas <atzoumas@rudderstack.com>
2025-04-16 17:39:02 +0300

    chore: granular configuration for adaptive rate limiting (#5739)
    # Description
    
    - Support granular configuration in adaptive rate limiter, different for
    each destination.
    - Decouple `throttleTolerancePercentage` from `increasePercentage` and
    `decreasePercentage`
    
    
    ## Security
    
    - [x] The code changed/added as part of this pull request won't create
    any security issues with how the software is being used.

--- router/throttler/adaptivethrottlercounter/algorithm.go
+++ router/throttler/adaptivethrottlercounter/algorithm.go
@@ -16,27 +16,29 @@ type Adaptive struct {
 	wg                   *sync.WaitGroup
 }
 
-func New(config *config.Config, window config.ValueLoader[time.Duration]) *Adaptive {
+func New(destination string, config *config.Config, window config.ValueLoader[time.Duration]) *Adaptive {
 	lf := &limitFactor{value: 1}
 
-	increaseWindowMultiplier := config.GetReloadableIntVar(2, 1, "Router.throttler.adaptive.increaseWindowMultiplier")
+	increaseWindowMultiplier := config.GetReloadableIntVar(2, 1, "Router.throttler.adaptive."+destination+".increaseWindowMultiplier", "Router.throttler.adaptive.increaseWindowMultiplier")
 	increaseCounterWindow := func() time.Duration { return window.Load() * time.Duration(increaseWindowMultiplier.Load()) }
-	increasePercentage := config.GetReloadableInt64Var(10, 1, "Router.throttler.adaptive.increasePercentage")
+	increasePercentage := config.GetReloadableInt64Var(10, 1, "Router.throttler.adaptive."+destination+".increasePercentage", "Router.throttler.adaptive.increasePercentage")
+
 	ilc := &increaseLimitCounter{
 		limitFactor:        lf,
 		window:             increaseCounterWindow,
 		increasePercentage: increasePercentage,
 	}
 
-	decreaseWaitWindowMultiplier := config.GetReloadableIntVar(1, 1, "Router.throttler.adaptive.decreaseWaitWindowMultiplier")
+	decreaseWaitWindowMultiplier := config.GetReloadableIntVar(1, 1, "Router.throttler.adaptive."+destination+".decreaseWaitWindowMultiplier", "Router.throttler.adaptive.decreaseWaitWindowMultiplier")
 	decreaseWaitWindow := func() time.Duration { return window.Load() * time.Duration(decreaseWaitWindowMultiplier.Load()) }
-	decreasePercentage := config.GetReloadableInt64Var(30, 1, "Router.throttler.adaptive.decreasePercentage")
+	decreasePercentage := config.GetReloadableInt64Var(30, 1, "Router.throttler.adaptive."+destination+".decreasePercentage", "Router.throttler.adaptive.decreasePercentage")
+	throttleTolerancePercentage := config.GetReloadableInt64Var(10, 1, "Router.throttler.adaptive."+destination+".throttleTolerancePercentage", "Router.throttler.adaptive.throttleTolerancePercentage")
 	dlc := &decreaseLimitCounter{
 		limitFactor:                 lf,
 		window:                      func() time.Duration { return window.Load() },
 		waitWindow:                  decreaseWaitWindow,
 		decreasePercentage:          decreasePercentage,
-		throttleTolerancePercentage: func() int64 { return min(increasePercentage.Load(), decreasePercentage.Load()) },
+		throttleTolerancePercentage: func() int64 { return throttleTolerancePercentage.Load() },
 	}
 
 	var wg sync.WaitGroup
