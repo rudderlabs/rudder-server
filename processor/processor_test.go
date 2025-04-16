afc77e8bd716502d564cf772c611fea024621da7
Rohith BCS <rohith.bcs@gmail.com>
2025-04-16 13:56:53 +0530

    chore: fix test

--- processor/processor_test.go
+++ processor/processor_test.go
@@ -1874,7 +1874,7 @@ var _ = Describe("Processor with trackedUsers feature enabled", Ordered, func() 
 					"source_category":"",
 					"record_id":null,
 					"workspaceId":"",
-					"traceparent":""
+					"traceparent":"",
 					"connection_id":"source-from-transformer:destination-from-transformer"
 				}`, sourceIDToName[SourceIDEnabledNoUT]), string(job.Parameters))
 			}
@@ -2781,7 +2781,7 @@ var _ = Describe("Processor", Ordered, func() {
 				var paramsMap, expectedParamsMap map[string]interface{}
 				err := jsonrs.Unmarshal(job.Parameters, &paramsMap)
 				Expect(err).To(BeNil())
-				expectedStr := []byte(fmt.Sprintf(`{"source_id": "%v", "destination_id": "enabled-destination-a", "source_job_run_id": "", "error": "error-%v", "status_code": 400, "stage": "dest_transformer", "source_task_run_id": "", "record_id": null,"connection_id":"source-from-transformer:destination-from-transformer"}`, SourceIDEnabled, i+1))
+				expectedStr := []byte(fmt.Sprintf(`{"source_id": "%v", "destination_id": "enabled-destination-a", "source_job_run_id": "", "error": "error-%v", "status_code": 400, "stage": "dest_transformer", "source_task_run_id": "", "record_id": null,"connection_id":""enabled-source:enabled-destination-a"}`, SourceIDEnabled, i+1))
 				err = jsonrs.Unmarshal(expectedStr, &expectedParamsMap)
 				Expect(err).To(BeNil())
 				equals := reflect.DeepEqual(paramsMap, expectedParamsMap)
