e124bb0f846d1f21a6acc2c7eefed9aa09569a73
Akash Chetty <achetty.iitr@gmail.com>
2025-05-09 09:05:16 +0530

    chore: warehouse transformer migration to embedded destination transformer package (#5827)

---
+++ processor/internal/transformer/destination_transformer/embedded/warehouse/transformer.go
@@ -0,0 +1,256 @@
+package warehouse
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"maps"
+	"net/http"
+	"sort"
+	"strings"
+
+	"github.com/google/uuid"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/rudderlabs/rudder-go-kit/config"
+	"github.com/rudderlabs/rudder-go-kit/logger"
+	"github.com/rudderlabs/rudder-go-kit/stats"
+
+	"github.com/rudderlabs/rudder-server/processor/internal/transformer/destination_transformer/embedded/warehouse/internal/model"
+	"github.com/rudderlabs/rudder-server/processor/internal/transformer/destination_transformer/embedded/warehouse/internal/response"
+	wtypes "github.com/rudderlabs/rudder-server/processor/internal/transformer/destination_transformer/embedded/warehouse/internal/types"
+	"github.com/rudderlabs/rudder-server/processor/internal/transformer/destination_transformer/embedded/warehouse/internal/utils"
+	"github.com/rudderlabs/rudder-server/processor/types"
+	"github.com/rudderlabs/rudder-server/utils/timeutil"
+	whutils "github.com/rudderlabs/rudder-server/warehouse/utils"
+)
+
+const (
+	violationErrors     = "violationErrors"
+	redshiftStringLimit = 512
+)
+
+// Compile-time check to ensure ValidationError struct remains unchanged.
+var _ = struct {
+	Type     string
+	Message  string
+	Meta     map[string]string
+	Property string
+}(types.ValidationError{})
+
+func New(conf *config.Config, logger logger.Logger, statsFactory stats.Stats) *Transformer {
+	t := &Transformer{
+		logger:         logger.Child("warehouse-transformer"),
+		statsFactory:   statsFactory,
+		now:            timeutil.Now,
+		uuidGenerator:  uuid.NewString,
+		loggedFileName: generateLogFileName(),
+		sorter: func(x []string) []string {
+			sort.Strings(x)
+			return x
+		},
+	}
+
+	t.stats.matchedEvents = t.statsFactory.NewStat("warehouse_dest_transform_matched_events", stats.HistogramType)
+	t.stats.mismatchedEvents = t.statsFactory.NewStat("warehouse_dest_transform_mismatched_events", stats.HistogramType)
+	t.stats.comparisonTime = t.statsFactory.NewStat("warehouse_dest_transform_comparison_time", stats.TimerType)
+
+	t.config.enableIDResolution = conf.GetReloadableBoolVar(false, "Warehouse.enableIDResolution")
+	t.config.populateSrcDestInfoInContext = conf.GetReloadableBoolVar(true, "WH_POPULATE_SRC_DEST_INFO_IN_CONTEXT")
+	t.config.maxColumnsInEvent = conf.GetReloadableIntVar(200, 1, "WH_MAX_COLUMNS_IN_EVENT")
+	t.config.maxLoggedEvents = conf.GetReloadableIntVar(10000, 1, "Warehouse.maxLoggedEvents")
+	t.config.concurrentTransformations = conf.GetReloadableIntVar(10, 1, "Warehouse.concurrentTransformations")
+	return t
+}
+
+func (t *Transformer) Transform(_ context.Context, clientEvents []types.TransformerEvent) (res types.Response) {
+	if len(clientEvents) == 0 {
+		return
+	}
+
+	startTime := t.now()
+	metadata := clientEvents[0].Metadata
+	c := &cache{}
+
+	defer func() {
+		tags := stats.Tags{
+			"workspaceId":     metadata.WorkspaceID,
+			"sourceId":        metadata.SourceID,
+			"sourceType":      metadata.SourceType,
+			"destinationId":   metadata.DestinationID,
+			"destinationType": metadata.DestinationType,
+		}
+
+		t.statsFactory.NewTaggedStat("warehouse_dest_transform_request_latency", stats.TimerType, tags).Since(startTime)
+		t.statsFactory.NewTaggedStat("warehouse_dest_transform_requests", stats.CountType, tags).Increment()
+		t.statsFactory.NewTaggedStat("warehouse_dest_transform_input_events", stats.HistogramType, tags).Observe(float64(len(clientEvents)))
+		t.statsFactory.NewTaggedStat("warehouse_dest_transform_output_events", stats.HistogramType, tags).Observe(float64(len(res.Events)))
+		t.statsFactory.NewTaggedStat("warehouse_dest_transform_output_failed_events", stats.HistogramType, tags).Observe(float64(len(res.FailedEvents)))
+	}()
+
+	results := make(chan types.TransformerResponse, len(clientEvents))
+	done := make(chan struct{})
+
+	g := errgroup.Group{}
+	g.SetLimit(t.config.concurrentTransformations.Load())
+
+	go func() {
+		defer close(done)
+		for r := range results {
+			if r.Error != "" {
+				res.FailedEvents = append(res.FailedEvents, r)
+			} else {
+				res.Events = append(res.Events, r)
+			}
+		}
+	}()
+
+	for i := range clientEvents {
+		event := clientEvents[i]
+		eventMetadata := event.Metadata
+		eventMetadata.SourceDefinitionType = "" // TODO: Currently, it's getting ignored during JSON marshalling Remove this once we start using it.
+
+		g.Go(func() error {
+			r, err := t.processWarehouseMessage(c, &event)
+			if err != nil {
+				results <- transformerResponseFromErr(&eventMetadata, err)
+				return nil
+			}
+			for _, item := range r {
+				results <- types.TransformerResponse{
+					Output:     item,
+					Metadata:   eventMetadata,
+					StatusCode: http.StatusOK,
+				}
+			}
+			return nil
+		})
+	}
+
+	_ = g.Wait()
+	close(results)
+	<-done
+	return
+}
+
+func (t *Transformer) processWarehouseMessage(cache *cache, event *types.TransformerEvent) ([]map[string]any, error) {
+	wEvent := wtypes.New(event, t.uuidGenerator, t.now)
+	if err := t.checkValidContext(wEvent); err != nil {
+		return nil, fmt.Errorf("checking valid context: %w", err)
+	}
+	return t.handleEvent(wEvent, cache)
+}
+
+func (t *Transformer) checkValidContext(event *wtypes.TransformerEvent) error {
+	if !t.config.populateSrcDestInfoInContext.Load() {
+		return nil
+	}
+	contextVal, exists := event.Message["context"]
+	if !exists || contextVal == nil {
+		return nil
+	}
+	if !utils.IsObject(contextVal) {
+		return response.ErrContextNotMap
+	}
+	return nil
+}
+
+func (t *Transformer) eventContext(tec *transformEventContext) any {
+	contextVal, exists := tec.event.Message["context"]
+	if !t.config.populateSrcDestInfoInContext.Load() {
+		return contextVal
+	}
+	if !exists || contextVal == nil {
+		contextVal = map[string]interface{}{}
+	}
+	clonedContext := maps.Clone(contextVal.(map[string]any))
+	clonedContext["sourceId"] = tec.event.Metadata.SourceID
+	clonedContext["sourceType"] = tec.event.Metadata.SourceType
+	clonedContext["destinationId"] = tec.event.Metadata.DestinationID
+	clonedContext["destinationType"] = tec.event.Metadata.DestinationType
+	return clonedContext
+}
+
+func (t *Transformer) handleEvent(event *wtypes.TransformerEvent, cache *cache) ([]map[string]any, error) {
+	intrOpts := extractIntrOpts(event.Metadata.DestinationType, event.Message)
+	destOpts := extractDestOpts(event.Metadata.DestinationType, event.Metadata.DestinationConfig)
+	jsonPathsInfo := extractJSONPathInfo(append(intrOpts.jsonPaths, destOpts.jsonPaths...))
+
+	eventType := strings.ToLower(event.Metadata.EventType)
+
+	tec := &transformEventContext{
+		event:         event,
+		intrOpts:      &intrOpts,
+		destOpts:      &destOpts,
+		jsonPathsInfo: &jsonPathsInfo,
+		cache:         cache,
+		sorter:        t.sorter,
+	}
+
+	switch eventType {
+	case "track":
+		return t.trackEvents(tec)
+	case "identify":
+		return t.identifyEvents(tec)
+	case "page":
+		return t.pageEvents(tec)
+	case "screen":
+		return t.screenEvents(tec)
+	case "alias":
+		return t.aliasEvents(tec)
+	case "group":
+		return t.groupEvents(tec)
+	case "extract":
+		return t.extractEvents(tec)
+	case "merge":
+		return t.mergeEvents(tec)
+	default:
+		return nil, response.NewTransformerError(fmt.Sprintf("Unknown event type: %q", eventType), http.StatusBadRequest)
+	}
+}
+
+func transformerResponseFromErr(metadata *types.Metadata, err error) types.TransformerResponse {
+	var te *response.TransformerError
+	if ok := errors.As(err, &te); ok {
+		return types.TransformerResponse{
+			Output:     nil,
+			Metadata:   *metadata,
+			Error:      te.Error(),
+			StatusCode: te.StatusCode(),
+		}
+	}
+	return types.TransformerResponse{
+		Output:     nil,
+		Metadata:   *metadata,
+		Error:      response.ErrInternalServer.Error(),
+		StatusCode: response.ErrInternalServer.StatusCode(),
+	}
+}
+
+func (t *Transformer) getColumns(
+	destType string,
+	data map[string]any, metadata map[string]string,
+) (map[string]any, error) {
+	columns := make(map[string]any, len(data))
+
+	// uuid_ts and loaded_at datatypes are passed from here to create appropriate columns.
+	// Corresponding values are inserted when loading into the warehouse
+	uuidTS := whutils.ToProviderCase(destType, "uuid_ts")
+	columns[uuidTS] = model.DateTimeDataType
+
+	if destType == whutils.BQ {
+		columns["loaded_at"] = model.DateTimeDataType
+	}
+
+	for key, value := range data {
+		if dataType, ok := metadata[key]; ok {
+			columns[key] = dataType
+		} else {
+			columns[key] = dataTypeFor(destType, key, value, false)
+		}
+	}
+	if len(columns) > t.config.maxColumnsInEvent.Load() && !utils.IsRudderSources(data) && !utils.IsDataLake(destType) {
+		return nil, response.NewTransformerError(fmt.Sprintf("%s transformer: Too many columns outputted from the event", strings.ToLower(destType)), http.StatusBadRequest)
+	}
+	return columns, nil
+}
