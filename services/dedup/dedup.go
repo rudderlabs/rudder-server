bdaa10fa8d22a870230005b8bb976b8e51e169c0
Francesco Casula <fra.casula@gmail.com>
2025-06-18 12:16:40 +0200

    chore: leaky err logger

--- services/dedup/dedup.go
+++ services/dedup/dedup.go
@@ -3,6 +3,9 @@
 package dedup
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/rudderlabs/rudder-go-kit/config"
 	"github.com/rudderlabs/rudder-go-kit/logger"
 	"github.com/rudderlabs/rudder-go-kit/stats"
@@ -30,11 +33,14 @@ func New(conf *config.Config, stats stats.Stats, log logger.Logger) (Dedup, erro
 	if conf.GetBool("Dedup.KeyDB.Mirror", false) {
 		keydbDedup, err := keydb.NewKeyDB(conf, stats, log)
 		if err == nil {
-			return &mirror{
+			m := &mirror{
 				Dedup:  badgerDedup,
 				mirror: keydbDedup,
+				errs:   make(chan error, 1),
 				logger: log,
-			}, nil
+			}
+			go m.printErrs(conf)
+			return m, nil
 		}
 		log.Errorn("Failed to create keydb dedup", obskit.Error(err))
 	}
@@ -58,13 +64,17 @@ type Dedup interface {
 type mirror struct {
 	Dedup
 	mirror Dedup
+	errs   chan error
 	logger logger.Logger
 }
 
 func (m *mirror) Allowed(keys ...BatchKey) (map[BatchKey]bool, error) {
 	go func() { // fire & forget
 		if _, err := m.mirror.Allowed(keys...); err != nil {
-			m.logger.Errorn("Dedup mirroring call to Allowed failed", obskit.Error(err))
+			select {
+			case m.errs <- fmt.Errorf("call to Allowed failed: %w", err):
+			default: // leaky bucket to avoid filling the logs if the system fails badly
+			}
 		}
 	}()
 	return m.Dedup.Allowed(keys...)
@@ -73,13 +83,31 @@ func (m *mirror) Allowed(keys ...BatchKey) (map[BatchKey]bool, error) {
 func (m *mirror) Commit(keys []string) error {
 	go func() { // fire & forget
 		if err := m.mirror.Commit(keys); err != nil {
-			m.logger.Errorn("Dedup mirroring call to Commit failed", obskit.Error(err))
+			select {
+			case m.errs <- fmt.Errorf("call to Commit failed: %w", err):
+			default: // leaky bucket to avoid filling the logs if the system fails badly
+			}
 		}
 	}()
 	return m.Dedup.Commit(keys)
 }
 
 func (m *mirror) Close() {
-	go m.mirror.Close()
+	m.mirror.Close()
+	close(m.errs)
 	m.Dedup.Close()
+}
+
+func (m *mirror) printErrs(conf *config.Config) {
+	ticker := time.NewTicker(conf.GetDuration("Dedup.KeyDB.PrintErrorsInterval", 10, time.Second))
+	for range ticker.C {
+		select {
+		case err, ok := <-m.errs:
+			if !ok {
+				return
+			}
+			m.logger.Errorn("Dedup mirroring error", obskit.Error(err))
+		default:
+		}
+	}
 }
