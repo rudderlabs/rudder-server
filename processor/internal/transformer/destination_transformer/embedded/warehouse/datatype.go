fdcd09fd9a49f515ebb65a7f4751cb4c1602ed45
Akash Chetty <achetty.iitr@gmail.com>
2025-06-30 13:39:45 +0530

    fix: custom type in payload during enrichment (#6064)
    # Description
    
    - We rely on being `SingularEvent` only to contain `map[string]any`, but
    some context properties are being directly added to the SingularEvent as
    the struct type like these:
    - [Bot
    Details](https://github.com/rudderlabs/rudder-server/blob/master/internal/enricher/bot.go#L49-L54).
    - [Geo
    enrichment](https://github.com/rudderlabs/rudder-server/blob/master/internal/enricher/geolocation.go#L134-L135)
    - [Tracking
    plan.](https://github.com/rudderlabs/rudder-server/blob/master/processor/trackingplan.go#L32-L50)
    - In case of Bot Details, we don't have access to the struct, so we
    can't infer the type and handle it. Adding support for any custom struct
    which checks for `reflect.Struct`, `reflect.Ptr`, `reflect.Slice`,
    `reflect.Array` using reflection.
    
    ## Linear Ticket
    
    - Resolves WAR-821
    
    ## Security
    
    - [x] The code changed/added as part of this pull request won't create
    any security issues with how the software is being used.

--- processor/internal/transformer/destination_transformer/embedded/warehouse/datatype.go
+++ processor/internal/transformer/destination_transformer/embedded/warehouse/datatype.go
@@ -1,15 +1,10 @@
 package warehouse
 
 import (
-	"fmt"
 	"math/big"
 
-	"github.com/samber/lo"
-
-	"github.com/rudderlabs/rudder-server/internal/enricher"
 	"github.com/rudderlabs/rudder-server/processor/internal/transformer/destination_transformer/embedded/warehouse/internal/model"
 	"github.com/rudderlabs/rudder-server/processor/internal/transformer/destination_transformer/embedded/warehouse/internal/utils"
-	"github.com/rudderlabs/rudder-server/processor/types"
 	whutils "github.com/rudderlabs/rudder-server/warehouse/utils"
 )
 
@@ -28,7 +23,7 @@ func dataTypeFor(destType, key string, val any, isJSONKey bool) string {
 
 func primitiveType(val any) string {
 	switch v := val.(type) {
-	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, uintptr:
 		return model.IntDataType
 	case float64:
 		return getFloatType(v)
@@ -85,7 +80,7 @@ func overrideForRedshift(val any, isJSONKey bool) string {
 
 func shouldUseTextForRedshift(data any) bool {
 	switch v := data.(type) {
-	case []any, []types.ValidationError, map[string]any:
+	case []any, map[string]any:
 		jsonVal, _ := utils.MarshalJSON(v)
 		// Javascript strings are UTF-16 encoded, use utf16 instead of utf8 package for determining the length
 		if utils.UTF16RuneCountInString(string(jsonVal)) > redshiftStringLimit {
@@ -105,103 +100,4 @@ func convertValIfDateTime(val any, colType string) any {
 		return utils.ToTimestamp(val)
 	}
 	return val
-}
-
-func convertToFloat64IfInteger(val any) any {
-	switch v := val.(type) {
-	case int:
-		return float64(v)
-	case int8:
-		return float64(v)
-	case int16:
-		return float64(v)
-	case int32:
-		return float64(v)
-	case int64:
-		return float64(v)
-	case uint:
-		return float64(v)
-	case uint8:
-		return float64(v)
-	case uint16:
-		return float64(v)
-	case uint32:
-		return float64(v)
-	case uint64:
-		return float64(v)
-	}
-	return val
-}
-
-func convertToSliceIfViolationErrors(val any) any {
-	if validationErrors, ok := val.([]types.ValidationError); ok {
-		result := make([]any, len(validationErrors))
-		for i, e := range validationErrors {
-			result[i] = map[string]any{
-				"type":     e.Type,
-				"message":  e.Message,
-				"meta":     lo.MapValues(e.Meta, func(value, _ string) any { return value }),
-				"property": e.Property,
-			}
-		}
-		return result
-	}
-	return val
-}
-
-func addDataAndMetadataForContextGeoEnrichment(tec *transformEventContext, data map[string]any, metadata map[string]string, key string, val any) error {
-	if geoLocation, ok := val.(enricher.Geolocation); ok {
-		if len(geoLocation.IP) > 0 {
-			ipKey, err := safeColumnNameCached(tec, transformColumnNameCached(tec, key+"_ip"))
-			if err != nil {
-				return fmt.Errorf("could not get ip column name: %w", err)
-			}
-			data[ipKey], metadata[ipKey] = geoLocation.IP, model.StringDataType
-		}
-		if len(geoLocation.City) > 0 {
-			cityKey, err := safeColumnNameCached(tec, transformColumnNameCached(tec, key+"_city"))
-			if err != nil {
-				return fmt.Errorf("could not get city column name: %w", err)
-			}
-			data[cityKey], metadata[cityKey] = geoLocation.City, model.StringDataType
-		}
-		if len(geoLocation.Country) > 0 {
-			countryKey, err := safeColumnNameCached(tec, transformColumnNameCached(tec, key+"_country"))
-			if err != nil {
-				return fmt.Errorf("could not get country column name: %w", err)
-			}
-			data[countryKey], metadata[countryKey] = geoLocation.Country, model.StringDataType
-		}
-		if len(geoLocation.Region) > 0 {
-			regionKey, err := safeColumnNameCached(tec, transformColumnNameCached(tec, key+"_region"))
-			if err != nil {
-				return fmt.Errorf("could not get region column name: %w", err)
-			}
-			data[regionKey], metadata[regionKey] = geoLocation.Region, model.StringDataType
-		}
-		if len(geoLocation.Postal) > 0 {
-			postalKey, err := safeColumnNameCached(tec, transformColumnNameCached(tec, key+"_postal"))
-			if err != nil {
-				return fmt.Errorf("could not get postal column name: %w", err)
-			}
-			data[postalKey], metadata[postalKey] = geoLocation.Postal, model.StringDataType
-		}
-		if len(geoLocation.Location) > 0 {
-			locationKey, err := safeColumnNameCached(tec, transformColumnNameCached(tec, key+"_location"))
-			if err != nil {
-				return fmt.Errorf("could not get location column name: %w", err)
-			}
-			data[locationKey], metadata[locationKey] = geoLocation.Location, model.StringDataType
-		}
-		if len(geoLocation.Timezone) > 0 {
-			timezoneKey, err := safeColumnNameCached(tec, transformColumnNameCached(tec, key+"_timezone"))
-			if err != nil {
-				return fmt.Errorf("could not get timezone column name: %w", err)
-			}
-			data[timezoneKey], metadata[timezoneKey] = geoLocation.Timezone, model.StringDataType
-		}
-		return nil
-	}
-	data[key] = val
-	return nil
 }
