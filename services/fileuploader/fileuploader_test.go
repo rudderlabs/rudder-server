eab797de5864d8a6af6fa738b3782b3eca51c59d
Mihir Khambhati <inmihir11@gmail.com>
2025-07-15 13:47:01 +0530

    chore: add test to check region is only removed for S3

--- services/fileuploader/fileuploader_test.go
+++ services/fileuploader/fileuploader_test.go
@@ -330,4 +330,21 @@ func TestOverride(t *testing.T) {
 		"b": "2",
 		"c": "3",
 	}))
+
+	config = map[string]interface{}{
+		"a":      "1",
+		"b":      "2",
+		"c":      "3",
+		"region": "us-east-1",
+	}
+	settings = backendconfig.StorageBucket{
+		Type: "MINIO",
+	}
+	bucket = overrideWithSettings(config, settings, "wrk-1")
+	Expect(bucket.Config).To(Equal(map[string]interface{}{
+		"a":      "1",
+		"b":      "2",
+		"c":      "3",
+		"region": "us-east-1",
+	}))
 }
