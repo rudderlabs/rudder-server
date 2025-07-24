rudderlabs/rudder-server

commit f9db3c5e83655141c08ea2473ea0c2c1d42ce44b

chore: reset timer to optimise router batching (#6114)

router/worker.go
@@ -62,8 +62,8 @@ type workerJob struct {
 }
 
 func (w *worker) workLoop() {
-	timeout := time.After(w.rt.reloadableConfig.jobsBatchTimeout.Load())
 	for {
+		timeout := time.After(w.rt.reloadableConfig.jobsBatchTimeout.Load())
 		select {
 		case message, hasMore := <-w.input:
 			if !hasMore {
@@ -251,8 +251,6 @@ func (w *worker) workLoop() {
 			}
 
 		case <-timeout:
-			timeout = time.After(w.rt.reloadableConfig.jobsBatchTimeout.Load())
-
 			if len(w.routerJobs) > 0 {
 				if w.rt.enableBatching {
 					w.destinationJobs = w.batchTransform(w.routerJobs)

commit 5c7e8d419abd038da0b4326d3e7b74752cb45e30

fix(router): worker's batch timeout is getting resetted after new job arrival (#6167)
# Description

Regression introduced by #6114
After a new job arrives in the worker's queue, the worker's batch
timeout is getting reset. This causes the following behaviour:
- For pipelines with high traffic: no significant effect
- For pipelines with low traffic: 
- No significant effect, as long as events arrive infrequently, i.e.
less than 1 event to each worker every 5sec
- Processing stalls until the full batch gets filled, as long as events
arrive more frequent than 5sec. Increased delivery times are observed by
customers in such a case.


We are now resetting the timer only after we actually process a batch

## Linear Ticket

resolves PIPE-2260

## Security

- [x] The code changed/added as part of this pull request won't create
any security issues with how the software is being used.

router/handle_lifecycle.go
@@ -204,7 +204,7 @@ func (rt *Handle) Setup(
 	rt.limiter.stats.pickup = partition.NewStats()
 
 	rt.limiter.transform = kitsync.NewReloadableLimiter(ctx, &limiterGroup, "rt_transform",
-		getReloadableRouterConfigInt("Limiter.transform.limit", rt.destType, 200),
+		getReloadableRouterConfigInt("Limiter.transform.limit", rt.destType, 1024),
 		stats.Default,
 		kitsync.WithLimiterDynamicPeriod(config.GetDurationVar(1, time.Second, getRouterConfigKeys("Limiter.transform.dynamicPeriod", destType)...)),
 		kitsync.WithLimiterTags(map[string]string{"destType": rt.destType}),

router/worker.go
@@ -62,8 +62,11 @@ type workerJob struct {
 }
 
 func (w *worker) workLoop() {
+	jobsBatchTimeout := time.After(w.rt.reloadableConfig.jobsBatchTimeout.Load())
+	resetjobsBatchTimeout := func() {
+		jobsBatchTimeout = time.After(w.rt.reloadableConfig.jobsBatchTimeout.Load())
+	}
 	for {
-		timeout := time.After(w.rt.reloadableConfig.jobsBatchTimeout.Load())
 		select {
 		case message, hasMore := <-w.input:
 			if !hasMore {
@@ -227,6 +230,7 @@ func (w *worker) workLoop() {
 				if len(w.routerJobs) >= w.rt.reloadableConfig.noOfJobsToBatchInAWorker.Load() {
 					w.destinationJobs = w.batchTransform(w.routerJobs)
 					w.processDestinationJobs()
+					resetjobsBatchTimeout()
 				}
 			} else if parameters.TransformAt == "router" {
 				w.routerJobs = append(w.routerJobs, types.RouterJobT{
@@ -239,6 +243,7 @@ func (w *worker) workLoop() {
 				if len(w.routerJobs) >= w.rt.reloadableConfig.noOfJobsToBatchInAWorker.Load() {
 					w.destinationJobs = w.transform(w.routerJobs)
 					w.processDestinationJobs()
+					resetjobsBatchTimeout()
 				}
 			} else {
 				w.destinationJobs = append(w.destinationJobs, types.DestinationJobT{
@@ -248,9 +253,10 @@ func (w *worker) workLoop() {
 					JobMetadataArray: []types.JobMetadataT{jobMetadata},
 				})
 				w.processDestinationJobs()
+				resetjobsBatchTimeout()
 			}
 
-		case <-timeout:
+		case <-jobsBatchTimeout:
 			if len(w.routerJobs) > 0 {
 				if w.rt.enableBatching {
 					w.destinationJobs = w.batchTransform(w.routerJobs)
@@ -258,6 +264,7 @@ func (w *worker) workLoop() {
 					w.destinationJobs = w.transform(w.routerJobs)
 				}
 				w.processDestinationJobs()
+				resetjobsBatchTimeout()
 			}
 		}
 	}