5ea53208de017bb914516941363f5796c383196d
mihir <mihir20121997@gmail.com>
2025-08-11 11:25:30 +0530

    chore: update keydb dependency
    - Update keydb dependency from v0.1.0-alpha to v0.2.0-alpha
    - Refactor dedup service initialization in test to use new constructor
    - Remove unused keydb cache import and factory function
    - Update mockedCloudStorage to implement new Download method signature

--- services/dedup/dedup_test.go
+++ services/dedup/dedup_test.go
@@ -17,7 +17,6 @@ import (
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 
-	keydbcache "github.com/rudderlabs/keydb/cache"
 	keydbclient "github.com/rudderlabs/keydb/client"
 	keydb "github.com/rudderlabs/keydb/node"
 	keydbproto "github.com/rudderlabs/keydb/proto"
@@ -577,12 +576,8 @@ func startKeydb(t testing.TB, conf *config.Config) {
 		SnapshotInterval: time.Minute,
 		Addresses:        []string{address},
 	}
-	cf := func(hashRange uint32) (keydb.Cache, error) {
-		conf.Set("BadgerDB.Dedup.Path", t.TempDir())
-		return keydbcache.BadgerFactory(conf, logger.NOP)(hashRange)
-	}
 	require.NoError(t, err)
-	service, err = keydb.NewService(ctx, nodeConfig, cf, &mockedCloudStorage{}, logger.NOP)
+	service, err = keydb.NewService(ctx, nodeConfig, &mockedCloudStorage{}, conf, stats.NOP, logger.NOP)
 	require.NoError(t, err)
 
 	// Create a gRPC server
@@ -626,7 +621,10 @@ func (m *mockedFilemanagerSession) Next() (fileObjects []*filemanager.FileInfo, 
 
 type mockedCloudStorage struct{}
 
-func (m *mockedCloudStorage) Download(_ context.Context, _ io.WriterAt, _ string) error { return nil }
+func (m *mockedCloudStorage) Download(_ context.Context, _ io.WriterAt, _ string, options ...filemanager.DownloadOption) error {
+	return nil
+}
+
 func (m *mockedCloudStorage) UploadReader(_ context.Context, _ string, _ io.Reader) (filemanager.UploadedFile, error) {
 	return filemanager.UploadedFile{}, nil
 }
