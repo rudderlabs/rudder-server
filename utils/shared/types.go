69444131b40930ebd39d888a19c8527831af7ff6
Satish Kumar <satish@rudderstack.com>
2025-09-08 12:31:29 +0530

    add types file

---
+++ utils/shared/types.go
@@ -0,0 +1,44 @@
+package shared
+
+type Metadata struct {
+	WorkspaceID string
+
+	SourceID           string
+	SourceTaskRunID    string
+	SourceJobID        string
+	SourceJobRunID     string
+	SourceDefinitionID string
+	SourceCategory     string
+
+	DestinationID           string
+	DestinationDefinitionID string
+
+	TransformationID        string
+	TransformationVersionID string
+
+	TrackingPlanID      string
+	TrackingPlanVersion int
+
+	EventType string
+	EventName string
+}
+
+type EventWithMetadata struct {
+	Event     map[string]interface{}
+	MessageID string
+	Metadata  *Metadata
+}
+
+type Mapping struct {
+	ID       string `json:"id"`
+	Type     string `json:"type"`
+	Incoming string `json:"incoming"`
+	Target   string `json:"target"`
+	Status   string `json:"status"`
+	Count    int64  `json:"count"`
+}
+
+type StatusDetails struct {
+	Status   string
+	Mappings []Mapping
+}
