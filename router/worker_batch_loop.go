003e52eabd534981dade3dc482ad003c9cf84704
Aris Tzoumas <atzoumas@rudderstack.com>
2025-10-24 11:08:43 +0300

    feat(router): dynamic worker job buffers
    🔒 Scanned for secrets using gitleaks 8.28.0

--- router/worker_batch_loop.go
+++ router/worker_batch_loop.go
@@ -1,21 +1,25 @@
 package router
 
 import (
+	"context"
 	"time"
 
 	"github.com/rudderlabs/rudder-go-kit/config"
+	"github.com/rudderlabs/rudder-go-kit/stats"
 	"github.com/rudderlabs/rudder-server/router/types"
 )
 
 type workerBatchLoop struct {
+	ctx                      context.Context                                             // context for managing the lifecycle of the loop
 	jobsBatchTimeout         config.ValueLoader[time.Duration]                           // timeout for processing jobs in a batch
 	noOfJobsToBatchInAWorker config.ValueLoader[int]                                     // maximum number of jobs to batch in a worker before processing
-	inputCh                  chan workerJob                                              // channel to receive jobs for processing
+	inputCh                  <-chan workerJob                                            // channel to receive jobs for processing
 	enableBatching           bool                                                        // whether to enable batching of jobs
 	batchTransform           func(routerJobs []types.RouterJobT) []types.DestinationJobT // function to transform router jobs into destination jobs in batch mode
 	transform                func(routerJobs []types.RouterJobT) []types.DestinationJobT // function to transform router jobs into destination jobs in non-batch mode
 	process                  func(destinationJobs []types.DestinationJobT)               // function to process the transformed destination jobs
 	acceptWorkerJob          func(workerJob workerJob) *types.RouterJobT                 // function to accept a worker job and return a router job if applicable
+	throughputStat           stats.Histogram                                             // stat to record throughput of the loop
 }
 
 // runLoop processes jobs from the input channel, batching them if it is enabled.
@@ -24,6 +28,7 @@ func (wl *workerBatchLoop) runLoop() {
 	var routerJobs []types.RouterJobT
 	doProcessRouterJobs := func() {
 		if len(routerJobs) > 0 {
+			start := time.Now()
 			var destinationJobs []types.DestinationJobT
 			if wl.enableBatching {
 				destinationJobs = wl.batchTransform(routerJobs)
@@ -31,6 +36,8 @@ func (wl *workerBatchLoop) runLoop() {
 				destinationJobs = wl.transform(routerJobs)
 			}
 			wl.process(destinationJobs)
+			throughputPerSecond := float64(len(routerJobs)) / (float64(time.Since(start).Milliseconds()) / 1000)
+			wl.throughputStat.Observe(throughputPerSecond)
 			routerJobs = nil // reset routerJobs for the next batch
 		}
 		jobsBatchTimeout = time.After(wl.jobsBatchTimeout.Load()) // reset the timeout
@@ -41,6 +48,13 @@ func (wl *workerBatchLoop) runLoop() {
 			if !ok {
 				doProcessRouterJobs() // process any remaining jobs in the batch
 				return                // input channel is closed, exit the loop
+			}
+			if wl.ctx.Err() != nil {
+				// Context is done, we are stopping the worker
+				// it is fine to ignore any pending jobs in the input channel (executing state)
+				// since when the router restarts, it will mark them as failed and reprocess them.
+				// What is important is to stop as soon as possible.
+				return
 			}
 			if !wl.enableBatching && workerJob.parameters.TransformAt != "router" && len(routerJobs) > 0 {
 				// process the current batch if batching is not enabled, transform for the current job is not at router and there are pending jobs in the batch
