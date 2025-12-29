2635fd09c983f110438ce2f1a1e785ce445d2aee
Aris Tzoumas <atzoumas@rudderstack.com>
2025-12-29 09:58:05 +0200

    fixup! feat: jobsdb partition migration grpc server and client
    🔒 Scanned for secrets using gitleaks 8.30.0

--- cluster/migrator/partitionmigration/client/migration_job_executor.go
+++ cluster/migrator/partitionmigration/client/migration_job_executor.go
@@ -106,247 +106,237 @@ type migrationJobExecutor struct {
 
 // Run executes the migration job
 func (mpe *migrationJobExecutor) Run(ctx context.Context) error {
-	err := func() error {
-		defer mpe.stats.NewTaggedStat("partition_mig_jobexec_run_time", stats.TimerType, mpe.statsTags()).RecordDuration()()
-		mpe.logger.Infon("Starting partition migration")
+	defer mpe.stats.NewTaggedStat("partition_mig_jobexec_run_time", stats.TimerType, mpe.statsTags()).RecordDuration()()
+	mpe.logger.Infon("Starting partition migration")
 
-		// mark any executing jobs as failed to handle previous interrupted migrations
-		if err := mpe.markExecutingJobsAsFailed(ctx); err != nil {
-			return fmt.Errorf("marking executing jobs as failed: %w", err)
-		}
-
-		// create a partition migrator client
-		client, err := NewPartitionMigrationClient(mpe.target, mpe.conf)
-		if err != nil {
-			return fmt.Errorf("creating partition migrator client: %w", err)
-		}
-		defer func() { // close the client on exit
-			if err := client.Close(); err != nil {
-				mpe.logger.Errorn("closing partition migrator client", obskit.Error(err))
-			}
-		}()
-
-		streamGroup, streamCtx := errgroup.WithContext(ctx) // group for sender and receiver goroutines
-		// open the stream
-		mpe.logger.Debugn("opening the stream")
-		stream, err := client.StreamJobs(streamCtx)
-		if err != nil {
-			return fmt.Errorf("initiating stream jobs: %w", err)
-		}
-
-		var totalSent atomic.Int64  // counter for total sent jobs
-		var totalAcked atomic.Int64 // counter for total acknowledged jobs
-
-		// allow only one unacked batch to be added at a time
-		// channel is closed when sender is done
-		var unackedBatchesMu sync.Mutex
-		unackedBatches := make(map[int][]*jobsdb.JobT)            // map of batch index to unacked jobs in case of failure, for marking them as failed
-		batchesToAck := make(chan lo.Tuple2[int, []*jobsdb.JobT]) // channel for sending unacked batch info to the receiver goroutine to acknowledge (one at a time)
-
-		// sender goroutine
-		streamGroup.Go(func() error {
-			mpe.logger.Infon("Starting to send jobs for migration")
-			defer mpe.logger.Infon("Finished sending jobs for migration")
-
-			// start by sending the metadata
-			mpe.logger.Debugn("sending metadata")
-			if err := mpe.sendWithTimeout(ctx, stream, &proto.StreamJobsRequest{Payload: &proto.StreamJobsRequest_Metadata{
-				Metadata: &proto.JobStreamMetadata{
-					MigrationJobId: mpe.migrationJobID,
-					TablePrefix:    mpe.sourceDB.Identifier(),
-					PartitionIds:   mpe.partitionIDs,
-				},
-			}}); err != nil {
-				return fmt.Errorf("sending metadata: %w", err)
-			}
-			mpe.logger.Debugn("metadata was sent")
-
-			// send job batches in chunks
-			batchIndex := -1
-			for {
-				batchIndex++
-				jobs, err := mpe.sourceDB.GetToProcess(streamCtx, jobsdb.GetQueryParams{
-					PartitionFilters: mpe.partitionIDs,
-					JobsLimit:        mpe.batchSize.Load(),
-				}, nil)
-				if err != nil {
-					return fmt.Errorf("getting batch %d of jobs to process: %w", batchIndex, err)
-				}
-				if len(jobs.Jobs) == 0 && !jobs.DSLimitsReached {
-					// no more jobs to process, we are done sending
-					_ = stream.CloseSend()
-					close(batchesToAck)
-					return nil
-				}
-				mpe.logger.Debugn("about to send batch of jobs",
-					logger.NewIntField("batchIndex", int64(batchIndex)),
-					logger.NewIntField("jobCount", int64(len(jobs.Jobs))),
-				)
-
-				mpe.logger.Debugn("marking jobs as executing",
-					logger.NewIntField("batchIndex", int64(batchIndex)),
-				)
-				// mark them as executing
-				if err := mpe.updateJobStatus(streamCtx, jobs.Jobs, jobsdb.Executing.State, nil); err != nil {
-					return fmt.Errorf("marking batch %d of jobs as executing: %w", batchIndex, err)
-				}
-				mpe.logger.Debugn("marked jobs as executing",
-					logger.NewIntField("batchIndex", int64(batchIndex)),
-				)
-				// add unacked batch
-				unackedBatchesMu.Lock()
-				unackedBatches[batchIndex] = jobs.Jobs
-				unackedBatchesMu.Unlock()
-				// send the batch info to the receiver goroutine
-				mpe.logger.Debugn("sending batch info to the receiver goroutine",
-					logger.NewIntField("batchIndex", int64(batchIndex)),
-				)
-
-				// split jobs into chunks and send them
-				chunks := lo.Chunk(jobs.Jobs, mpe.chunkSize.Load())
-				mpe.logger.Debugn("sending batch in chunks",
-					logger.NewIntField("batchIndex", int64(batchIndex)),
-					logger.NewIntField("chunkCount", int64(len(chunks))),
-				)
-				for i, chunk := range chunks {
-					mpe.logger.Debugn("sending chunk",
-						logger.NewIntField("batchIndex", int64(batchIndex)),
-						logger.NewIntField("chunk", int64(i+1)),
-					)
-					lastChunk := i == len(chunks)-1
-					if lastChunk { // send batch info to receiver only on last chunk
-						select {
-						case batchesToAck <- lo.T2(batchIndex, jobs.Jobs):
-							// added successfully
-						case <-streamCtx.Done():
-							return streamCtx.Err()
-						}
-					}
-					if err := mpe.sendWithTimeout(streamCtx, stream, &proto.StreamJobsRequest{
-						Payload: &proto.StreamJobsRequest_Chunk{
-							Chunk: &proto.JobsBatchChunk{
-								BatchIndex: int64(batchIndex),
-								LastChunk:  lastChunk,
-								Jobs:       lo.Map(chunk, proto.JobFromJobsdbJob),
-							},
-						},
-					}); err != nil {
-						return fmt.Errorf("streaming jobs chunk %d for batch %d: %w", i+1, batchIndex, err)
-					}
-					mpe.logger.Debugn("sent chunk",
-						logger.NewIntField("batchIndex", int64(batchIndex)),
-						logger.NewIntField("chunk", int64(i+1)),
-					)
-					totalSent.Add(int64(len(chunk)))
-					mpe.stats.NewTaggedStat("partition_mig_jobexec_jobs_sent", stats.CountType, mpe.statsTags()).Count(len(chunk))
-				}
-			}
-		})
-
-		// receiver goroutine
-		streamGroup.Go(func() error {
-			mpe.logger.Infon("Starting to receive acks for migrated jobs")
-			defer mpe.logger.Infon("Finished receiving acks for migrated jobs")
-			for {
-				select {
-				case <-streamCtx.Done(): // context cancelled
-					return streamCtx.Err()
-				case batchToAck, ok := <-batchesToAck: // new unacked batch to ack
-					if !ok { // channel is closed, just wait for an eof from the server
-						mpe.logger.Debugn("receiver waiting for stream to be closed by server")
-						_, err = mpe.receiveWithTimeout(streamCtx, stream)
-						if !errors.Is(err, io.EOF) {
-							return fmt.Errorf("received unexpected error while waiting for server stream to end: %w", err)
-						}
-						mpe.logger.Debugn("stream was closed by server")
-						return nil
-					}
-					// wait for the acknowledgement
-					mpe.logger.Debugn("waiting for ack from server",
-						logger.NewIntField("batchIndex", int64(batchToAck.A)),
-					)
-					ack, err := mpe.receiveWithTimeout(streamCtx, stream)
-					if err != nil {
-						return fmt.Errorf("receiving ack from server: %w", err)
-					}
-					if ack.BatchIndex != int64(batchToAck.A) {
-						return fmt.Errorf("received ack for batch %d, but expected ack for batch %d", ack.BatchIndex, batchToAck.A)
-					}
-					mpe.logger.Debugn("received ack from server",
-						logger.NewIntField("batchIndex", int64(batchToAck.A)),
-					)
-					// mark jobs as migrated
-					mpe.logger.Debugn("marking batch as migrated",
-						logger.NewIntField("batchIndex", int64(batchToAck.A)),
-					)
-					if err := mpe.updateJobStatus(streamCtx, batchToAck.B, jobsdb.Migrated.State, nil); err != nil {
-						return fmt.Errorf("marking batch %d as migrated: %w", batchToAck.A, err)
-					}
-					mpe.logger.Debugn("marked batch as migrated",
-						logger.NewIntField("batchIndex", int64(batchToAck.A)),
-					)
-					// remove from unacked batches
-					unackedBatchesMu.Lock()
-					delete(unackedBatches, batchToAck.A)
-					unackedBatchesMu.Unlock()
-					totalAcked.Add(int64(len(batchToAck.B)))
-					mpe.stats.NewTaggedStat("partition_mig_jobexec_jobs_acked", stats.CountType, mpe.statsTags()).Count(len(batchToAck.B))
-				}
-			}
-		})
-
-		g := &errgroup.Group{}
-		done := make(chan struct{})
-		// periodic logger
-		g.Go(func() error {
-			for {
-				select {
-				case <-done:
-					return nil
-				case <-time.After(mpe.progressPeriod.Load()):
-					mpe.logger.Infon("Partition migration in progress",
-						logger.NewIntField("sent", totalSent.Load()),
-						logger.NewIntField("acked", totalAcked.Load()),
-					)
-				}
-			}
-		})
-		// wait for sender and receiver to finish
-		g.Go(func() error {
-			defer close(done)
-			if err := streamGroup.Wait(); err != nil {
-				if ctx.Err() == nil {
-					mpe.logger.Errorn("Partition migration failed", obskit.Error(err))
-					for index, jobs := range unackedBatches { // no need to lock unackedBatches, we are done with sender and receiver goroutines
-						if err := mpe.updateJobStatus(ctx, jobs, jobsdb.Failed.State, fmt.Errorf("job migration interrupted: %w", err)); err != nil {
-							mpe.logger.Warnn("Could not mark non-migrated jobs as failed",
-								logger.NewIntField("batchIndex", int64(index)),
-								logger.NewStringField("tablePrefix", mpe.sourceDB.Identifier()),
-								logger.NewStringField("migrationJobID", mpe.migrationJobID),
-								logger.NewStringField("partitions", strings.Join(mpe.partitionIDs, ",")),
-								obskit.Error(err),
-							)
-						}
-					}
-				}
-				return fmt.Errorf("migrating partitions: %w", err)
-			}
-			mpe.logger.Infon("Partition migration completed successfully",
-				logger.NewIntField("total", totalAcked.Load()),
-			)
-			return nil
-		})
-		err = g.Wait()
-		mpe.logger.Infon("Partition migration progress final status",
-			logger.NewIntField("sent", totalSent.Load()),
-			logger.NewIntField("acked", totalAcked.Load()),
-			obskit.Error(err),
-		)
-		return err
-	}()
-	if err != nil {
-		mpe.logger.Errorn("Partition migration job execution failed", obskit.Error(err))
+	// mark any executing jobs as failed to handle previous interrupted migrations
+	if err := mpe.markExecutingJobsAsFailed(ctx); err != nil {
+		return fmt.Errorf("marking executing jobs as failed: %w", err)
 	}
+
+	// create a partition migrator client
+	client, err := NewPartitionMigrationClient(mpe.target, mpe.conf)
+	if err != nil {
+		return fmt.Errorf("creating partition migrator client: %w", err)
+	}
+	defer func() { // close the client on exit
+		if err := client.Close(); err != nil {
+			mpe.logger.Errorn("closing partition migrator client", obskit.Error(err))
+		}
+	}()
+
+	streamGroup, streamCtx := errgroup.WithContext(ctx) // group for sender and receiver goroutines
+	// open the stream
+	mpe.logger.Debugn("Opening the grpc stream")
+	stream, err := client.StreamJobs(streamCtx)
+	if err != nil {
+		return fmt.Errorf("initiating stream jobs: %w", err)
+	}
+
+	var totalSent atomic.Int64  // counter for total sent jobs
+	var totalAcked atomic.Int64 // counter for total acknowledged jobs
+
+	var unackedBatchesMu sync.Mutex
+	unackedBatches := make(map[int][]*jobsdb.JobT)            // map of batch index to unacked jobs in case of failure, for marking them as failed.
+	batchesToAck := make(chan lo.Tuple2[int, []*jobsdb.JobT]) // channel for sending unacked batch info to the receiver goroutine to acknowledge (one at a time). channel is closed when sender is done
+
+	// sender goroutine
+	streamGroup.Go(func() error {
+		mpe.logger.Infon("Starting to send jobs for migration")
+		defer mpe.logger.Infon("Finished sending jobs for migration")
+
+		// start by sending the metadata
+		mpe.logger.Debugn("Sending metadata")
+		if err := mpe.sendWithTimeout(ctx, stream, &proto.StreamJobsRequest{Payload: &proto.StreamJobsRequest_Metadata{
+			Metadata: &proto.JobStreamMetadata{
+				MigrationJobId: mpe.migrationJobID,
+				TablePrefix:    mpe.sourceDB.Identifier(),
+				PartitionIds:   mpe.partitionIDs,
+			},
+		}}); err != nil {
+			return fmt.Errorf("sending metadata: %w", err)
+		}
+		mpe.logger.Debugn("Metadata was sent")
+
+		// send job batches in chunks
+		batchIndex := -1
+		for {
+			batchIndex++
+			jobs, err := mpe.sourceDB.GetToProcess(streamCtx, jobsdb.GetQueryParams{
+				PartitionFilters: mpe.partitionIDs,
+				JobsLimit:        mpe.batchSize.Load(),
+			}, nil)
+			if err != nil {
+				return fmt.Errorf("getting batch %d of jobs to process: %w", batchIndex, err)
+			}
+			if len(jobs.Jobs) == 0 && !jobs.DSLimitsReached {
+				// no more jobs to process, we are done sending
+				_ = stream.CloseSend()
+				close(batchesToAck)
+				return nil
+			}
+			mpe.logger.Debugn("About to send batch of jobs",
+				logger.NewIntField("batchIndex", int64(batchIndex)),
+				logger.NewIntField("jobCount", int64(len(jobs.Jobs))),
+			)
+			mpe.logger.Debugn("Marking jobs as executing",
+				logger.NewIntField("batchIndex", int64(batchIndex)),
+			)
+			// mark them as executing
+			if err := mpe.updateJobStatus(streamCtx, jobs.Jobs, jobsdb.Executing.State, nil); err != nil {
+				return fmt.Errorf("marking batch %d of jobs as executing: %w", batchIndex, err)
+			}
+			mpe.logger.Debugn("Marked jobs as executing",
+				logger.NewIntField("batchIndex", int64(batchIndex)),
+			)
+			// add to unacked batches
+			unackedBatchesMu.Lock()
+			unackedBatches[batchIndex] = jobs.Jobs
+			unackedBatchesMu.Unlock()
+
+			// split jobs into chunks and send them
+			chunks := lo.Chunk(jobs.Jobs, mpe.chunkSize.Load())
+			mpe.logger.Debugn("Sending batch in chunks",
+				logger.NewIntField("batchIndex", int64(batchIndex)),
+				logger.NewIntField("chunkCount", int64(len(chunks))),
+			)
+			for i, chunk := range chunks {
+				mpe.logger.Debugn("Sending chunk",
+					logger.NewIntField("batchIndex", int64(batchIndex)),
+					logger.NewIntField("chunk", int64(i+1)),
+				)
+				lastChunk := i == len(chunks)-1
+				if lastChunk { // send batch info to receiver only just before the last chunk
+					mpe.logger.Debugn("Sending batch info to the receiver goroutine",
+						logger.NewIntField("batchIndex", int64(batchIndex)),
+					)
+					select {
+					case batchesToAck <- lo.T2(batchIndex, jobs.Jobs):
+						// added successfully
+					case <-streamCtx.Done():
+						return streamCtx.Err()
+					}
+				}
+				if err := mpe.sendWithTimeout(streamCtx, stream, &proto.StreamJobsRequest{
+					Payload: &proto.StreamJobsRequest_Chunk{
+						Chunk: &proto.JobsBatchChunk{
+							BatchIndex: int64(batchIndex),
+							LastChunk:  lastChunk,
+							Jobs:       lo.Map(chunk, proto.JobFromJobsdbJob),
+						},
+					},
+				}); err != nil {
+					return fmt.Errorf("streaming jobs chunk %d for batch %d: %w", i+1, batchIndex, err)
+				}
+				mpe.logger.Debugn("Sent chunk",
+					logger.NewIntField("batchIndex", int64(batchIndex)),
+					logger.NewIntField("chunk", int64(i+1)),
+				)
+				totalSent.Add(int64(len(chunk)))
+				mpe.stats.NewTaggedStat("partition_mig_jobexec_jobs_sent", stats.CountType, mpe.statsTags()).Count(len(chunk))
+			}
+		}
+	})
+
+	// receiver goroutine
+	streamGroup.Go(func() error {
+		mpe.logger.Infon("Starting to receive acks for migrated jobs")
+		defer mpe.logger.Infon("Finished receiving acks for migrated jobs")
+		for {
+			select {
+			case <-streamCtx.Done(): // context cancelled
+				return streamCtx.Err()
+			case batchToAck, ok := <-batchesToAck: // new unacked batch to ack
+				if !ok { // channel is closed, just wait for an eof from the server
+					mpe.logger.Debugn("Receiver waiting for stream to be closed by server")
+					_, err = mpe.receiveWithTimeout(streamCtx, stream)
+					if !errors.Is(err, io.EOF) {
+						return fmt.Errorf("received unexpected error while waiting for server stream to end: %w", err)
+					}
+					mpe.logger.Debugn("Stream was closed by server")
+					return nil
+				}
+				// wait for the acknowledgement
+				mpe.logger.Debugn("Waiting for ack from server",
+					logger.NewIntField("batchIndex", int64(batchToAck.A)),
+				)
+				ack, err := mpe.receiveWithTimeout(streamCtx, stream)
+				if err != nil {
+					return fmt.Errorf("receiving ack from server: %w", err)
+				}
+				if ack.BatchIndex != int64(batchToAck.A) {
+					return fmt.Errorf("received ack for batch %d, but expected ack for batch %d", ack.BatchIndex, batchToAck.A)
+				}
+				mpe.logger.Debugn("Received ack from server",
+					logger.NewIntField("batchIndex", int64(batchToAck.A)),
+				)
+				// mark jobs as migrated
+				mpe.logger.Debugn("Marking batch as migrated",
+					logger.NewIntField("batchIndex", int64(batchToAck.A)),
+				)
+				if err := mpe.updateJobStatus(streamCtx, batchToAck.B, jobsdb.Migrated.State, nil); err != nil {
+					return fmt.Errorf("marking batch %d as migrated: %w", batchToAck.A, err)
+				}
+				mpe.logger.Debugn("Marked batch as migrated",
+					logger.NewIntField("batchIndex", int64(batchToAck.A)),
+				)
+				// remove from unacked batches
+				unackedBatchesMu.Lock()
+				delete(unackedBatches, batchToAck.A)
+				unackedBatchesMu.Unlock()
+				totalAcked.Add(int64(len(batchToAck.B)))
+				mpe.stats.NewTaggedStat("partition_mig_jobexec_jobs_acked", stats.CountType, mpe.statsTags()).Count(len(batchToAck.B))
+			}
+		}
+	})
+
+	g := &errgroup.Group{}
+	done := make(chan struct{})
+	// periodic logger
+	g.Go(func() error {
+		for {
+			select {
+			case <-done:
+				return nil
+			case <-time.After(mpe.progressPeriod.Load()):
+				mpe.logger.Infon("Partition migration in progress",
+					logger.NewIntField("sent", totalSent.Load()),
+					logger.NewIntField("acked", totalAcked.Load()),
+				)
+			}
+		}
+	})
+	// wait for sender and receiver to finish
+	g.Go(func() error {
+		defer close(done)
+		if err := streamGroup.Wait(); err != nil {
+			if ctx.Err() == nil {
+				mpe.logger.Errorn("Partition migration failed", obskit.Error(err))
+				for index, jobs := range unackedBatches { // no need to lock unackedBatches, we are done with sender and receiver goroutines
+					if err := mpe.updateJobStatus(ctx, jobs, jobsdb.Failed.State, fmt.Errorf("job migration interrupted: %w", err)); err != nil {
+						mpe.logger.Warnn("Could not mark non-migrated jobs as failed",
+							logger.NewIntField("batchIndex", int64(index)),
+							logger.NewStringField("tablePrefix", mpe.sourceDB.Identifier()),
+							logger.NewStringField("migrationJobID", mpe.migrationJobID),
+							logger.NewStringField("partitions", strings.Join(mpe.partitionIDs, ",")),
+							obskit.Error(err),
+						)
+					}
+				}
+			}
+			return fmt.Errorf("migrating partitions: %w", err)
+		}
+		mpe.logger.Infon("Partition migration completed successfully",
+			logger.NewIntField("total", totalAcked.Load()),
+		)
+		return nil
+	})
+	err = g.Wait()
+	mpe.logger.Infon("Partition migration progress final status",
+		logger.NewIntField("sent", totalSent.Load()),
+		logger.NewIntField("acked", totalAcked.Load()),
+		obskit.Error(err),
+	)
 	return err
 }
 
