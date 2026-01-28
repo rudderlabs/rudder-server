f7f80a1c47f40ee6d74277ca037ce00200c58cd1
Francesco Casula <fra.casula@gmail.com>
2026-01-28 14:01:09 +0100

    test: python mirroring
    🔒 Scanned for secrets using gitleaks version is set by build process

--- processor/internal/transformer/user_transformer/user_transformer_test.go
+++ processor/internal/transformer/user_transformer/user_transformer_test.go
@@ -860,6 +860,186 @@ func TestUserTransformer(t *testing.T) {
 					require.Equal(t, expectedResponse, rsp)
 				})
 			})
+
+			t.Run("python mirroring routing", func(t *testing.T) {
+				msgID := "messageID-0"
+				expectedResponse := types.Response{
+					Events: []types.TransformerResponse{
+						{
+							Output: map[string]any{
+								"src-key-1": msgID,
+							},
+							Metadata: types.Metadata{
+								MessageID: msgID,
+							},
+							StatusCode: http.StatusOK,
+						},
+					},
+				}
+
+				makeEvents := func(language string) []types.TransformerEvent {
+					return []types.TransformerEvent{{
+						Metadata: types.Metadata{
+							MessageID: msgID,
+						},
+						Message: map[string]any{
+							"src-key-1": msgID,
+						},
+						Destination: backendconfig.DestinationT{
+							DestinationDefinition: backendconfig.DestinationDefinitionT{
+								Name: "test-destination",
+							},
+							Transformations: []backendconfig.TransformationT{
+								{
+									ID:        "test-transformation",
+									VersionID: "test-version",
+									Language:  language,
+								},
+							},
+						},
+						Credentials: []types.Credential{
+							{
+								ID:       "test-credential",
+								Key:      "test-key",
+								Value:    "test-value",
+								IsSecret: false,
+							},
+						},
+					}}
+				}
+
+				t.Run("python mirroring routes to python mirror URL when configured", func(t *testing.T) {
+					jsMirrorSrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						t.Error("request should not hit JS mirror transformer")
+					}))
+					defer jsMirrorSrv.Close()
+
+					pythonMirrorSrv := httptest.NewServer(&endpointTransformer{
+						supportedPaths: []string{"/customTransform"},
+						t:              t,
+					})
+					defer pythonMirrorSrv.Close()
+
+					c := config.New()
+					c.Set("Processor.maxRetry", 1)
+					c.Set("USER_TRANSFORM_MIRROR_URL", jsMirrorSrv.URL)
+					c.Set("PYTHON_TRANSFORM_MIRROR_URL", pythonMirrorSrv.URL)
+
+					tr := user_transformer.New(c, logger.NOP, stats.Default, user_transformer.ForMirroring())
+					rsp := tr.Transform(context.TODO(), makeEvents("pythonfaas"))
+
+					require.Equal(t, expectedResponse, rsp)
+				})
+
+				t.Run("pythonwithlibs mirroring routes to python mirror URL", func(t *testing.T) {
+					jsMirrorSrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						t.Error("request should not hit JS mirror transformer")
+					}))
+					defer jsMirrorSrv.Close()
+
+					pythonMirrorSrv := httptest.NewServer(&endpointTransformer{
+						supportedPaths: []string{"/customTransform"},
+						t:              t,
+					})
+					defer pythonMirrorSrv.Close()
+
+					c := config.New()
+					c.Set("Processor.maxRetry", 1)
+					c.Set("USER_TRANSFORM_MIRROR_URL", jsMirrorSrv.URL)
+					c.Set("PYTHON_TRANSFORM_MIRROR_URL", pythonMirrorSrv.URL)
+
+					tr := user_transformer.New(c, logger.NOP, stats.Default, user_transformer.ForMirroring())
+					rsp := tr.Transform(context.TODO(), makeEvents("pythonwithlibs"))
+
+					require.Equal(t, expectedResponse, rsp)
+				})
+
+				t.Run("python mirroring skips when python mirror URL not configured", func(t *testing.T) {
+					jsMirrorSrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						t.Error("request should not hit JS mirror transformer")
+					}))
+					defer jsMirrorSrv.Close()
+
+					c := config.New()
+					c.Set("Processor.maxRetry", 1)
+					c.Set("USER_TRANSFORM_MIRROR_URL", jsMirrorSrv.URL)
+					// PYTHON_TRANSFORM_MIRROR_URL not set
+
+					memStats, err := memstats.New()
+					require.NoError(t, err)
+
+					tr := user_transformer.New(c, logger.NOP, memStats, user_transformer.ForMirroring())
+					rsp := tr.Transform(context.TODO(), makeEvents("pythonfaas"))
+
+					require.Equal(t, types.Response{}, rsp)
+					require.EqualValues(t, 1, memStats.Get("processor_transformer_skipped_events_for_mirroring", nil).LastValue())
+				})
+
+				t.Run("javascript mirroring routes to JS mirror URL when configured", func(t *testing.T) {
+					jsMirrorSrv := httptest.NewServer(&endpointTransformer{
+						supportedPaths: []string{"/customTransform"},
+						t:              t,
+					})
+					defer jsMirrorSrv.Close()
+
+					pythonMirrorSrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						t.Error("request should not hit Python mirror transformer")
+					}))
+					defer pythonMirrorSrv.Close()
+
+					c := config.New()
+					c.Set("Processor.maxRetry", 1)
+					c.Set("USER_TRANSFORM_MIRROR_URL", jsMirrorSrv.URL)
+					c.Set("PYTHON_TRANSFORM_MIRROR_URL", pythonMirrorSrv.URL)
+
+					tr := user_transformer.New(c, logger.NOP, stats.Default, user_transformer.ForMirroring())
+					rsp := tr.Transform(context.TODO(), makeEvents("javascript"))
+
+					require.Equal(t, expectedResponse, rsp)
+				})
+
+				t.Run("javascript mirroring skips when JS mirror URL not configured", func(t *testing.T) {
+					pythonMirrorSrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						t.Error("request should not hit Python mirror transformer")
+					}))
+					defer pythonMirrorSrv.Close()
+
+					c := config.New()
+					c.Set("Processor.maxRetry", 1)
+					// USER_TRANSFORM_MIRROR_URL not set
+					c.Set("PYTHON_TRANSFORM_MIRROR_URL", pythonMirrorSrv.URL)
+
+					memStats, err := memstats.New()
+					require.NoError(t, err)
+
+					tr := user_transformer.New(c, logger.NOP, memStats, user_transformer.ForMirroring())
+					rsp := tr.Transform(context.TODO(), makeEvents("javascript"))
+
+					require.Equal(t, types.Response{}, rsp)
+					require.EqualValues(t, 1, memStats.Get("processor_transformer_skipped_events_for_mirroring", nil).LastValue())
+				})
+
+				t.Run("mirroring skips when both mirror URLs not configured", func(t *testing.T) {
+					c := config.New()
+					c.Set("Processor.maxRetry", 1)
+					// Neither URL set
+
+					memStats, err := memstats.New()
+					require.NoError(t, err)
+
+					tr := user_transformer.New(c, logger.NOP, memStats, user_transformer.ForMirroring())
+
+					// Test Python
+					rsp := tr.Transform(context.TODO(), makeEvents("pythonfaas"))
+					require.Equal(t, types.Response{}, rsp)
+
+					// Test JavaScript
+					rsp = tr.Transform(context.TODO(), makeEvents("javascript"))
+					require.Equal(t, types.Response{}, rsp)
+
+					require.EqualValues(t, 2, memStats.Get("processor_transformer_skipped_events_for_mirroring", nil).LastValue())
+				})
+			})
 		})
 	}
 }
