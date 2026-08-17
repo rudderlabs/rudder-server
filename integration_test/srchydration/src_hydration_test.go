15fb2cb4145873f938dee9a31912ffda87cd9301
Aris Tzoumas <atzoumas@rudderstack.com>
2026-08-17 10:03:36 +0300

    fix: src hydration flaky integration test

--- integration_test/srchydration/src_hydration_test.go
+++ integration_test/srchydration/src_hydration_test.go
@@ -413,15 +413,20 @@ func prepareExpectedReports(t *testing.T, sourceId string, gwOnly bool, numEvent
 
 func requireReports(t *testing.T, ctx context.Context, db *sql.DB, expectedReports []reportRow) {
 	t.Helper()
+	// multiple rows can be recorded for the same report key, so aggregate their counts
 	query := `
 					SELECT
 					  workspace_id, instance_id, source_id, destination_id,
-					  in_pu, pu, status_code, status, count,
+					  in_pu, pu, status_code, status, SUM(count),
 					  terminal_state, initial_state, source_category, event_type
 					FROM
 					  reports
+					GROUP BY
+					  workspace_id, instance_id, source_id, destination_id,
+					  in_pu, pu, status_code, status,
+					  terminal_state, initial_state, source_category, event_type
 					ORDER BY
-					  source_id, id;
+					  source_id, MIN(id);
 				`
 	require.Eventuallyf(t, func() bool {
 		rows, err := db.QueryContext(ctx, query)
